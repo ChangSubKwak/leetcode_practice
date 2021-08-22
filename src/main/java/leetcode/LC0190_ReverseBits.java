0619b904d6fb42987fa91d9e906245233cb23722
ChangSubKwak <paradise1732@gmail.com>
2021-08-22 23:54:41 +0900

    feat: solved 191. Number of 1 Bits

---
+++ src/main/java/leetcode/LC0190_ReverseBits.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class LC0190_ReverseBits {
+    public int reverseBits(int n) {
+        return Integer.reverse(n);
+    }
+    
+	public static void main(String[] args) {
+		LC0190_ReverseBits t = new LC0190_ReverseBits();
+		System.out.println(t.reverseBits(43261596));
+	}
+}
