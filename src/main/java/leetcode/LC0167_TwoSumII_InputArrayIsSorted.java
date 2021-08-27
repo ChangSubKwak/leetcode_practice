31aab8bc5cd3e29c2bb21cbc596b7aca1afa64bc
ChangSubKwak <paradise1732@gmail.com>
2021-08-27 23:51:25 +0900

    feat: (WIP) solving 167. Two Sum II - Input array is sorted

---
+++ src/main/java/leetcode/LC0167_TwoSumII_InputArrayIsSorted.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class LC0167_TwoSumII_InputArrayIsSorted {
+    public int[] twoSum(int[] numbers, int target) {
+        return null;
+    }
+    
+	public static void main(String[] args) {
+		LC0167_TwoSumII_InputArrayIsSorted t = new LC0167_TwoSumII_InputArrayIsSorted();
+		System.out.println(t.twoSum(new int[] {2, 7, 11, 15}, 9));
+	}
+}
