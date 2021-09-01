a1d78dbf1df9c44edcdfbdf9bca5bd3f57cfe18d
ChangSubKwak <paradise1732@gmail.com>
2021-09-01 22:30:02 +0900

    feat: (WIP) solving 198. House Robber

---
+++ src/main/java/leetcode/LC0198_HouseRobber.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class LC0198_HouseRobber {
+    public int rob(int[] nums) {
+    	System.out.println(Arrays.toString(nums));
+        return 0;
+    }
+    
+	public static void main(String[] args) {
+		LC0198_HouseRobber t = new LC0198_HouseRobber();
+		System.out.println(t.rob(new int[] { 1, 2, 3, 1 }));
+	}
+}
