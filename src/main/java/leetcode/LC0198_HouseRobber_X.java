248d9621764d68fa4c7757c605e07c15fecabe31
ChangSubKwak <paradise1732@gmail.com>
2021-09-02 22:26:17 +0900

    feat: (WIP) rename 198. Houser Robber

--- src/main/java/leetcode/LC0198_HouseRobber_X.java
+++ src/main/java/leetcode/LC0198_HouseRobber_X.java
@@ -2,14 +2,14 @@ package leetcode;
 
 import java.util.Arrays;
 
-public class LC0198_HouseRobber {
+public class LC0198_HouseRobber_X {
     public int rob(int[] nums) {
     	System.out.println(Arrays.toString(nums));
         return 0;
     }
     
 	public static void main(String[] args) {
-		LC0198_HouseRobber t = new LC0198_HouseRobber();
+		LC0198_HouseRobber_X t = new LC0198_HouseRobber_X();
 		System.out.println(t.rob(new int[] { 1, 2, 3, 1 }));
 	}
 }
