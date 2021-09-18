4ed9f48439577a632f839bac55a5e47f26317153
ChangSubKwak <paradise1732@gmail.com>
2021-09-18 16:40:25 +0900

    feat: (WIP) solving 126. Word Ladder II #2

--- src/main/java/leetcode/LC0126_WordLadderII.java
+++ src/main/java/leetcode/LC0126_WordLadderII.java
@@ -1,9 +1,23 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class LC0126_WordLadderII {
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+    	// init
+    	Set<String> dict = new HashSet<String>(wordList);
+    	List<List<String>> res = new ArrayList<List<String>>();
+    	Map<String, ArrayList<String>> nodeNeighbors = new HashMap<String, ArrayList<String>>();
+    	Map<String, Integer> distance = new HashMap<String, Integer>();
+    	
+    	dict.add(beginWord);
+    	//bfs(beginWord, endWord, dict, nodeNeighbors, distance);
+    	
         // bfs
     	// dfs
     	
