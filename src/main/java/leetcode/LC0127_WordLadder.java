b632560be11be9aeb55e0f6e032ff3dbe3e010d0
ChangSubKwak <paradise1732@gmail.com>
2021-09-24 23:46:49 +0900

    feat: solved 127. Word Ladder

---
+++ src/main/java/leetcode/LC0127_WordLadder.java
@@ -0,0 +1,106 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+public class LC0127_WordLadder {
+	public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+    	// init
+    	Set<String> dict = new HashSet<String>(wordList);
+    	List<List<String>> res = new ArrayList<List<String>>();
+    	Map<String, ArrayList<String>> nodeNeighbors = new HashMap<String, ArrayList<String>>();
+    	Map<String, Integer> distance = new HashMap<String, Integer>();
+    	ArrayList<String> solution = new ArrayList<>();
+    	
+    	dict.add(beginWord);
+    	bfs(beginWord, endWord, dict, nodeNeighbors, distance);
+    	dfs(beginWord, endWord, dict, nodeNeighbors, distance, solution, res);
+    	
+    	return res.size() > 0 ? res.get(0).size() : 0;
+    }
+    
+    private void bfs(String beginWord, String endWord, Set<String> dict, Map<String, ArrayList<String>> nodeNeighbors, Map<String, Integer> distance) {
+    	for (String str: dict) {
+    		nodeNeighbors.put(str, new ArrayList<String>());
+    	}
+    	
+    	Queue<String> queue = new LinkedList<String>();
+    	queue.offer(beginWord);
+    	distance.put(beginWord, 0);
+    	
+    	while (!queue.isEmpty()) {
+    		int count = queue.size();
+    		boolean foundEnd = false;
+    		
+    		for (int i = 0; i < count; i++) {
+    			String cur = queue.poll();
+    			int curDistance = distance.get(cur);
+    			List<String> neighbors = getNeighbors(cur, dict);
+    			
+    			for (String neighbor: neighbors) {
+    				nodeNeighbors.get(cur).add(neighbor);
+    				if (!distance.containsKey(neighbor)) {
+    					distance.put(neighbor, curDistance + 1);
+    					if (endWord.equals(neighbor)) {
+    						foundEnd = true;
+    					} else {
+    						queue.offer(neighbor);
+    					}
+    				}
+    			}
+        		if (foundEnd) {
+        			break;
+        		}
+    		}
+    	}
+    }
+    
+    private List<String> getNeighbors(String node, Set<String> dict) {
+    	List<String> res = new ArrayList<>();
+    	char[] chs = node.toCharArray();
+    	
+    	for (char ch = 'a'; ch <= 'z'; ch++) {
+    		for (int i = 0; i < chs.length; i++) {
+    			if (chs[i] == ch) {
+    				continue;
+    			}
+    			char old_ch = chs[i];			// why does it assign to old_ch?
+    											// use old_ch to recover original char 
+    			chs[i] = ch;
+    			if (dict.contains(String.valueOf(chs))) {
+    				res.add(String.valueOf(chs));
+    			}
+    			chs[i] = old_ch;
+    		}
+    	}
+    	
+    	return res;
+    }
+    
+    private void dfs(String startWord, String endWord, Set<String> dict, Map<String, ArrayList<String>> nodeNeighbors, Map<String, Integer> distance, ArrayList<String> solution, List<List<String>> res) {
+    	solution.add(startWord);
+    	if (endWord.equals(startWord)) {
+    		res.add(new ArrayList<String>(solution));
+    	} else {
+    		for (String next : nodeNeighbors.get(startWord)) {
+    			if (distance.get(next) == distance.get(startWord) + 1) {
+    				dfs(next, endWord, dict, nodeNeighbors, distance, solution, res);
+    			}
+    		}
+    	}
+    	solution.remove(solution.size() - 1);
+    }
+	
+	public static void main(String[] args) {
+		LC0127_WordLadder t = new LC0127_WordLadder();
+		System.out.println(t.ladderLength("hit", "cog", Arrays.asList("hot","dot","dog","lot","log","cog")));
+		System.out.println(t.ladderLength("hit", "cog", Arrays.asList("hot","dot","dog","lot","log")));
+	}
+}
