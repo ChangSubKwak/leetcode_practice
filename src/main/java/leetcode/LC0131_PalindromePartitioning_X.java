0f71b2864d6a2cba4bc17b3ba456408ce2a18ef3
ChangSubKwak <paradise1732@gmail.com>
2021-09-27 23:03:34 +0900

    feat: solved 131. Palindrom Partitioning

--- src/main/java/leetcode/LC0131_PalindromePartitioning_X.java
+++ src/main/java/leetcode/LC0131_PalindromePartitioning_X.java
@@ -3,7 +3,7 @@ package leetcode;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LC0131_PalindromePartitioning {
+public class LC0131_PalindromePartitioning_X {
     public List<List<String>> partition(String s) {
         int len = s.length();
         boolean[][] dp = new boolean[len][len];
@@ -16,10 +16,18 @@ public class LC0131_PalindromePartitioning {
     	if (start >= s.length()) {
     		result.add(new ArrayList<>(currentList));
     	}
+    	for (int end = start; end < s.length(); end++) {
+    		if (s.charAt(start) == s.charAt(end) && (end - start <= 2 || dp[start + 1][end - 1])) {
+    			dp[start][end] = true;
+    			currentList.add(s.substring(start, end + 1));
+    			dfs(result, s, end + 1, currentList, dp);
+    			currentList.remove(currentList.size() - 1);
+    		}
+    	}
     }
 	
 	public static void main(String[] args) {
-		LC0131_PalindromePartitioning t = new LC0131_PalindromePartitioning();
+		LC0131_PalindromePartitioning_X t = new LC0131_PalindromePartitioning_X();
 		System.out.println(t.partition("aab"));
 	}
 }
