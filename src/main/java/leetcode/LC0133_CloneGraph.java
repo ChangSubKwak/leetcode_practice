fd42559b9f0c7cf9b1a886635b2aa2ccc67fe993
ChangSubKwak <paradise1732@gmail.com>
2021-10-09 23:40:32 +0900

    feat: (WIP) solving 133. Clone Graph #2

--- src/main/java/leetcode/LC0133_CloneGraph.java
+++ src/main/java/leetcode/LC0133_CloneGraph.java
@@ -1,9 +1,12 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LC0133_CloneGraph {
+	
 	class Node {
 	    public int val;
 	    public List<Node> neighbors;
@@ -20,22 +23,66 @@ public class LC0133_CloneGraph {
 	        neighbors = _neighbors;
 	    }
 	}
+
+	Map<Integer, Node> map = new HashMap<>();
+	
 	
     public Node cloneGraph(Node node) {
+    	if (node == null) {
+    		return null;
+    	}
+    	
     	Node cloneNode = new Node();
     	cloneNode.val = node.val;
     	
-    	for (int i = 0; i < node.neighbors.size(); i++) {
-    		cloneNode.neighbors.add(node.neighbors.get(i));
+    	List<Node> neighborsNode = node.neighbors;
+    	for(int i = 0; i < neighborsNode.size(); i++) {
+    		cloneNode.neighbors.add(neighborsNode.get(i));
     	}
     	
-		return cloneNode;
+    	return cloneNode;
     }
-	
+    
+    public void executeTest() {
+    	Node node1 = new Node(1);
+    	Node node2 = new Node(2);
+    	Node node3 = new Node(3);
+    	Node node4 = new Node(4);
+    	
+    	node1.neighbors.add(node2);
+    	node1.neighbors.add(node4);
+    	
+    	node2.neighbors.add(node1);
+    	node2.neighbors.add(node3);
+    	
+    	node3.neighbors.add(node2);
+    	node3.neighbors.add(node4);
+    	
+    	node4.neighbors.add(node1);
+    	node4.neighbors.add(node3);
+    	
+    	printNodeAndNeighborsVal(node1);
+    	printNodeAndNeighborsVal(node2);
+    	printNodeAndNeighborsVal(node3);
+    	printNodeAndNeighborsVal(node4);
+
+    	System.out.println("--- Clone Node ---");
+    	Node cloneNode = cloneGraph(node1);
+    	printNodeAndNeighborsVal(cloneNode);
+    }
+    
+    public void printNodeAndNeighborsVal(Node node) {
+    	System.out.print("val(" + node.val + ")\t");
+    	System.out.print("neighbors(");
+    	for (int i = 0; i < node.neighbors.size(); i++) {
+    		System.out.print(node.neighbors.get(i).val + (i == node.neighbors.size() - 1 ? "" : " ") );
+    	}
+    	System.out.println(")");
+    }
+    
 	public static void main(String[] args) {
 		@SuppressWarnings("unused")
 		LC0133_CloneGraph t = new LC0133_CloneGraph();
-		
-
+		t.executeTest();
 	}
 }
