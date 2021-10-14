f4bd2d7733841d8b480d2e2afe9631e54bc11ce3
ChangSubKwak <paradise1732@gmail.com>
2021-10-14 20:35:59 +0900

    feat: (WIP) solving 279. Perfect Squares

---
+++ src/main/java/leetcode/LC0279_PerfectSquares.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class LC0279_PerfectSquares {
+    public int numSquares(int n) {
+    	int[] dp = new int[10001];
+    	
+    	return 0;
+    }
+
+	public static void main(String[] args) {
+		LC0279_PerfectSquares t = new LC0279_PerfectSquares();
+		System.out.println(t.numSquares(12));
+	}
+
+}
