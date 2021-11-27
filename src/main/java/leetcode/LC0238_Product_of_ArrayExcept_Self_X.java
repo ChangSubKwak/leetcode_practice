f16a295f685b847ced302ed276f40be81da7bce5
ChangSubKwak <paradise1732@gmail.com>
2021-11-27 17:48:26 +0900

    feat: solved 238. Product of Array Except Self

---
+++ src/main/java/leetcode/LC0238_Product_of_ArrayExcept_Self_X.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class LC0238_Product_of_ArrayExcept_Self_X {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+        
+        return res;
+    }
+}
