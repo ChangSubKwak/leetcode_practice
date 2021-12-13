9dad5a671a297a4af5344d779578a0d8415424d3
ChangSubKwak <paradise1732@gmail.com>
2021-12-13 23:47:13 +0900

    feat: solved 1446. Consecutive Characters

---
+++ src/main/java/leetcode/LC1446_ConsecutiveCharacters.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class LC1446_ConsecutiveCharacters {
+    public int minStartValue(int[] nums) {
+        int start = 1;
+        while (true) {
+            int sum = start;
+            for (int i = 0; i < nums.length; i++) {
+                sum += nums[i];
+                if (sum < 1) {
+                    break;
+                }
+            }
+            
+            if (sum >= 1) {
+                break;
+            }
+            start++;
+        }
+        return start;
+    }
+}
