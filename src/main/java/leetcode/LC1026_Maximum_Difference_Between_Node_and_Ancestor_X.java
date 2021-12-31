cde9ea5faf3aa4a830a5af01faeba0a61d848372
ChangSubKwak <paradise1732@gmail.com>
2021-12-31 19:26:45 +0900

    feat: solved 1026. Maximum Difference Between Node and Ancestor

---
+++ src/main/java/leetcode/LC1026_Maximum_Difference_Between_Node_and_Ancestor_X.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class LC1026_Maximum_Difference_Between_Node_and_Ancestor_X {
+    public int maxAncestorDiff(TreeNode root) {
+        return dfs(root, root.val, root.val);
+    }
+
+    public int dfs(TreeNode root, int mn, int mx) {
+        if (root == null) return mx - mn;
+        mx = Math.max(mx, root.val);
+        mn = Math.min(mn, root.val);
+        return Math.max(dfs(root.left, mn, mx), dfs(root.right, mn, mx));
+    }
+}
