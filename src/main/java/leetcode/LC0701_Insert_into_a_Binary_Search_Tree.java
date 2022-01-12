afc56c9a4cac8a962f158933bd362f160348a942
changsubkwak <paradise1732@gmail.com>
2022-01-12 20:05:18 +0900

    feat: solved 701. Insert into a Binary Search Tree

--- src/main/java/leetcode/LC0701_Insert_into_a_Binary_Search_Tree.java
+++ src/main/java/leetcode/LC0701_Insert_into_a_Binary_Search_Tree.java
@@ -1,6 +1,36 @@
 package leetcode;
 
-public class LC0701_InsertintoaBinarySearchTree {
+public class LC0701_Insert_into_a_Binary_Search_Tree {
+    private void recursive(TreeNode root, int val) {
+        if (root == null) {
+            // root = new TreeNode(val);
+            return;
+        }
+        
+        if (root.val > val) {
+            recursive(root.left, val);
+            if (root.left == null) {
+                root.left = new TreeNode(val);
+            }
+        }
+        
+        if (root.val < val) {
+            recursive(root.right, val);
+            if (root.right == null) {
+                root.right = new TreeNode(val);
+            }
+        }
+    }
+    
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        recursive(root, val);
+        return root;
+    }
+	
+	/*
     private void recursive(TreeNode root, TreeNode parent, char direction, int val) {
         if (root == null && direction == 'L') {
             root = new TreeNode(val);
@@ -29,4 +59,5 @@ public class LC0701_InsertintoaBinarySearchTree {
         recursive(root, null, 'M', val);
         return root;
     }
+    */
 }
