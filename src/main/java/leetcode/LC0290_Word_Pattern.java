0f07d464d251cde83641fb55941a6292bca979ef
changsubkwak <paradise1732@gmail.com>
2022-01-17 20:01:22 +0900

    feat: solved 239. Word Pattern

---
+++ src/main/java/leetcode/LC0290_Word_Pattern.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LC0290_Word_Pattern {
+    public boolean wordPattern(String pattern, String s) {
+        String[] word = s.split(" ");
+        Map<Character, String> map = new HashMap<>();
+        
+        if (pattern.length() != word.length) {
+            return false;
+        }
+        
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            if (!map.containsKey(c)) {
+                if (map.containsValue(word[i])) {
+                   return false;
+                }
+                
+                map.put(c, word[i]);
+                continue;
+            }
+            
+            String tempWord = map.get(c);
+            if (!tempWord.equals(word[i])) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
