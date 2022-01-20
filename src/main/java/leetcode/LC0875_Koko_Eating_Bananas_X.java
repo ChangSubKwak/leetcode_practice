6c49f8f806080f3181981e148d113d6901209b4a
changsubkwak <paradise1732@gmail.com>
2022-01-20 22:20:54 +0900

    feat: solved 875. Koko Eating Bananas

---
+++ src/main/java/leetcode/LC0875_Koko_Eating_Bananas_X.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+public class LC0875_KoKo_Eating_Bananas_X {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1, right = 1;
+        for (int pile : piles) {
+            right = Math.max(right, pile);
+        }
+
+        while (left < right) {
+            int middle = (left + right) / 2;
+            int hourSpent = 0;
+
+            for (int pile : piles) {
+                hourSpent += Math.ceil((double) pile / middle);
+            }
+
+            if (hourSpent <= h) {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+        }
+        return right;
+    }
+}
