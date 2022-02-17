981e2d969543b2c1fdc411944f245a9379f01f55
changsubkwak <paradise1732@gmail.com>
2022-02-17 11:19:19 +0900

    feat: solved 24. Swap Nodes in Pairs

--- src/main/java/leetcode/LC0024_Swap_Nodes_in_Pairs.java
+++ src/main/java/leetcode/LC0024_Swap_Nodes_in_Pairs.java
@@ -2,6 +2,47 @@ package leetcode;
 
 public class LC0024_Swap_Nodes_in_Pairs {
     public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        int count = 0;
+        ListNode p = new ListNode();
+        p.next = head;
+        
+        ListNode t1 = new ListNode();
+        ListNode t2 = new ListNode();
+        ListNode t0 = new ListNode();
+        
+        while(p.next != null) {
+            t1 = p;
+            p = p.next;
+            t2 = p;
+            count++;
+            if (count % 2 == 1) {
+                t0 = t1;
+                continue;
+            }
+            
+            if (count == 2) {
+                t1.next = t2.next;
+                t2.next = t1;
+                head = t2;
+                p = t1;
+                continue;
+            }
+            
+            t1.next = t2.next;
+            t0.next = t2;
+            t2.next = t1;
+            p = t1;
+        }
+        
+        return head;
+    }
+    
+	/*
+	public ListNode swapPairs(ListNode head) {
     	ListNode t = head;
     	
     	while(t != null) {
@@ -19,6 +60,7 @@ public class LC0024_Swap_Nodes_in_Pairs {
     	
         return head;
     }
+    */
 	
 	public static void main(String[] args) {
 		LC0024_Swap_Nodes_in_Pairs t = new LC0024_Swap_Nodes_in_Pairs();
