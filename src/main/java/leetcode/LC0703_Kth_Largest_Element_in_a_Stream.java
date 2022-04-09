1a6901cdbc86d2bf195b376a8f7b09c19479f3ce
changsubkwak <paradise1732@gmail.com>
2022-04-09 09:11:20 +0900

    feat: solved 703. Kth Largest Element in a Stream

--- src/main/java/leetcode/LC0703_Kth_Largest_Element_in_a_Stream.java
+++ src/main/java/leetcode/LC0703_Kth_Largest_Element_in_a_Stream.java
@@ -1,13 +1,26 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LC0703_Kth_Largest_Element_in_a_Stream {
-
-    public void KthLargest(int k, int[] nums) {
-        
-    }
-    
-    public int add(int val) {
-        return 0;
-    }
-
+	class KthLargest {
+	    private List<Integer> list;
+	    private int k;
+		
+	    public KthLargest(int k, int[] nums) {
+	        list = Arrays.stream(nums)
+	        	.boxed()
+	            .collect(Collectors.toList());
+            this.k = k;
+	    }
+	    
+	    public int add(int val) {
+	        list.add(val);
+	        Collections.sort(list);
+	        return list.get(list.size() - k);
+	    }
+	}
 }
