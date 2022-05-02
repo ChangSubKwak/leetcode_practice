b303745f248199978524015a20ee0f10ccb6a2bc
changsubkwak <paradise1732@gmail.com>
2022-05-02 09:01:32 +0900

    fix: create inner class

--- src/main/java/leetcode/LC0705_Design_HashSet.java
+++ src/main/java/leetcode/LC0705_Design_HashSet.java
@@ -1,25 +1,26 @@
 package leetcode;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LC0705_Design_HashSet {
-    Set<Integer> set;
+	class MyHashSet {
+	    Set<Integer> set;
 
-    public MyHashSet() {
-        set = new HashSet();
-    }
-    
-    public void add(int key) {
-        set.add(key);
-    }
-    
-    public void remove(int key) {
-        set.remove(key);
-    }
-    
-    public boolean contains(int key) {
-        return set.contains(key);
-    }
+	    public MyHashSet() {
+	        set = new HashSet();
+	    }
+	    
+	    public void add(int key) {
+	        set.add(key);
+	    }
+	    
+	    public void remove(int key) {
+	        set.remove(key);
+	    }
+	    
+	    public boolean contains(int key) {
+	        return set.contains(key);
+	    }
+	}
 }
