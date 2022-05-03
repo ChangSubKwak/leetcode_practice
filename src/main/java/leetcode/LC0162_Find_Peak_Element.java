6eeee8983d9b067e6d6b9b4dfe4228c2827e2b44
changsubkwak <paradise1732@gmail.com>
2022-05-04 00:47:34 +0900

    feat: solved 162. Find Peak Element

--- src/main/java/leetcode/LC0162_Find_Peak_Element.java
+++ src/main/java/leetcode/LC0162_Find_Peak_Element.java
@@ -2,6 +2,50 @@ package leetcode;
 
 public class LC0162_Find_Peak_Element {
     public int findPeakElement(int[] nums) {
-        return 0;
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            boolean firstIsGreater = false;
+            boolean secondIsGreater = false;
+            
+            if (i == 0) {
+                if (Integer.MIN_VALUE < nums[i]) {
+                    firstIsGreater = true;
+                }
+                
+                if (length == 1 && nums[i] > Integer.MIN_VALUE) {
+                    secondIsGreater = true;
+                }
+                
+                if (length > 1 && nums[i] > nums[i+1]) {
+                    secondIsGreater = true;
+                }
+                
+                if (firstIsGreater && secondIsGreater) {
+                    return 0;
+                }
+                continue;
+            }
+            
+            if (i == length - 1) {
+                if (nums[i-1] < nums[i]) {
+                    firstIsGreater = true;
+                }
+                
+                if (nums[i] > Integer.MIN_VALUE) {
+                    secondIsGreater = true;
+                }
+                
+                if (firstIsGreater && secondIsGreater) {
+                    return length - 1;
+                }
+                continue;
+            }
+            
+            if (nums[i-1] < nums[i] && nums[i] > nums[i+1]) {
+                return i;
+            }
+        }
+        
+        return length - 1;
     }
 }
