c85d80b0cce27032c4acc07b17171fdf4ecf47bc
changsubkwak <paradise1732@gmail.com>
2022-05-11 00:15:49 +0900

    feat: solved 202. Happy Number

---
+++ src/main/java/leetcode/LC0202_Happy_Number.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class LC0202_Happy_Number {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        
+        int sum = 0;
+        while(sum != 1) {
+            String s = String.valueOf(n);
+            sum = 0;
+            for (int i = 0; i < s.length(); i++) {
+                sum += Math.pow((int)(s.charAt(i) - '0'), 2);
+            }
+            
+            n = sum;
+            if (set.contains(sum)) {
+                return false;
+            }
+            set.add(sum);
+        }
+        return true;
+    }
+}
