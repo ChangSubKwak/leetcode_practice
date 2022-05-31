5cfc7e3674c8f0758f7f6674e607a6220a28d064
changsubkwak <paradise1732@gmail.com>
2022-05-31 22:06:04 +0900

    feat: solved 29. Divide Two Integers

---
+++ src/main/java/leetcode/LC0029_Divide_Two_Integers_X.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+public class LC0029_Divide_Two_Integers_X {
+    public int divide(int dividend, int divisor) {
+        int sign = 1;
+        if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)) {
+            sign = -1;
+        }
+        
+        long ldividend = Math.abs((long) dividend);
+        long ldivisor = Math.abs((long) divisor);
+
+        if ((ldividend == 0) || (ldividend < ldivisor))	{
+            return 0;
+        }
+
+        long lans = ldivide(ldividend, ldivisor);
+
+        int ans;
+        if (lans > Integer.MAX_VALUE) {
+            ans = (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        } else {
+            ans = (int) (sign * lans);
+        }
+        return ans;
+    }
+
+    private long ldivide(long ldividend, long ldivisor) {
+        if (ldividend < ldivisor) {
+            return 0;
+        }
+
+        long sum = ldivisor;
+        long multiple = 1;
+        while ((sum + sum) <= ldividend) {
+            sum += sum;
+            multiple += multiple;
+        }
+        return multiple + ldivide(ldividend - sum, ldivisor);
+    }
+}
