d35ef617e38105c1ce1862488a7003a4e280710c
ChangSubKwak <paradise1732@gmail.com>
2022-06-06 10:17:46 +0900

    feat: solved 51. N-Queens (another idea)

--- src/main/java/leetcode/LC0051_NQueens.java
+++ src/main/java/leetcode/LC0051_NQueens.java
@@ -2,9 +2,60 @@ package leetcode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class LC0051_NQueens {
+	public List<List<String>> solveNQueens(int n) {
+		char[][] board = new char[n][n];
+		for(int i = 0; i < n; i++)
+			for(int j = 0; j < n; j++)
+				board[i][j] = '.';
+		List<List<String>> res = new ArrayList<>();
+		dfs(board, 0, res);
+		return res;
+	}
+
+	private void dfs(char[][] board, int colIndex, List<List<String>> res) {
+		if(colIndex == board.length) {
+			res.add(construct(board));
+			return;
+		}
+
+		for(int i = 0; i < board.length; i++) {
+			if(validate(board, i, colIndex)) {
+				board[i][colIndex] = 'Q';
+				dfs(board, colIndex + 1, res);
+				board[i][colIndex] = '.';
+			}
+		}
+	}
+
+	private boolean validate(char[][] board, int newY, int newX) {
+		for(int y = 0; y < board.length; y++) {
+			for(int x = 0; x < newX; x++) {
+				if (board[y][x] == 'Q' && (Math.abs(newY - y) == Math.abs(newX - x) || newY == y)) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	private List<String> construct(char[][] board) {
+		List<String> res = new LinkedList<>();
+		for (char[] chars : board) {
+			String s = new String(chars);
+			res.add(s);
+		}
+		return res;
+	}
+
+	/**
+	 * solution #1
+	 */
+	/*
 	public boolean check(List<Integer> rowList, int x, int len) {
 		int[] dx = {-1,  1,  1, -1};
 		int[] dy = {-1, -1,  1,  1};
@@ -34,7 +85,7 @@ public class LC0051_NQueens {
 		}
 		return true;
 	}
-	
+
 	public void execute(List<List<String>> result, char[][] board, List<Integer> rowList) {
 		int len = board.length;
 		if (rowList.size() == len) {
@@ -62,7 +113,7 @@ public class LC0051_NQueens {
 			board[rowList.size()][x] = '.';
 		}
 	}
-	
+
     public List<List<String>> solveNQueens(int n) {
     	List<List<String>> result = new ArrayList<>();
     	if (n==1) {
@@ -79,4 +130,6 @@ public class LC0051_NQueens {
     	
         return result;
     }
+
+	*/
 }
