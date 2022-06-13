1cd1edac375a8206e2c05937ffeebb3668246993
changsubkwak <paradise1732@gmail.com>
2022-06-13 19:20:57 +0900

    feat: solved 120. Triangle

--- src/main/java/leetcode/LC0120_Triangle.java
+++ src/main/java/leetcode/LC0120_Triangle.java
@@ -5,6 +5,29 @@ import java.util.List;
 
 public class LC0120_Triangle {
     public int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle.size() == 1) {
+            return triangle.get(0).get(0);
+        }
+        
+        int row = triangle.size();
+        int[] dp = new int[row];
+        
+        for (int i = 0; i < row; i++) {
+            dp[i] = triangle.get(row - 1).get(i);
+        }
+        
+        for (int i = row - 2; i >= 0; i--) {
+            List<Integer> list = triangle.get(i);
+            for (int j = 0; j < list.size(); j++) {
+                dp[j] = Math.min(dp[j] + list.get(j), dp[j + 1] + list.get(j));
+            }
+        }
+        
+        return dp[0];
+    }
+    
+    /*
+    public int minimumTotal(List<List<Integer>> triangle) {
         int size = triangle.size();
         int[] dp = new int[size];
         for (int i = 0 ; i < triangle.get(size - 1).size() ; i++)
@@ -14,23 +37,8 @@ public class LC0120_Triangle {
         	for (int x = 0 ; x < triangle.get(y).size() ; x++) {
         		dp[x] = Math.min(triangle.get(y).get(x) + dp[x], triangle.get(y).get(x) + dp[x+1]);
         	}
-//        	System.out.println(Arrays.toString(dp));
         }
-        
-        //return Arrays.stream(dp).min().getAsInt();
         return dp[0];
     }
-	
-	public static void main(String[] args) {
-		LC0120_Triangle t = new LC0120_Triangle();
-		List<List<Integer>> list = new ArrayList<>();
-		List<Integer> temp = new ArrayList<>();
-		temp.add(2);	list.add(new ArrayList<>(temp));
-		temp.clear();	temp.add(3);	temp.add(4);	list.add(new ArrayList<>(temp));
-		temp.clear();	temp.add(6);	temp.add(5);	temp.add(7);	list.add(new ArrayList<>(temp));
-		temp.clear();	temp.add(4);	temp.add(1);	temp.add(8);	temp.add(3);	list.add(new ArrayList<>(temp));
-		
-		System.out.println(list);
-		System.out.println(t.minimumTotal(list));
-	}
+    */
 }
