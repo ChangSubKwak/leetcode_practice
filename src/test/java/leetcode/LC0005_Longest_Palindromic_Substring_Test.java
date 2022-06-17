b5101214f3797b288f589135e6e10778837fd455
changsubkwak <paradise1732@gmail.com>
2022-06-17 09:22:36 +0900

    test: implement test case - 5. Longest Palindromic Substring

---
+++ src/test/java/leetcode/LC0005_Longest_Palindromic_Substring_Test.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LC0005_Longest_Palindromic_Substring_Test {
+	LC0005_Longest_Palindromic_Substring lc;
+	
+	@BeforeEach
+	void setUp() {
+		lc = new LC0005_Longest_Palindromic_Substring();
+	}
+	
+	@Test
+	void test() {
+		assertThat(lc.longestPalindrome("babad")).isEqualTo("bab");
+		assertThat(lc.longestPalindrome("cbbd")).isEqualTo("bb");
+		assertThat(lc.longestPalindrome("a")).isEqualTo("a");
+		assertThat(lc.longestPalindrome("ac")).isEqualTo("a");
+		assertThat(lc.longestPalindrome("bb")).isEqualTo("bb");
+		assertThat(lc.longestPalindrome("zudfweormatjycujjirzjpyrmaxurectxrtqedmmgergwdvjmjtstdhcihacqnothgttgqfywcpgnuvwglvfiuxteopoyizgehkwuvvkqxbnufkcbodlhdmbqyghkojrgokpwdhtdrwmvdegwycecrgjvuexlguayzcammupgeskrvpthrmwqaqsdcgycdupykppiyhwzwcplivjnnvwhqkkxildtyjltklcokcrgqnnwzzeuqioyahqpuskkpbxhvzvqyhlegmoviogzwuiqahiouhnecjwysmtarjjdjqdrkljawzasriouuiqkcwwqsxifbndjmyprdozhwaoibpqrthpcjphgsfbeqrqqoqiqqdicvybzxhklehzzapbvcyleljawowluqgxxwlrymzojshlwkmzwpixgfjljkmwdtjeabgyrpbqyyykmoaqdambpkyyvukalbrzoyoufjqeftniddsfqnilxlplselqatdgjziphvrbokofvuerpsvqmzakbyzxtxvyanvjpfyvyiivqusfrsufjanmfibgrkwtiuoykiavpbqeyfsuteuxxjiyxvlvgmehycdvxdorpepmsinvmyzeqeiikajopqedyopirmhymozernxzaueljjrhcsofwyddkpnvcvzixdjknikyhzmstvbducjcoyoeoaqruuewclzqqqxzpgykrkygxnmlsrjudoaejxkipkgmcoqtxhelvsizgdwdyjwuumazxfstoaxeqqxoqezakdqjwpkrbldpcbbxexquqrznavcrprnydufsidakvrpuzgfisdxreldbqfizngtrilnbqboxwmwienlkmmiuifrvytukcqcpeqdwwucymgvyrektsnfijdcdoawbcwkkjkqwzffnuqituihjaklvthulmcjrhqcyzvekzqlxgddjoir")).isEqualTo("gykrkyg");
+	}
+}
