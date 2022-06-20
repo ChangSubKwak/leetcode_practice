50e7dcbd71321f04cdce707e8411cd1d47e1715c
changsubkwak <paradise1732@gmail.com>
2022-06-20 11:03:30 +0900

    test: implement test case - 820. Short Encoding of Words

---
+++ src/test/java/leetcode/LC820_Short_Encoding_of_Words_Test.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class LC820_Short_Encoding_of_Words_Test {
+    
+    LC820_Short_Encoding_of_Words test;
+    
+    @BeforeEach
+    void setUp() {
+        test = new LC820_Short_Encoding_of_Words();
+    }
+    
+    @Test
+    void test() {
+        assertThat(test.minimumLengthEncoding(new String[]{"p", "grah", "qwosp"})).isEqualTo(11);
+        assertThat(test.minimumLengthEncoding(new String[]{"feipyxx", "e"})).isEqualTo(10);
+        assertThat(test.minimumLengthEncoding(new String[]{"time", "me", "bell"})).isEqualTo(10);
+        assertThat(test.minimumLengthEncoding(new String[]{"t"})).isEqualTo(2);
+        assertThat(test.minimumLengthEncoding(new String[]{"me", "time"})).isEqualTo(5);
+    }
+}
