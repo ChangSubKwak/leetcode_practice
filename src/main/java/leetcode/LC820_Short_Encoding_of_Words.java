69812c12cb00a00e6538bdb233dd3f355caf85f4
changsubkwak <paradise1732@gmail.com>
2022-06-20 11:03:55 +0900

    feat: solved 820. Short Encoding of Words

---
+++ src/main/java/leetcode/LC820_Short_Encoding_of_Words.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC820_Short_Encoding_of_Words {
+    public int minimumLengthEncoding(String[] words) {
+        int length = words.length;
+        if (length == 1) {
+            return words[0].length() + 1;
+        }
+        
+        List<String> list = new ArrayList<>();
+        for (String s: words) {
+            list.add(s);
+        }
+        boolean isContinue = true;
+
+        start:
+        while(isContinue) {
+            for (int i = 0; i < list.size(); i++) {
+                for (int j = 0; j < list.size(); j++) {
+                    if (i == j) {
+                        continue;
+                    }
+                    if (list.get(j).endsWith(list.get(i))) {
+                        list.remove(i);
+                        continue start;
+                    }
+                }
+            }
+            isContinue = false;
+        }
+        
+        int sum = list.size();
+        for (String s: list) {
+            sum += s.length();
+        }
+        
+        return sum;
+    }
+}
