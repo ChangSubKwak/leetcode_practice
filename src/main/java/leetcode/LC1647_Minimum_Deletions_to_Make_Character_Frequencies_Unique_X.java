2683147c5ca41f0de8422f0456f2943e0a3d25bf
changsubkwak <paradise1732@gmail.com>
2022-06-29 15:35:39 +0900

    refactor: change variable name

--- src/main/java/leetcode/LC1647_Minimum_Deletions_to_Make_Character_Frequencies_Unique_X.java
+++ src/main/java/leetcode/LC1647_Minimum_Deletions_to_Make_Character_Frequencies_Unique_X.java
@@ -5,23 +5,23 @@ import java.util.Arrays;
 public class LC1647_Minimum_Deletions_to_Make_Character_Frequencies_Unique_X {
     
     public int minDeletions(String s) {
-        int[] freq = new int[26];
+        int[] count = new int[26];
     
         for (char c : s.toCharArray()){
-            freq[c - 'a']++;
+            count[c - 'a']++;
         }
     
-        Arrays.sort(freq);
+        Arrays.sort(count);
         int deleteCount = 0;
         for (int i = 24; i >= 0; i--) {
-            if (freq[i] == 0) {
+            if (count[i] == 0) {
                 break;
             }
         
-            if (freq[i] >= freq[i + 1]) {
-                int prev = freq[i];
-                freq[i] = Math.max(0, freq[i + 1] -1);
-                deleteCount += prev - freq[i];
+            if (count[i] >= count[i + 1]) {
+                int prev = count[i];
+                count[i] = Math.max(0, count[i + 1] -1);
+                deleteCount += prev - count[i];
             }
         }
         return deleteCount;
