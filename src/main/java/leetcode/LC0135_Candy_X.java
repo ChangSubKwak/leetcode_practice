6f4617cc9f75f10fbaf8de30971689dd7678ff0a
changsubkwak <paradise1732@gmail.com>
2022-07-04 21:42:21 +0900

    feat: solved 135. Candy

--- src/main/java/leetcode/LC0135_Candy_X.java
+++ src/main/java/leetcode/LC0135_Candy_X.java
@@ -4,34 +4,27 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class LC0135_Candy_X {
-    private int getCell(int y, int x) {
-        if (x < 0 || y < 0) {
-            return 0;
-        }
-        
-        return dp[y][x];
-    }
-    
-    private int row;
-    private int col;
-    private int[][] dp;
-    private int maxEdge;
-    
-    public int maximalSquare(char[][] matrix) {
-        row = matrix.length;
-        col = matrix[0].length;
-        dp = new int[row][col];
-        maxEdge = 0;
-        
-        for (int y = 0; y < row; y++) {
-            for (int x = 0; x < col; x++) {
-                if (matrix[y][x] == '1') {
-                    dp[y][x] = Collections.min(Arrays.asList(getCell(y - 1, x), getCell(y, x - 1), getCell(y - 1, x - 1))) + 1;
-                    maxEdge = Math.max(maxEdge, dp[y][x]);
+    public int candy(int[] ratings) {
+        int[] candies = new int[ratings.length];
+        Arrays.fill(candies, 1);
+        boolean hasChanged = true;
+        while (hasChanged) {
+            hasChanged = false;
+            for (int i = 0; i < ratings.length; i++) {
+                if (i != ratings.length - 1 && ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
+                    candies[i] = candies[i + 1] + 1;
+                    hasChanged = true;
+                }
+                if (i > 0 && ratings[i] > ratings[i - 1] && candies[i] <= candies[i - 1]) {
+                    candies[i] = candies[i - 1] + 1;
+                    hasChanged = true;
                 }
             }
         }
-        
-        return maxEdge * maxEdge;
+        int sum = 0;
+        for (int candy : candies) {
+            sum += candy;
+        }
+        return sum;
     }
 }
