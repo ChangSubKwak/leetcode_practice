72d3431c4511b97de07b31782af000cbe52cbe18
changsubkwak <paradise1732@gmail.com>
2022-07-06 23:54:30 +0900

    feat: solved 509. Fibonacci Number

---
+++ src/main/java/leetcode/LC0509_Fibonacci_Number.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+public class LC0509_Fibonacci_Number {
+    public int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+}
