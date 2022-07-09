7de221bd4e2f8c6b0f17dab7459bf33c3466f507
changsubkwak <paradise1732@gmail.com>
2022-07-09 12:02:42 +0900

    test: implement test case - 1696. Jump Game VI

---
+++ src/test/java/leetcode/LC1696_Jump_Game_VI_Test.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+class LC1696_Jump_Game_VI_Test {
+
+    LC1696_Jump_Game_VI test = new LC1696_Jump_Game_VI();
+
+    @Test
+    void exampleTest() {
+        assertThat(test.maxResult(new int[] {1, -1, -2, 4, -7, 3}, 2)).isEqualTo(7);
+        assertThat(test.maxResult(new int[] {10, -5, -2, 4, 0, 3}, 3)).isEqualTo(17);
+        assertThat(test.maxResult(new int[] {1, -5, -20, 4, -1, 3, -6, -3}, 2)).isEqualTo(0);
+    }
+}
