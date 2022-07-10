02cac4e1f7c58750dbc6eeeb4f9df2a0128c43b1
changsubkwak <paradise1732@gmail.com>
2022-07-10 23:50:22 +0900

    chore: change function name

--- src/test/java/leetcode/LC1696_Jump_Game_VI_Test.java
+++ src/test/java/leetcode/LC1696_Jump_Game_VI_Test.java
@@ -9,7 +9,7 @@ class LC1696_Jump_Game_VI_Test {
     LC1696_Jump_Game_VI test = new LC1696_Jump_Game_VI();
 
     @Test
-    void exampleTest() {
+    void test() {
         assertThat(test.maxResult(new int[] {1, -1, -2, 4, -7, 3}, 2)).isEqualTo(7);
         assertThat(test.maxResult(new int[] {10, -5, -2, 4, 0, 3}, 3)).isEqualTo(17);
         assertThat(test.maxResult(new int[] {1, -5, -20, 4, -1, 3, -6, -3}, 2)).isEqualTo(0);
