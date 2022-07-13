3a529f1cbc59c570028c2a7b2ec69b4061b16120
changsubkwak <paradise1732@gmail.com>
2022-07-14 00:09:05 +0900

    feat: 102. Binary Tree Level Order Traversal

--- src/main/java/leetcode/LC0102_Binary_Tree_Level_Order_Traversal.java
+++ src/main/java/leetcode/LC0102_Binary_Tree_Level_Order_Traversal.java
@@ -3,44 +3,68 @@ package leetcode;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LC0102_BinaryTreeLevelOrderTraversal {
+public class LC0102_Binary_Tree_Level_Order_Traversal {
+	List<List<Integer>> result;
+
+	private void recursive(TreeNode root, int depth) {
+		if (root == null) {
+			return;
+		}
+
+		if (result.size() < depth) {
+			result.add(new ArrayList<>());
+		}
+
+		result.get(depth - 1).add(root.val);
+		recursive(root.left, depth + 1);
+		recursive(root.right, depth + 1);
+	}
+
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		result = new ArrayList<>();
+		recursive(root, 1);
+		return result;
+	}
+
+	/*
 	public int getDepth(TreeNode root, int depth) {
 		if (root == null) return depth;
 		return Math.max(getDepth(root.left, depth+1), getDepth(root.right, depth+1));
 	}
-	
+
 	public void inorder(TreeNode root, List<List<Integer>> list, int depth) {
 		if (root == null) return ;
 		inorder(root.left , list, depth + 1);
-		
+
 		list.get(depth-1).add(root.val);
-		
+
 		inorder(root.right, list, depth + 1);
 	}
-	
+
     public List<List<Integer>> levelOrder(TreeNode root) {
-    	if (root==null) 
+    	if (root==null)
     		return new ArrayList<>();
-    	
+
     	int depth = getDepth(root, 1);
 //    	System.out.println(depth);
     	List<List<Integer>> list = new ArrayList<>();
     	for (int i = 0 ; i < depth ; i++)
     		list.add(new ArrayList<Integer>());
     	inorder(root, list, 1);
-    	
+
     	for (int i = 0 ; i < list.size() ; i++) {
     		if (list.get(i).size() == 0) {
     			list.remove(i);
     			i--;
     		}
     	}
-    	
+
     	return list;
     }
+    */
 	
 	public static void main(String[] args) {
-		LC0102_BinaryTreeLevelOrderTraversal t = new LC0102_BinaryTreeLevelOrderTraversal();
+		LC0102_Binary_Tree_Level_Order_Traversal t = new LC0102_Binary_Tree_Level_Order_Traversal();
 		TreeNode tn15 = new TreeNode(15);
 		TreeNode tn7  = new TreeNode(7);
 		TreeNode tn20 = new TreeNode(20, tn15, tn7);
