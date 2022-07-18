cecc6de641068e60b6fde0f3e65d243ce16c0132
changsubkwak <paradise1732@gmail.com>
2022-07-18 13:59:33 +0900

    feat: solved 860. Lemonade Change

---
+++ src/main/java/leetcode/C0860_Lemonade_Change.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class C0860_Lemonade_Change {
+    public boolean lemonadeChange(int[] bills) {
+        Map<Integer, Integer> billMap = new HashMap<>();
+        billMap.put(5, 0);
+        billMap.put(10, 0);
+        billMap.put(20, 0);
+    
+        for (int  bill: bills) {
+            if (bill == 5) {
+                billMap.put(5, billMap.get(5) + 1);
+                continue;
+            }
+        
+            if (bill == 10) {
+                if (billMap.get(5) <= 0) {
+                    return false;
+                }
+                billMap.put(10, billMap.get(10) + 1);
+                billMap.put(5, billMap.get(5) - 1);
+                continue;
+            }
+        
+            if (billMap.get(5) >= 1 && billMap.get(10) >= 1) {
+                billMap.put(20, billMap.get(20) + 1);
+                billMap.put(10, billMap.get(10) - 1);
+                billMap.put(5, billMap.get(5) - 1);
+                continue;
+            }
+        
+            if (billMap.get(5) >= 3) {
+                billMap.put(20, billMap.get(20) + 1);
+                billMap.put(5, billMap.get(5) - 3);
+                continue;
+            }
+        
+            return false;
+        }
+    
+        return true;
+    }
+}
