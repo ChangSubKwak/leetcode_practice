6cc1bf5d5846db2eddfa6c6cc20911da6fdd8124
changsubkwak <paradise1732@gmail.com>
2022-07-18 13:59:17 +0900

    test: implement test case - 860. Lemonade Change

---
+++ src/test/java/leetcode/LC0860_Lemonade_Change_Test.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LC0860_Lemonade_Change_Test {
+    C0860_Lemonade_Change test;
+    
+    @BeforeEach
+    void setUp() {
+        test = new C0860_Lemonade_Change();
+    }
+    
+    @Test
+    void test() {
+        assertThat(test.lemonadeChange(new int[]{5, 5, 5, 10, 20})).isTrue();
+        assertThat(test.lemonadeChange(new int[]{5, 5, 10, 10, 20})).isFalse();
+        assertThat(test.lemonadeChange(new int[]{5, 5, 5, 5, 5, 5, 10, 10, 20, 20})).isTrue();
+        assertThat(test.lemonadeChange(new int[]{5, 5, 5, 5, 5, 10, 10, 10, 10, 20})).isTrue();
+        assertThat(test.lemonadeChange(new int[]{5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 10, 10, 20, 20, 20})).isTrue();
+    }
+}
