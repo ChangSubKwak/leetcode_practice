a3ad23ae3e1dc58701b0c9ea77a3f85f00e2d0de
changsubkwak <paradise1732@gmail.com>
2022-07-20 23:25:14 +0900

    test: implement test case - 792. Number of Matching Subsequences

---
+++ src/test/java/leetcode/LC0792_Number_of_Matching_Subsequences_Test.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+public class LC0792_Number_of_Matching_Subsequences_Test {
+	LC0792_Number_of_Matching_Subsequences test;
+	
+	@BeforeEach
+	void setUp() {
+		test = new LC0792_Number_of_Matching_Subsequences();
+	}
+	
+	@Test
+	void test() {
+		assertThat(test.numMatchingSubseq("abcde", new String[] {"a", "acd", "ace", "bb"})).isEqualTo(3);
+		assertThat(test.numMatchingSubseq("dsahjpjauf", new String[] {"ahjpjau","ja","ahbwzgqnuk","tnmlanowax"})).isEqualTo(2);
+	}
+}
