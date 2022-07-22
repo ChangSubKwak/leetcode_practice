0604b1471fc3cf7b3260533b245b7437a22a8655
changsubkwak <paradise1732@gmail.com>
2022-07-22 17:35:24 +0900

    feat: solved 792. Number of Matching Subsequences

--- src/main/java/leetcode/LC0792_Number_of_Matching_Subsequences.java
+++ src/main/java/leetcode/LC0792_Number_of_Matching_Subsequences.java
@@ -1,48 +1,34 @@
 package leetcode;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LC0792_Number_of_Matching_Subsequences {
     public int numMatchingSubseq(String s, String[] words) {
-        Map<String, Integer> map = new HashMap<>();
-        Map<String, Integer> countMap = new HashMap<>();
-        for (String word : words) {
-            map.put(word, 0);
-            if (countMap.containsKey(word)) {
-                countMap.put(word, countMap.get(word) + 1);
-                continue;
-            }
-            countMap.put(word, 1);
+        int answer = 0;
+        List<List<StringBuilder>> waiting = new ArrayList<>();
+        for (int i = 0; i <= 128; i++) {
+            waiting.add(new ArrayList<>());
         }
         
-        Set<String> resultSet = new HashSet<>();
+        for (String w : words) {
+            waiting.get(w.charAt(0)).add(new StringBuilder(w));
+        }
+    
         for (char c : s.toCharArray()) {
-            Set<String> keys = new HashSet<>(map.keySet());
-            for (String word : keys) {
-                if (resultSet.contains(word)) {
+            List<StringBuilder> advance = new ArrayList<>(waiting.get(c));
+            waiting.get(c).clear();
+    
+            for (StringBuilder it : advance){
+                it.deleteCharAt(0);
+                if (it.length() != 0) {
+                    waiting.get(it.charAt(0)).add(it);
                     continue;
                 }
-                
-                int pos = map.get(word);
-                if (pos < word.length() && c == word.charAt(pos)) {
-                    map.put(word, map.get(word) + 1);
-                }
-                
-                if (word.length() == map.get(word)) {
-                    map.remove(word);
-                    resultSet.add(word);
-                }
+                answer++;
             }
         }
         
-        int count = 0;
-        for (String s2: resultSet) {
-            count += countMap.get(s2);
-        }
-        
-        return count;
+        return answer;
     }
 }
