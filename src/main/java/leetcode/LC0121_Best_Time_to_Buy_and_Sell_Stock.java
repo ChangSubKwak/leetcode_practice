00d643501c240225f2e0c60afcb3e093a1254673
changsubkwak <paradise1732@gmail.com>
2022-08-16 00:25:28 +0900

    feat: solved 121. Best Time to Buy and Sell Stock

--- src/main/java/leetcode/LC0121_Best_Time_to_Buy_and_Sell_Stock.java
+++ src/main/java/leetcode/LC0121_Best_Time_to_Buy_and_Sell_Stock.java
@@ -2,6 +2,20 @@ package leetcode;
 
 public class LC0121_Best_Time_to_Buy_and_Sell_Stock {
     public int maxProfit(int[] prices) {
+        int length = prices.length;
+
+        int max = 0;
+        int benefit = 0;
+        for (int i = 1 ; i < length; i++) {
+            benefit += prices[i] - prices[i - 1];
+            benefit = Math.max(benefit, 0);
+            max = Math.max(max, benefit);
+        }
+
+        return max;
+    }
+
+    public int maxProfit1(int[] prices) {
         int curMax = 0;
         int soFarMax = 0;
         
