55af45fd81747b614d3759d478b4d73d15985c88
changsubkwak <paradise1732@gmail.com>
2022-08-19 18:21:19 +0900

    feat: solved 659. Split Array into Consecutive Subsequences

---
+++ src/main/java/leetcode/LC0659_Split_Array_into_Consecutive_Subsequences_X.java
@@ -0,0 +1,51 @@
+package leetcode;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class LC0659_Split_Array_into_Consecutive_Subsequences_X {
+    public boolean isPossible(int[] nums) {
+        PriorityQueue<Interval> pq = new PriorityQueue<>(new Comparator<Interval>(){
+            @Override
+            public int compare(Interval a, Interval b) {
+                if (a.end == b.end) {
+                    return Integer.compare(a.length, b.length);
+                }
+                return Integer.compare(a.end, b.end);
+            }
+        });
+        
+        for (int num : nums) {
+            while (!pq.isEmpty() && pq.peek().end + 1 < num) {
+                if (pq.poll().length < 3) {
+                    return false;
+                }
+            }
+            
+            if (pq.isEmpty() || pq.peek().end == num) {
+                pq.add(new Interval(num, num));
+            } else {
+                pq.add(new Interval(pq.poll().start, num));
+            }
+        }
+        
+        while (!pq.isEmpty()) {
+            if (pq.poll().length < 3)
+                return false;
+        }
+        
+        return true;
+    }
+    
+    class Interval {
+        int start;
+        int end;
+        int length;
+        
+        public Interval(int start, int end) {
+            this.start = start;
+            this.end = end;
+            length = end - start + 1;
+        }
+    }
+}
