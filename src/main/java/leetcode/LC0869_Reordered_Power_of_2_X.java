64e255a4f94d6185077e6f738487f29082bb4f12
changsubkwak <paradise1732@gmail.com>
2022-08-26 22:24:50 +0900

    feat: solved 869. Reordered Power of 2

---
+++ src/main/java/leetcode/LC0869_Reordered_Power_of_2_X.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class LC0869_Reordered_Power_of_2_X {
+    public boolean reorderedPowerOf2(int N) {
+        int[] A = count(N);
+        for (int i = 0; i < 31; ++i) {
+            if (Arrays.equals(A, count(1 << i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public int[] count(int N) {
+        int[] ans = new int[10];
+        while (N > 0) {
+            ans[N % 10]++;
+            N /= 10;
+        }
+        return ans;
+    }
+}
