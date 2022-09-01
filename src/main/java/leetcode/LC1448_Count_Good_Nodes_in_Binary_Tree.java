728099e1e43eba3b28b1ef8114b39758cc05b295
changsubkwak <paradise1732@gmail.com>
2022-09-02 00:43:40 +0900

    feat: solved 1448. Count Good Nodes in Binary Tree

--- src/main/java/leetcode/LC1448_Count_Good_Nodes_in_Binary_Tree.java
+++ src/main/java/leetcode/LC1448_Count_Good_Nodes_in_Binary_Tree.java
@@ -1,7 +1,25 @@
 package leetcode;
 
 public class LC1448_Count_Good_Nodes_in_Binary_Tree {
+    private int count;
+
+    private void recur(TreeNode root, int max) {
+        if (root == null) {
+            return;
+        }
+
+        if (max <= root.val) {
+            max = root.val;
+            count++;
+        }
+        recur(root.left, max);
+        recur(root.right, max);
+    }
+
     public int goodNodes(TreeNode root) {
-        return 4;
+        count = 0;
+        recur(root, root.val);
+
+        return count;
     }
 }
