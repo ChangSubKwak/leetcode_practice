804e6ceab2fcb6c3919bbb698e3047c181709283
changsubkwak <paradise1732@gmail.com>
2022-09-01 23:47:06 +0900

    test: implement test case - 1448. Count Good Nodes in Binary Tree

---
+++ src/test/java/leetcode/LC1448_Count_Good_Nodes_in_Binary_Tree_Test.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+
+public class LC1448_Count_Good_Nodes_in_Binary_Tree_Test {
+    LC1448_Count_Good_Nodes_in_Binary_Tree test;
+
+    @BeforeEach
+    void setUp() {
+        test = new LC1448_Count_Good_Nodes_in_Binary_Tree();
+    }
+
+    @Test
+    void test() {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(4);
+        root.left.left = new TreeNode(3);
+        root.right.left = new TreeNode(1);
+        root.right.right = new TreeNode(5);
+
+        Assertions.assertThat(test.goodNodes(root)).isEqualTo(4);
+    }
+}
