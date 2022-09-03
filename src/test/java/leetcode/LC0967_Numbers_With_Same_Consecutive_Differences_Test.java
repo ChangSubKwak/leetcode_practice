dd76b71d2bbc22f4afaf332191feec90f24f1b5b
changsubkwak <paradise1732@gmail.com>
2022-09-03 21:08:33 +0900

    test: implement test case #2 - 967. Numbers With Same Consecutive Differences

--- src/test/java/leetcode/LC0967_Numbers_With_Same_Consecutive_Differences_Test.java
+++ src/test/java/leetcode/LC0967_Numbers_With_Same_Consecutive_Differences_Test.java
@@ -17,6 +17,6 @@ public class LC0967_Numbers_With_Same_Consecutive_Differences_Test {
     @Test
     void test() {
         assertThat(test.numsSameConsecDiff(3, 7)).isEqualTo(new int[] {181, 292, 707, 818, 929});
-        // assertThat(test.numsSameConsecDiff(2, 1)).isEqualTo(new int[] {10, 12, 21, 23, 32, 34, 43, 45, 54, 56, 65, 67, 76, 78, 87, 89, 98});
+        assertThat(test.numsSameConsecDiff(2, 1)).isEqualTo(new int[] {10, 12, 21, 23, 32, 34, 43, 45, 54, 56, 65, 67, 76, 78, 87, 89, 98});
     }
 }
