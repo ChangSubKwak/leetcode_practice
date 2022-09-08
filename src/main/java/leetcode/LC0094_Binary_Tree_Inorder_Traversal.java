80f78e11c823ed69dece277fc17b795d81bea875
changsubkwak <paradise1732@gmail.com>
2022-09-08 22:20:49 +0900

    feat: solved 94. Binary Tree Inorder Traversal

---
+++ src/main/java/leetcode/LC0094_Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import java.util.*;
+
+public class LC0094_Binary_Tree_Inorder_Traversal {
+    private void inorder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+        return result;
+    }
+}
