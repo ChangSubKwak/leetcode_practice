1a1d33020cb30fbdff4fc4beb2b97679e63c72d2
changsubkwak <paradise1732@gmail.com>
2022-09-09 23:50:09 +0900

    test: implement test case - 1996. The Number of Weak Characters in the Game

---
+++ src/test/java/leetcode/LC1996_The_Number_of_Weak_Characters_in_the_Game_Test.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+class LC1996_The_Number_of_Weak_Characters_in_the_Game_Test {
+    LC1996_The_Number_of_Weak_Characters_in_the_Game test = new LC1996_The_Number_of_Weak_Characters_in_the_Game();
+
+    @Test
+    void exampleTest() {
+        assertThat(test.numberOfWeakCharacters(new int[][] { {5, 5}, {6, 3}, {3, 6} })).isEqualTo(0);
+        assertThat(test.numberOfWeakCharacters(new int[][] { {2, 2}, {3, 3} })).isEqualTo(1);
+        assertThat(test.numberOfWeakCharacters(new int[][] { {1, 5}, {10, 4}, {4, 3} })).isEqualTo(1);
+    }
+}
