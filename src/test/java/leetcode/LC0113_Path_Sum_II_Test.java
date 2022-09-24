7523dd160beb8db962a2df0e2c0a80a869d8904d
changsubkwak <paradise1732@gmail.com>
2022-09-24 23:59:48 +0900

    test: implement test case - 113. Path Sum II

---
+++ src/test/java/leetcode/LC0113_Path_Sum_II_Test.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+
+public class LC0113_Path_Sum_II_Test {
+	LC0113_Path_Sum_II test;
+	
+	@Test
+	void test() {
+		test = new LC0113_Path_Sum_II();
+
+		TreeNode root = new TreeNode(5);
+		root.left = new TreeNode(4);
+		root.left.left = new TreeNode(11);
+		root.left.left.left = new TreeNode(7);
+		root.left.left.right = new TreeNode(2);
+		root.left.right = new TreeNode(7);
+
+		root.right = new TreeNode(8);
+		root.right.left = new TreeNode(13);
+		root.right.right = new TreeNode(4);
+		root.right.right.left = new TreeNode(5);
+		root.right.right.right = new TreeNode(1);
+
+		Assertions.assertThat(test.pathSum(root, 22)).isEqualTo(
+			Arrays.asList(
+				Arrays.asList(5, 4, 11, 2),
+				Arrays.asList(5, 8, 4, 5)
+			)
+		);
+	}
+}
