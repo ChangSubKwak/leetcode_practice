66640e87253f74f93b10076e78773cb422ffd2a1
changsubkwak <paradise1732@gmail.com>
2022-09-30 00:39:26 +0900

    chore: add semi-colon - 658. Find K Closet Elements

--- src/test/java/leetcode/LC0658_Find_K_Closet_Elements_Test.java
+++ src/test/java/leetcode/LC0658_Find_K_Closet_Elements_Test.java
@@ -11,7 +11,7 @@ public class LC0658_Find_K_Closet_Elements_Test {
 	void test() {
 		LC0658_Find_K_Closet_Elements test = new LC0658_Find_K_Closet_Elements();
 
-		assertThat(test.findClosestElements(new int[] {1, 2, 3, 4, 5}, 4, 3)).isEqualTo(Arrays.asList(1, 2, 3, 4))
-		assertThat(test.findClosestElements(new int[] {1, 2, 3, 4, 5}, 4, 1)).isEqualTo(Arrays.asList(1, 2, 3, 4))
+		assertThat(test.findClosestElements(new int[] {1, 2, 3, 4, 5}, 4, 3)).isEqualTo(Arrays.asList(1, 2, 3, 4));
+		assertThat(test.findClosestElements(new int[] {1, 2, 3, 4, 5}, 4, 1)).isEqualTo(Arrays.asList(1, 2, 3, 4));
 	}
 }
