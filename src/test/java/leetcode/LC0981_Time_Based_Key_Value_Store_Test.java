b5493cb1aaff19afc7e516ba6d86639796335cca
changsubkwak <paradise1732@gmail.com>
2022-10-06 23:56:16 +0900

    test: (WIP) implmenting test case - 981. Time Based Key-Value Store

---
+++ src/test/java/leetcode/LC0981_Time_Based_Key_Value_Store_Test.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.*;
+
+import org.junit.jupiter.api.*;
+
+public class LC0981_Time_Based_Key_Value_Store_Test {
+    @Test
+    void test() {
+        LC0981_Time_Based_Key_Value_Store test = new LC0981_Time_Based_Key_Value_Store();
+
+    }
+}
