720343507b27b8a8f0024342e8f3af17d22721e6
ChangSubKwak <paradise1732@gmail.com>
2022-10-07 16:40:14 +0900

    test: implement tes case - 981. Time Based Key Value Store

--- src/test/java/leetcode/LC0981_Time_Based_Key_Value_Store_Test.java
+++ src/test/java/leetcode/LC0981_Time_Based_Key_Value_Store_Test.java
@@ -9,7 +9,12 @@ import org.junit.jupiter.api.*;
 public class LC0981_Time_Based_Key_Value_Store_Test {
     @Test
     void test() {
-        LC0981_Time_Based_Key_Value_Store test = new LC0981_Time_Based_Key_Value_Store();
-
+        LC0981_Time_Based_Key_Value_Store.TimeMap test = new LC0981_Time_Based_Key_Value_Store.TimeMap();
+        test.set("foo", "bar", 1);
+        assertThat(test.get("foo", 1)).isEqualTo("bar");
+        assertThat(test.get("foo", 3)).isEqualTo("bar");
+        test.set("foo", "bar2", 4);
+        assertThat(test.get("foo", 4)).isEqualTo("bar2");
+        assertThat(test.get("foo", 5)).isEqualTo("bar2");
     }
 }
