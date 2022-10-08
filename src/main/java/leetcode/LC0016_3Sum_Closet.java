e85dc0ba50aa3edb48d787cba01e35225a69a07c
changsubkwak <paradise1732@gmail.com>
2022-10-08 15:48:36 +0900

    feat: solved - 16. 3Sum Closet

---
+++ src/main/java/leetcode/LC0016_3Sum_Closet.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.*;
+
+public class LC0016_3Sum_Closet {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int result = nums[0] + nums[1] + nums[2];
+        for(int i = 0; i < nums.length - 2; i++){
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if (Math.abs(sum - target) < Math.abs(result - target)) {
+                    result = sum;
+                }
+
+                if (sum < target) {
+                    left++;
+                    continue;
+                }
+                
+                right--;
+            }
+        }
+        return result;
+    }
+}
