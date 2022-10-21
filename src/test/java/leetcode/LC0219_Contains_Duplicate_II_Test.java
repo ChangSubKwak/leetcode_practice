31286ce493c1434c0fc0685587c381088823877e
ChangSubKwak <paradise1732@gmail.com>
2022-10-21 22:37:30 +0900

    test: implement test case - 219. Contains Duplicate II

---
+++ src/test/java/leetcode/LC0219_Contains_Duplicate_II_Test.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LC0219_Contains_Duplicate_II_Test {
+    LC0219_Contains_Duplicate_II test;
+    
+    @BeforeEach
+    void setUp() {
+        test = new LC0219_Contains_Duplicate_II();
+    }
+    
+    @Test
+    void minimumTotal() {
+        assertThat(test.containsNearbyDuplicate(new int[]{1, 2, 3, 1}, 3)).isTrue();
+        assertThat(test.containsNearbyDuplicate(new int[]{1, 0, 1, 1}, 1)).isTrue();
+        assertThat(test.containsNearbyDuplicate(new int[]{1, 2, 3, 1, 2, 3}, 2)).isTrue();
+    }
+}
