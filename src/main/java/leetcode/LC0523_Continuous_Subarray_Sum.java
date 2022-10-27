a9d8723489462591120a1ab2d057edd7f786bffd
ChangSubKwak <paradise1732@gmail.com>
2022-10-27 22:05:06 +0900

    feat: solved - 523. Continuous Subarray Sum

--- src/main/java/leetcode/LC0523_Continuous_Subarray_Sum.java
+++ src/main/java/leetcode/LC0523_Continuous_Subarray_Sum.java
@@ -1,7 +1,25 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LC0523_Continuous_Subarray_Sum {
     public boolean checkSubarraySum(int[] nums, int k) {
-        return true;
+        int sum = 0;
+        Map<Integer, Integer> leftIndex = new HashMap<>();
+        leftIndex.put(0, 0);
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (!leftIndex.containsKey(sum % k)) {
+                leftIndex.put(sum % k, i + 1);
+                continue;
+            }
+
+            if (leftIndex.get(sum % k) < i) {
+                return true;
+            }
+        }
+        return false;
     }
 }
