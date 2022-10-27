eefbeb4bb19a4b4f76504cea63b79d08a5a14ecb
ChangSubKwak <paradise1732@gmail.com>
2022-10-27 09:32:53 +0900

    fix: modify test case - 523. Continuous Subarray Sum

--- src/test/java/leetcode/LC0523_Continuous_Subarray_Sum_Test.java
+++ src/test/java/leetcode/LC0523_Continuous_Subarray_Sum_Test.java
@@ -8,7 +8,7 @@ public class LC0523_Continuous_Subarray_Sum_Test {
 	@Test
 	void test() {
 		LC0523_Continuous_Subarray_Sum test = new LC0523_Continuous_Subarray_Sum();
-		assertThat(test.checkSubarraySum(new int[]{23, 2, 4, 6, 7}, 6)).isTrue());
+		assertThat(test.checkSubarraySum(new int[]{23, 2, 4, 6, 7}, 6)).isTrue();
 		assertThat(test.checkSubarraySum(new int[]{23, 2, 4, 6, 7}, 6)).isTrue();
 		assertThat(test.checkSubarraySum(new int[]{23, 2, 4, 6, 7}, 6)).isFalse();
 	}
