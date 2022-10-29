e9cb0b8ac95a5eec06b0a8e33e3dd126504f2d3a
ChangSubKwak <paradise1732@gmail.com>
2022-10-29 10:08:25 +0900

    feat: solved - 49. Group Anagrams

--- src/main/java/leetcode/LC0049_Group_Anagrams.java
+++ src/main/java/leetcode/LC0049_Group_Anagrams.java
@@ -1,56 +1,34 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
-public class LC0049_GroupAnagrams {
-	public boolean isEqual(String s1, String s2) {
-		int[] c1 = new int[26];
-		int[] c2 = new int[26];
-		
-		if (s1.length() != s2.length()) return false;
-		for (int i = 0 ; i < s1.length() ; i++) {
-			c1[s1.charAt(i) - 'a']++;
-			c2[s2.charAt(i) - 'a']++;
-		}
-		for (int i = 0 ; i < 26 ; i++) {
-			if (c1[i] != c2[i]) return false;
-		}
-		
-		
-		return true;
-	}
-	
-    public List<List<String>> groupAnagrams(String[] strs) {
-    	List<List<String>> result = new ArrayList<>();
-    	List<String> org = new ArrayList<>();
-    	org.addAll(Arrays.asList(strs));
-    	
-    	List<String> list = new ArrayList<>();
-    	while(org.size() != 0) {
-    		String s = org.get(0);
-    		for (int i = 0 ; i < org.size() ; i++) {
-    			String t = org.get(i);
-    			if (isEqual(s, t)) {
-    				list.add(org.get(i));
-    				org.remove(i);
-    				i--;
-    			}
-    		}
-    		result.add(list);
-    		list = new ArrayList<>();
-    	}
-    	
-        return result;
-    }
-    
-	public static void main(String[] args) {
-		LC0049_GroupAnagrams t = new LC0049_GroupAnagrams();
-		List<List<String>> l = t.groupAnagrams(new String[]{"eat","tea","tan","ate","nat","bat"});
-		//List<List<String>> l = t.groupAnagrams(new String[]{""});
-		//List<List<String>> l = t.groupAnagrams(new String[]{"a"});
-		for (int i = 0 ; i < l.size() ; i++)
-			System.out.println(l.get(i));
-	}
+public class LC0049_Group_Anagrams {
+	public List<List<String>> groupAnagrams(String[] strs) {
+     int length = strs.length;
+
+     Map<String, List<String>> map = new HashMap<>();
+     for (String word : strs) {
+         char[] wordCharArray = word.toCharArray();
+         Arrays.sort(wordCharArray);
+         String sorted = new String(wordCharArray);
+
+         List<String> list = null;
+         if (!map.containsKey(sorted)) {
+             list = new ArrayList<>();
+             list.add(word);
+             map.put(sorted, list);
+             continue;
+         }
+         list = map.get(sorted);
+         list.add(word);
+         map.put(sorted, list);
+     }
+
+     List<List<String>> result = new ArrayList<>();
+     for (List<String> list : map.values()) {
+         result.add(list);
+     }
+
+     return result;
+ }
 }
