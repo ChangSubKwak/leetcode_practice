daabb8b3b532ef9af753c9a90a8241e45ed0272e
changsubkwak <paradise1732@gmail.com>
2022-10-30 23:54:17 +0900

    test: implement test case - 1293. Shorttest Path in a Grid with Obstacles Elimination

---
+++ src/test/java/leetcode/LC1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination_Test.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+public class LC1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination_Test {
+	@Test
+	void test() {
+		LC1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination test = new LC1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination();
+		assertThat(test.shortestPath(new int[][] {
+			{0, 0, 0},
+			{1, 1, 0},
+			{0, 0, 0},
+			{0, 1, 1},
+			{0, 0, 0}
+		}, 1)).isEqualTo(6);
+
+		assertThat(test.shortestPath(new int[][] {
+			{0, 1, 1},
+			{1, 1, 1},
+			{1, 0, 0}
+		}, 1)).isEqualTo(-1);
+	}
+}
