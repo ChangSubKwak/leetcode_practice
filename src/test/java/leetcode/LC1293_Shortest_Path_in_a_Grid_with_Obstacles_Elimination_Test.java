87b666fa73a7990d1f7c3f30b5b6ccac07f1a5aa
changsubkwak <paradise1732@gmail.com>
2022-10-31 00:44:33 +0900

    chore: modify class name

--- src/test/java/leetcode/LC1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination_Test.java
+++ src/test/java/leetcode/LC1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination_Test.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.*;
 public class LC1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination_Test {
 	@Test
 	void test() {
-		LC1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination test = new LC1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination();
+		LC1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination_X test = new LC1293_Shortest_Path_in_a_Grid_with_Obstacles_Elimination_X();
 		assertThat(test.shortestPath(new int[][] {
 			{0, 0, 0},
 			{1, 1, 0},
