b38c85266348f41e62aa507cb2e0365dace72534
changsubkwak <paradise1732@gmail.com>
2022-11-06 23:39:53 +0900

    fix: change from 'of' to 'Array.asList'

--- src/main/java/leetcode/LC0345_Reverse_Vowels_of_a_String.java
+++ src/main/java/leetcode/LC0345_Reverse_Vowels_of_a_String.java
@@ -1,14 +1,13 @@
 package leetcode;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class LC0345_Reverse_Vowels_of_a_String {
     public String reverseVowels(String s) {
         int start = 0;
         int end = s.length() - 1;
 
-        Set<Character> set = new HashSet<>(Set.of('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+        Set<Character> set = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
 
         StringBuilder sb = new StringBuilder(s);
         while (start < end) {
