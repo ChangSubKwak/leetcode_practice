e6923e4f8be2ff26a7bc2c91809163569cd99686
changsubkwak <paradise1732@gmail.com>
2022-11-13 22:19:43 +0900

    feat: solved(2nd) - 151. Reverse Words in a String

--- src/main/java/leetcode/LC0151_Reverse_Words_in_a_String.java
+++ src/main/java/leetcode/LC0151_Reverse_Words_in_a_String.java
@@ -1,7 +1,27 @@
 package leetcode;
 
+import java.util.*;
+import java.util.stream.*;
+
 public class LC0151_Reverse_Words_in_a_String {
+
     public String reverseWords(String s) {
+        List<String> splitWords = Arrays.stream(s.split(" "))
+            .collect(Collectors.toList());
+
+        StringBuilder result = new StringBuilder();
+        for (int i = splitWords.size() - 1; i >= 0; i--) {
+            String word = splitWords.get(i).trim();
+            result.append(word);
+
+            if (i != 0 && word.length() != 0) {
+                result.append(" ");
+            }
+        }
+
+        return result.toString().trim();
+    }
+    public String reverseWordsV1(String s) {
         String[] splitedWords = s.trim().split(" ");
         int length = splitedWords.length;
         
