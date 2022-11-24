deff426d471ad2540a2e51b08d17175d2e72a72d
ChangSubKwak <paradise1732@gmail.com>
2022-11-24 23:06:33 +0900

    test: (WIP) implement test code - 79. Word Search

---
+++ src/test/java/leetcode/LC0079_Word_Search_Test.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LC0079_Word_Search_Test {
+	@Test
+	void test() {
+		
+	}
+}
