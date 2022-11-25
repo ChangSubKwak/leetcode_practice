459cecc5920e6b0d6575a74933fd9c0a00d18fb3
ChangSubKwak <paradise1732@gmail.com>
2022-11-25 11:03:58 +0900

    test: implement test code - 79. Word Search

--- src/test/java/leetcode/LC0079_Word_Search_Test.java
+++ src/test/java/leetcode/LC0079_Word_Search_Test.java
@@ -7,6 +7,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LC0079_Word_Search_Test {
 	@Test
 	void test() {
-		
+		LC0079_Word_Search test = new LC0079_Word_Search();
+		assertThat(test.exist(new char[][]{
+			{'A', 'B', 'C', 'E'},
+			{'S', 'F', 'C', 'S'},
+			{'A', 'D', 'E', 'E'}
+		}, "ABCCED")).isTrue();
+		assertThat(test.exist(new char[][]{
+			{'A', 'B', 'C', 'E'},
+			{'S', 'F', 'C', 'S'},
+			{'A', 'D', 'E', 'E'}
+		}, "SEE")).isTrue();
+		assertThat(test.exist(new char[][]{
+			{'A', 'B', 'C', 'E'},
+			{'S', 'F', 'C', 'S'},
+			{'A', 'D', 'E', 'E'}
+		}, "ABCB")).isFalse();
 	}
 }
