5851aa6826180e367dd8c3bd6c1eefa934fd982d
changsubkwak <paradise1732@gmail.com>
2022-12-12 23:46:54 +0900

    test: change code - 70. Climbing Stairs

--- src/test/java/leetcode/LC0070_Climbing_Stairs_Test.java
+++ src/test/java/leetcode/LC0070_Climbing_Stairs_Test.java
@@ -5,15 +5,11 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
 public class LC0070_Climbing_Stairs_Test {
-	LC0070_Climbing_Stairs test;
-	
-	@BeforeEach
-	void setUp() {
-		test = new LC0070_Climbing_Stairs();
-	}
-	
+
 	@Test
 	void test() {
+		LC0070_Climbing_Stairs test = new LC0070_Climbing_Stairs();
+
 		assertThat(test.climbStairs(2)).isEqualTo(2);
 		assertThat(test.climbStairs(3)).isEqualTo(3);
 		assertThat(test.climbStairs(45)).isEqualTo(1836311903);
