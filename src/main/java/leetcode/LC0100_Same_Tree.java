ebff93179d5c7cb9a7b217e8d43c6e6a1508f435
ChangSubKwak <paradise1732@gmail.com>
2023-01-10 19:50:06 +0900

    feat: solved - 100. Same Tree

--- src/main/java/leetcode/LC0100_Same_Tree.java
+++ src/main/java/leetcode/LC0100_Same_Tree.java
@@ -1,7 +1,28 @@
 package leetcode;
 
-public class LC0100_SameTree {
-    public boolean isSameTree1(TreeNode p, TreeNode q) {
+public class LC0100_Same_Tree {
+
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		if (p == null && q != null) {
+		 return false;
+		}
+
+		if (p != null && q == null) {
+		 return false;
+		}
+
+		if (p == null && q == null) {
+		 return true;
+		}
+
+		if (p.val != q.val) {
+		 return false;
+		}
+
+		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+	 }
+
+    public boolean isSameTreeV1(TreeNode p, TreeNode q) {
     	if (p == null && q == null)
     		return true;
     	
@@ -11,10 +32,10 @@ public class LC0100_SameTree {
     	if (p != null && q == null)
     		return false;
     	
-    	if (!isSameTree1(p.left, q.left)) return false;
+    	if (!isSameTreeV1(p.left, q.left)) return false;
     	if (p.val != q.val)
     		return false;
-    	if (!isSameTree1(p.right, q.right)) return false;
+    	if (!isSameTreeV1(p.right, q.right)) return false;
         
     	return true;
     }
