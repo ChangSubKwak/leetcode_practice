e34f780bdfab89f38f74e420273f605e0f9878f9
changsubkwak <paradise1732@gmail.com>
2023-01-11 23:38:49 +0900

    test: implement test case - Minimum Time to Collect All Apples in a Tree

--- src/main/java/leetcode/LC0797_All_Paths_From_Source_to_Target.java
+++ src/main/java/leetcode/LC0797_All_Paths_From_Source_to_Target.java
@@ -4,36 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LC0797_All_Paths_From_Source_to_Target {
-    private List<List<Integer>> result;
-    private boolean[] visited;
-
-    private void recur(int[][] graph, int node, List<Integer> path) {
-        if (node == graph.length - 1) {
-            path.add(node);
-            result.add(new ArrayList<>(path));
-            path.remove(path.size() - 1);
+    List<List<Integer>> result;
+    int size;
+    
+    private void recursive(int[][] graph, int row, List<Integer> list) {
+        if (row == size - 1) {
+            result.add(new ArrayList<Integer>(list));
             return;
         }
-
-        visited[node] = true;
-        path.add(node);
-        for (int i = 0; i < graph[node].length; i++) {
-            int nextNode = graph[node][i];
-            if (visited[nextNode]) {
-                continue;
-            }
-            recur(graph, nextNode, path);
+        
+        for(int i = 0; i < graph[row].length; i++) {
+            list.add(graph[row][i]);
+            recursive(graph, graph[row][i], list);
+            list.remove(list.size() - 1);
         }
-        path.remove(path.size() - 1);
-        visited[node] = false;
     }
-
+    
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        int col = graph[0].length;
+        size = graph.length;
+        
         result = new ArrayList<>();
-        visited = new boolean[graph.length];
-
-        recur(graph, 0, new ArrayList<>());
-
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        for (int i = 0; i < col; i++) {
+            list.add(graph[0][i]);
+            recursive(graph, graph[0][i], list);
+            list.remove(list.size() - 1);
+        }
+        
         return result;
     }
 }
