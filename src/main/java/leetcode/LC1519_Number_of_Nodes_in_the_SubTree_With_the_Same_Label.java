52fc0b9e488d68480616a59f544259f9a3be4c6a
ChangSubKwak <paradise1732@gmail.com>
2023-01-13 18:50:00 +0900

    feat: (WIP) solving - 1519. Number of Nodes in the Sub-Tree With the Same Label

---
+++ src/main/java/leetcode/LC1519_Number_of_Nodes_in_the_SubTree_With_the_Same_Label.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC1519_Number_of_Nodes_in_the_SubTree_With_the_Same_Label {
+    public int[] countSubTrees(int n, int[][] edges, String labels) {
+        return null;
+    }
+}
