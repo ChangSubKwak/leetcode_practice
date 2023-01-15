4c801a9cbd7b5c5d1b3430c347921218b70f7132
changsubkwak <paradise1732@gmail.com>
2023-01-15 22:38:28 +0900

    test: implement test case - 2421. Number of Good Paths

---
+++ src/test/java/leetcode/LC2421_Number_of_Good_Paths_Test.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+class LC2421_Number_of_Good_Paths_Test {
+    LC2421_Number_of_Good_Paths test = new LC2421_Number_of_Good_Paths();
+
+    @Test
+    void exampleTest() {
+        assertThat(test.numberOfGoodPaths(new int[]{1, 3, 2, 1, 3}, new int[][]{{0, 1}, {0, 2}, {2, 3}, {2, 4}})).isEqualTo(6);
+        assertThat(test.numberOfGoodPaths(new int[]{1, 1, 2, 2, 3}, new int[][]{{0, 1}, {1, 2}, {2, 3}, {2, 4}})).isEqualTo(7);
+        //assertThat(test.numberOfGoodPaths(new int[]{1}, new int[][]{{}})).isEqualTo(1);
+        // assertThat(test.numberOfGoodPaths(new int[]{1}, new int[][]{{}})).isEqualTo(1);
+    }
+}
