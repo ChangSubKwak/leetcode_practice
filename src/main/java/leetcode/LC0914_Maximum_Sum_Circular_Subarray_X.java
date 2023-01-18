8d621c64cee12e7b61b543929d4e9a42760e0192
ChangSubKwak <paradise1732@gmail.com>
2023-01-18 22:39:26 +0900

    feat: solved - 914. Maximum Sum Circular Subarray

---
+++ src/main/java/leetcode/LC0914_Maximum_Sum_Circular_Subarray_X.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class LC0914_Maximum_Sum_Circular_Subarray_X {
+    public int maxSubarraySumCircular(int[] nums) {
+        int total = 0, maxSum = nums[0], curMax = 0, minSum = nums[0], curMin = 0;
+        for (int num : nums) {
+            curMax = Math.max(curMax + num, num);
+            maxSum = Math.max(maxSum, curMax);
+            curMin = Math.min(curMin + num, num);
+            minSum = Math.min(minSum, curMin);
+            total += num;
+        }
+        return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;
+    }
+}
