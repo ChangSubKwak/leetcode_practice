99df0bef9c30a1a3495d077a2f47493c0ee5917a
ChangSubKwak <paradise1732@gmail.com>
2023-01-20 11:03:28 +0900

    feat: solved - 974. Subarray Sums Divisible by K

--- src/main/java/leetcode/LC0974_Subarray_Sums_Divisible_by_K.java
+++ src/main/java/leetcode/LC0974_Subarray_Sums_Divisible_by_K.java
@@ -2,6 +2,23 @@ package leetcode;
 
 public class LC0974_Subarray_Sums_Divisible_by_K {
     public int subarraysDivByK(int[] nums, int k) {
-        return 0;
+        int[] modCount =  new int[k];
+
+        int remainder = 0;
+        for (int num : nums) {
+            remainder += (num % k);
+            if (remainder < 0) {
+                remainder += k;
+            }
+            remainder %= k;
+            modCount[remainder]++;
+        }
+
+        int count = 0;
+        for (int i = 0; i < k; i++) {
+            count += modCount[i] * (modCount[i] - 1) / 2;
+        }
+
+        return count + modCount[0];
     }
 }
