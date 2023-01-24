dc06630001132673fabf16718b62122a3a2adb40
paradise1732 <paradise1732@gmail.com>
2023-01-24 19:13:06 +0900

    feat: solved - 997. Find the Town Judge

--- src/main/java/leetcode/LC0997_Find_the_Town_Judge.java
+++ src/main/java/leetcode/LC0997_Find_the_Town_Judge.java
@@ -1,15 +1,35 @@
 package leetcode;
 
-public class LC0997_Find_the_Town_Judge_X {
-    public int findJudge(int N, int[][] trust) {
-        int[] count = new int[N+1];
-        for (int[] t: trust) {
-            count[t[0]]--;
-            count[t[1]]++;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class LC0997_Find_the_Town_Judge {
+    public int findJudge(int n, int[][] trust) {
+        if (n ==1 && trust.length == 0) {
+            return 1;
         }
-        for (int i = 1; i <= N; ++i) {
-            if (count[i] == N - 1) return i;
+    
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+    
+        for (int i = 1; i <= n; i++) {
+            set.add(i);
         }
-        return -1;
+    
+        for (int i = 0; i < trust.length; i++) {
+            set.remove(trust[i][0]);
+            map.put(trust[i][1], map.getOrDefault(trust[i][1], 0) + 1);
+        }
+    
+        if (set.size() != 1) {
+            return -1;
+        }
+    
+        int candidate = set.iterator().next();
+        int trusted = map.get(candidate);
+    
+        return trusted == n - 1 ? candidate : -1;
     }
 }
