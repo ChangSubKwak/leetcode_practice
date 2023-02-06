5f67557d288e174667bfbdae9c7952b043b9bf81
ChangSubKwak <paradise1732@gmail.com>
2023-02-06 19:06:52 +0900

    feat: solved - 1470. Shuffle the Array

---
+++ src/main/java/leetcode/LC1470_Shuffle_the_Array.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class LC1470_Shuffle_the_Array {
+    public int[] shuffle(int[] nums, int n) {
+        int[] result = new int[2 * n];
+        for (int i = 0; i < n; i++) {
+            result[2 * i] = nums[i];
+            result[2 * i + 1] = nums[i + n];
+        }
+        return result;
+    }
+}
