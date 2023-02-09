62e171ce29068d51869db4bd009db257788565d4
changsubkwak <paradise1732@gmail.com>
2023-02-10 00:58:44 +0900

    feat: solved - 2306. Naming a Company

--- src/test/java/leetcode/LC2306_Naming_a_Company_Test.java
+++ src/test/java/leetcode/LC2306_Naming_a_Company_Test.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.*;
 class LC2306_Naming_a_Company_Test {
     @Test
     void test() {
-        LC2306_Naming_a_Company test = new LC2306_Naming_a_Company();
+        LC2306_Naming_a_Company_X test = new LC2306_Naming_a_Company_X();
         assertThat(test.distinctNames(new String[]{"coffee", "donuts", "time", "toffee"})).isEqualTo(6);
         assertThat(test.distinctNames(new String[]{"lack", "back"})).isEqualTo(0);
     }
