f25691727970e42f74ab65d502e465f9fb31b8e7
ChangSubKwak <paradise1732@gmail.com>
2023-02-21 09:46:55 +0900

    feat: solved - 540. Single Element in a Sorted Array

--- src/main/java/leetcode/LC0540_Single_Element_in_a_Sorted_Array.java
+++ src/main/java/leetcode/LC0540_Single_Element_in_a_Sorted_Array.java
@@ -1,18 +1,16 @@
 package leetcode;
 
 public class LC0540_Single_Element_in_a_Sorted_Array {
+    // TODO : Next time, I'll solve it through binary search.
     public int singleNonDuplicate(int[] nums) {
-        int length = nums.length;
-        if (length == 1) {
-            return nums[0];
-        }
-        
-        for (int i = 0; i < length; i+=2) {
-            if (i + 1 < length && nums[i] != nums[i+1]) {
+        int i = 0;
+        while (i < nums.length - 1) {
+            if (nums[i] != nums[i + 1]) {
                 return nums[i];
             }
+            i += 2;
         }
-        
-        return nums[length - 1];
+
+        return nums[nums.length - 1];
     }
 }
