c49ef88080d417d1ab3dcac9737ad89236af292f
ChangSubKwak <paradise1732@gmail.com>
2023-03-04 16:55:06 +0900

    test: implement test case - KMP Algorithm

--- src/main/java/util/KMP_Algorithm.java
+++ src/main/java/util/KMP_Algorithm.java
@@ -25,4 +25,29 @@ public class KMP_Algorithm {
 
         return pi;
     }
+
+    public int strStr(String haystack, String needle) {
+        List<Integer> pi = getPi(needle);
+
+        int lengthOfHaystack = haystack.length();
+        int lengthOfPattern = needle.length();
+
+        int lengthOfMatched = 0;
+
+        for (int i = 0 ; i < lengthOfHaystack ; i++) {
+            while (lengthOfMatched > 0 && haystack.charAt(i) != needle.charAt(lengthOfMatched)) {
+                lengthOfMatched = pi.get(lengthOfMatched - 1);
+            }
+
+            boolean isMatched = haystack.charAt(i) == needle.charAt(lengthOfMatched);
+            if (isMatched && lengthOfMatched == lengthOfPattern - 1) {
+                return i - lengthOfPattern + 1;
+            }
+
+            if (isMatched) {
+                lengthOfMatched++;
+            }
+        }
+        return -1;
+    }
 }
--- src/test/java/util/KMP_Algorithm_Test.java
+++ src/test/java/util/KMP_Algorithm_Test.java
@@ -14,4 +14,14 @@ class KMP_Algorithm_Test {
         assertThat(kmpAlgorithm.getPi("ABAABAB")).isEqualTo(Arrays.asList(0, 0, 1, 1, 2, 3, 2));
         assertThat(kmpAlgorithm.getPi("AABAA")).isEqualTo(Arrays.asList(0, 1, 0, 1, 2));
     }
+
+    @Test
+    void testStrStr() {
+        KMP_Algorithm kmpAlgorithm = new KMP_Algorithm();
+
+        assertThat(kmpAlgorithm.strStr("AAAABBBBCCCC", "AAAA")).isEqualTo(0);
+        assertThat(kmpAlgorithm.strStr("AAAABBBBCCCC", "BBBB")).isEqualTo(4);
+        assertThat(kmpAlgorithm.strStr("AAAABBBBCCCC", "CCCC")).isEqualTo(8);
+        assertThat(kmpAlgorithm.strStr("AAAABBBBCCCC", "AACB")).isEqualTo(-1);
+    }
 }
