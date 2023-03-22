09d4b0fe1b6c4f428dcd239644b0415ec547b914
ChangSubKwak <paradise1732@gmail.com>
2023-03-22 11:44:06 +0900

    test: implement test case - 2492. Minimum Score of a Path Between Two Cities

---
+++ src/test/java/leetcode/LC2492_Minimum_Score_of_a_Path_Between_Two_Cities_Test.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+class LC2492_Minimum_Score_of_a_Path_Between_Two_Cities_Test {
+    @Test
+    void test() {
+        LC2492_Minimum_Score_of_a_Path_Between_Two_Cities test = new LC2492_Minimum_Score_of_a_Path_Between_Two_Cities();
+    }
+}
