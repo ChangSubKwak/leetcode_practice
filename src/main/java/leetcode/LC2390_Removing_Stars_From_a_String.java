bfb70c5d802a822dc7e08c99cc3dcf4682b23de7
ChangSubKwak <paradise1732@gmail.com>
2023-04-12 09:11:58 +0900

    feat: solved - 2390. Removing Stars From a String

--- src/main/java/leetcode/LC2390_Removing_Stars_From_a_String.java
+++ src/main/java/leetcode/LC2390_Removing_Stars_From_a_String.java
@@ -2,6 +2,27 @@ package leetcode;
 
 public class LC2390_Removing_Stars_From_a_String {
     public String removeStars(String s) {
-        return "";
+        StringBuilder result = new StringBuilder();
+        int starLength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '*') {
+                starLength++;
+                continue;
+            }
+
+            if (starLength > 0) {
+                result.setLength(result.length() - starLength);
+                starLength = 0;
+            }
+
+            result.append(c);
+        }
+
+        if (starLength > 0) {
+            result.setLength(result.length() - starLength);
+        }
+
+        return result.toString();
     }
 }
