4b33effbd36bc0b3b55b637d66a4bdc72cd9f36c
changsubkwak <paradise1732@gmail.com>
2023-05-14 01:40:39 +0900

    feat: solved - 2366. Count Ways To Build Good Strings

--- src/main/java/leetcode/LC2466_Count_Ways_To_Build_Good_Strings.java
+++ src/main/java/leetcode/LC2466_Count_Ways_To_Build_Good_Strings.java
@@ -2,6 +2,27 @@ package leetcode;
 
 public class LC2466_Count_Ways_To_Build_Good_Strings {
     public int countGoodStrings(int low, int high, int zero, int one) {
-        return 8;
+        int[] dp = new int[high + 1];
+        dp[0] = 1;
+        int mod = 1_000_000_007;
+
+        for (int i = 1; i <= high; i++) {
+            if (i >= zero) {
+                dp[i] += dp[i - zero];
+            }
+
+            if (i >= one) {
+                dp[i] += dp[i - one];
+            }
+            dp[i] %= mod;
+        }
+
+        int answer = 0;
+        for (int i = low; i <= high; i++) {
+            answer += dp[i];
+            answer %= mod;
+        }
+
+        return answer;
     }
 }
