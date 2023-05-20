3643e48e7126f94a08834943fa0352319e941b4a
changsubkwak <paradise1732@gmail.com>
2023-05-21 01:45:03 +0900

    feat: solving - 399. Evaluate Division

---
+++ src/main/java/leetcode/LC0399_Evaluate_Division.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+import java.util.List;
+
+public class LC0399_Evaluate_Division {
+	public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+
+		return new double[] {0.0, 0.0};
+	}
+}
--- src/test/java/leetcode/LC0399_Evaluate_Division_Test.java
+++ src/test/java/leetcode/LC0399_Evaluate_Division_Test.java
@@ -27,5 +27,37 @@ public class LC0399_Evaluate_Division_Test {
         )).isEqualTo(new double[]{
             6.0, 5.0, -1.0, 1.0, -1.0
         });
+
+        assertThat(test.calcEquation(
+            Arrays.asList(
+                Arrays.asList("a", "b"),
+                Arrays.asList("b", "c"),
+                Arrays.asList("bc", "cd")
+            ),
+            new double[] {1.5, 2.5, 5.0},
+            Arrays.asList(
+                Arrays.asList("a", "c"),
+                Arrays.asList("c", "b"),
+                Arrays.asList("bc", "cd"),
+                Arrays.asList("cd", "bc")
+            )
+        )).isEqualTo(new double[]{
+            3.75, 0.4, 5.0, 0.2
+        });
+
+        assertThat(test.calcEquation(
+            Arrays.asList(
+                Arrays.asList("a", "b")
+            ),
+            new double[] {0.5},
+            Arrays.asList(
+                Arrays.asList("a", "c"),
+                Arrays.asList("b", "a"),
+                Arrays.asList("a", "c"),
+                Arrays.asList("x", "y")
+            )
+        )).isEqualTo(new double[]{
+            0.5, 2.0, -1.0, -1.0
+        });
     }
 }
