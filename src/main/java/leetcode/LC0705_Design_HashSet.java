04e7ea7bac36ef576646bf5b75d3b50f55b3cfed
ChangSubKwak <paradise1732@gmail.com>
2023-05-30 11:14:32 +0900

    feat: solved - 705. Design HashSet

--- src/main/java/leetcode/LC0705_Design_HashSet.java
+++ src/main/java/leetcode/LC0705_Design_HashSet.java
@@ -4,23 +4,33 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class LC0705_Design_HashSet {
-	class MyHashSet {
-	    Set<Integer> set;
+	static class MyHashSet {
+		private int HASH_SIZE = 1000_000 / 32 + 1;
+    	private int[] values;
 
-	    public MyHashSet() {
-	        set = new HashSet<>();
-	    }
-	    
-	    public void add(int key) {
-	        set.add(key);
-	    }
-	    
-	    public void remove(int key) {
-	        set.remove(key);
-	    }
-	    
-	    public boolean contains(int key) {
-	        return set.contains(key);
-	    }
+		public MyHashSet() {
+			values = new int[HASH_SIZE];
+		}
+
+		public void add(int key) {
+			int arrayIndex = (key / 32);
+			int indexBit = 1 << (key % 32);
+			values[arrayIndex] |= indexBit;
+		}
+
+		public void remove(int key) {
+			int arrayIndex = (key / 32);
+			int indexBit = 1 << (key % 32);
+			values[arrayIndex] &= (~indexBit);
+		}
+
+		public boolean contains(int key) {
+			int arrayIndex = (key / 32);
+			int indexBit = 1 << (key % 32);
+			if ((values[arrayIndex] & indexBit) != 0x00) {
+				return true;
+			}
+			return false;
+		}
 	}
 }
