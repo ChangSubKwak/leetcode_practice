f53726e221407d52c3e965a8e76092ea96ea26ff
changsubkwak <paradise1732@gmail.com>
2023-05-31 23:57:11 +0900

    test: (WIP) implementing test case - 1396. Design Underground System

---
+++ src/test/java/leetcode/LC1396_Design_Underground_System_Test.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+class LC1396_Design_Underground_System_Test {
+    @Test
+    void test() {
+        LC1396_Design_Underground_System.UndergroundSystem test = new LC1396_Design_Underground_System.UndergroundSystem();
+
+    }
+}
