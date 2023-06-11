c764b5a67a2c312f390dc37fd62f335808d33124
changsubkwak <paradise1732@gmail.com>
2023-06-12 01:22:17 +0900

    test: add test case - 1146. Snapshot Array

--- src/test/java/leetcode/LC1146_Snapshot_Array_Test.java
+++ src/test/java/leetcode/LC1146_Snapshot_Array_Test.java
@@ -11,7 +11,26 @@ class LC1146_Snapshot_Array_Test {
         test.set(0, 5);
         test.snap();
         test.set(0, 6);
-
         assertThat(test.get(0, 0)).isEqualTo(5);
+
+        test = new LC1146_Snapshot_Array.SnapshotArray(1);
+        test.set(0, 15);
+        test.snap();
+        test.snap();
+        test.snap();
+        assertThat(test.get(0, 2)).isEqualTo(15);
+        test.snap();
+        test.snap();
+        assertThat(test.get(0, 0)).isEqualTo(15);
+
+        test = new LC1146_Snapshot_Array.SnapshotArray(2);
+        test.snap();
+        assertThat(test.get(1, 0)).isEqualTo(0);
+        assertThat(test.get(0, 0)).isEqualTo(0);
+        test.set(1, 8);
+        assertThat(test.get(1, 0)).isEqualTo(0);
+        test.set(0, 20);
+        assertThat(test.get(0, 0)).isEqualTo(0);
+        test.set(0, 7);
     }
 }
