8b8e850a0b5a6b66bcc401f9b97211ffb8508e87
changsubkwak <paradise1732@gmail.com>
2023-06-25 23:53:00 +0900

    feat: (WIP) solving - 1575. Count All Possible Routes

---
+++ src/main/java/leetcode/LC1575_Count_All_Possible_Routes.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+import java.util.*;
+
+public class LC1575_Count_All_Possible_Routes {
+    public int countRoutes(int[] locations, int start, int finish, int fuel) {
+        return 0;
+    }
+}
