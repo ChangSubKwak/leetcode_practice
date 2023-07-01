b7c42d795b62be328e8980498f087e59c1a80eb2
changsubkwak <paradise1732@gmail.com>
2023-07-01 23:45:07 +0900

    feat: solved - 2305. Fair Distribution of Cookies

--- src/main/java/leetcode/LC2305_Fair_Distribution_of_Cookies.java
+++ src/main/java/leetcode/LC2305_Fair_Distribution_of_Cookies.java
@@ -1,7 +1,24 @@
 package leetcode;
 
 public class LC2305_Fair_Distribution_of_Cookies {
+    private int recur(int[] cookies, int start, int k, int[] values, int min) {
+        if (start == cookies.length) {
+            int max = 0;
+            for (int value : values) {
+                max = Math.max(max, value);
+            }
+            return Math.min(min, max);
+        }
+
+        for (int i = 0; i < k; i++) {
+            values[i] += cookies[start];
+            min = recur(cookies, start + 1, k, values, min);
+            values[i] -= cookies[start];
+        }
+        return min;
+    }
+
     public int distributeCookies(int[] cookies, int k) {
-        return 0;
+        return recur(cookies, 0, k, new int[k], Integer.MAX_VALUE);
     }
 }
