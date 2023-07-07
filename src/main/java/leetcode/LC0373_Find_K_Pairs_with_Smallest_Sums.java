4bdd34020a4cfd4299a4dced319fda5fafdbeefd
ChangSubKwak <paradise1732@gmail.com>
2023-07-07 11:25:43 +0900

    feat: (WIP) solving - 373. Find K Pairs with Smallest Sums

---
+++ src/main/java/leetcode/LC0373_Find_K_Pairs_with_Smallest_Sums.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+import java.util.List;
+
+public class LC0373_Find_K_Pairs_with_Smallest_Sums {
+    public List<List<Integer>> kSmallestPairs(int[] ints, int[] ints1, int i) {
+        return null;
+    }
+}
