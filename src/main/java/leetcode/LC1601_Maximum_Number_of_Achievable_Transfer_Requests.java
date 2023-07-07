af7457a900ad88135edb2149d382ea557a1eec2a
ChangSubKwak <paradise1732@gmail.com>
2023-07-07 11:26:43 +0900

    fix: merge work - 1601. Maximum Number of Achievable Transfer Requests

--- src/main/java/leetcode/LC1601_Maximum_Number_of_Achievable_Transfer_Requests.java
+++ src/main/java/leetcode/LC1601_Maximum_Number_of_Achievable_Transfer_Requests.java
@@ -1,11 +1,8 @@
 package leetcode;
 
 public class LC1601_Maximum_Number_of_Achievable_Transfer_Requests {
-<<<<<<< HEAD
+
     private int answer;
-=======
-    private int answer = 0;
->>>>>>> e3e0df3 (feat: solved - 1601. Maximum Number of Achievable Transfer Requests)
 
     private void maxRequest(int[][] requests, int[] indegree, int n, int index, int count) {
         if (index == requests.length) {
@@ -30,10 +27,7 @@ public class LC1601_Maximum_Number_of_Achievable_Transfer_Requests {
     }
 
     public int maximumRequests(int n, int[][] requests) {
-<<<<<<< HEAD
         answer = 0;
-=======
->>>>>>> e3e0df3 (feat: solved - 1601. Maximum Number of Achievable Transfer Requests)
         int[] indegree = new int[n];
         maxRequest(requests, indegree, n, 0, 0);
 
