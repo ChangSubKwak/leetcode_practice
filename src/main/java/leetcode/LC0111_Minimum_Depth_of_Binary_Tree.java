eaa67219664fa6b9f962d2ae40a85dada1b93dcc
changsubkwak <paradise1732@gmail.com>
2023-07-12 23:58:30 +0900

    feat: (WIP) solving - 111. Minimum Depth of Binary Tree

--- src/main/java/leetcode/LC0111_Minimum_Depth_of_Binary_Tree.java
+++ src/main/java/leetcode/LC0111_Minimum_Depth_of_Binary_Tree.java
@@ -1,27 +1,8 @@
 package leetcode;
 
 public class LC0111_Minimum_Depth_of_Binary_Tree {
-    private int recur(TreeNode root, int depth) {
-        if (root.left == null && root.right == null) {
-            return depth;
-        }
-
-        int min = 100001;
-        if (root.left != null) {
-            min = Math.min(min, recur(root.left, depth + 1));
-        }
-
-        if (root.right != null) {
-            min = Math.min(min, recur(root.right, depth + 1));
-        }
-
-        return min;
-    }
 
     public int minDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        return recur(root, 1);
+        return 0;
     }
 }
