5443989c856cbf5b8e82add85b3b1b4ff443dfad
changsubkwak <paradise1732@gmail.com>
2023-07-17 22:05:58 +0900

    test: add test case - 445. Add Two Numbers II

--- src/test/java/leetcode/LC0445_Add_Two_Numbers_II_Test.java
+++ src/test/java/leetcode/LC0445_Add_Two_Numbers_II_Test.java
@@ -25,5 +25,25 @@ public class LC0445_Add_Two_Numbers_II_Test {
         result.next.next.next = new ListNode(7);
 
         assertThat(test.addTwoNumbers(l1, l2)).isEqualTo(result);
+
+        l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        result = new ListNode(8);
+        result.next = new ListNode(0);
+        result.next.next = new ListNode(7);
+
+        assertThat(test.addTwoNumbers(l1, l2)).isEqualTo(result);
+
+        l1 = new ListNode(0);
+        l2 = new ListNode(0);
+        result = new ListNode(0);
+
+        assertThat(test.addTwoNumbers(l1, l2)).isEqualTo(result);
     }
 }
