32a221702bed1d0a65bd381093fde960f268b047
ChangSubKwak <paradise1732@gmail.com>
2023-07-21 17:25:59 +0900

    feat: solved - 146. LRU Cache

--- src/main/java/leetcode/LC0146_LRU_Cache.java
+++ src/main/java/leetcode/LC0146_LRU_Cache.java
@@ -4,21 +4,24 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class LC0146_LRU_Cache {
-    private LinkedHashMap<Integer, Integer> map;
-    @SuppressWarnings("serial")
-	public LC0146_LRU_Cache(int capacity) {
-        map = new LinkedHashMap<Integer, Integer> (capacity, 0.75f, true){
-            protected boolean removeEldestEntry(Map.Entry eldest) {
-                return size() > capacity;
-            }
-        };        
-    }
-    
-    public int get(int key) {
-        return map.getOrDefault(key, -1);
-    }
 
-    public void put(int key, int value) {
-        map.put(key, value);
+	static class LRUCache {
+        private LinkedHashMap<Integer, Integer> map;
+
+        public LRUCache(int capacity) {
+            map = new LinkedHashMap<>(capacity, 0.75f, true) {
+                protected boolean removeEldestEntry(Map.Entry eldest) {
+                    return size() > capacity;
+                }
+            };
+        }
+
+        public int get(int key) {
+            return map.getOrDefault(key, -1);
+        }
+
+        public void put(int key, int value) {
+            map.put(key, value);
+        }
     }
 }
