09f203db42d0af975f8c5e349f8364dc2c5a8e32
changsubkwak <paradise1732@gmail.com>
2023-07-31 23:50:48 +0900

    feat: (WIP) solving - 808. Soup Servings

--- src/main/java/leetcode/LC0808_Soup_Servings.java
+++ src/main/java/leetcode/LC0808_Soup_Servings.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LC0808_Soup_Servings {
-    private double calculateDP(int i, int j, Map<Integer, Map<Integer, Double>> dp) {
-        return 0.625;
+    public double soupServings(int i) {
+        return 0.62500;
     }
 }
