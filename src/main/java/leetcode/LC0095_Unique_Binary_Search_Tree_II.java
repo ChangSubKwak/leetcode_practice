53676911e73ddfefff8d600e5f93f55bc2585dd4
changsubkwak <paradise1732@gmail.com>
2023-08-05 23:33:06 +0900

    feat: (WIP) solving - 95. Unique Binary Search Tree II

---
+++ src/main/java/leetcode/LC0095_Unique_Binary_Search_Tree_II.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC0095_Unique_Binary_Search_Tree_II {
+    public List<TreeNode> generateTrees(int n) {
+        return null;
+    }
+}
