536fe0b5aa575d564876a6fb455ae69907f99709
changsubkwak <paradise1732@gmail.com>
2023-09-02 22:11:36 +0900

    feat: newly solved - 345. Reverse Vowels of a String

--- src/main/java/leetcode/LC0345_Reverse_Vowels_of_a_String.java
+++ src/main/java/leetcode/LC0345_Reverse_Vowels_of_a_String.java
@@ -4,27 +4,21 @@ import java.util.*;
 
 public class LC0345_Reverse_Vowels_of_a_String {
     public String reverseVowels(String s) {
-        int start = 0;
-        int end = s.length() - 1;
-
-        Set<Character> set = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+        List<Integer> position = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U').contains(s.charAt(i))) {
+                position.add(i);
+            }
+        }
 
         StringBuilder sb = new StringBuilder(s);
-        while (start < end) {
-            while(start < s.length() && !set.contains(s.charAt(start))) {
-                start++;
-            }
+        for (int i = 0; i < position.size() / 2; i++) {
+            int start = position.get(i);
+            int end   = position.get(position.size() - 1 - i);
 
-            while(end >= 0 && !set.contains(s.charAt(end))) {
-                end--;
-            }
-
-            if (0 <= start && start < s.length() && 0 <= end && end < s.length()) {
-                sb.setCharAt(start, s.charAt(end));
-                sb.setCharAt(end, s.charAt(start));
-                start++;
-                end--;
-            }
+            char t = sb.charAt(start);
+            sb.setCharAt(start, sb.charAt(end));
+            sb.setCharAt(end, t);
         }
 
         return sb.toString();
