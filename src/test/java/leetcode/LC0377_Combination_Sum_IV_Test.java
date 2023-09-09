12bf051ad29d874eefa730f667853ea2afecd76e
changsubkwak <paradise1732@gmail.com>
2023-09-09 23:10:45 +0900

    test: remove @BeforeEach and declare main class(LC0377_Combination_Sum_IV)

--- src/test/java/leetcode/LC0377_Combination_Sum_IV_Test.java
+++ src/test/java/leetcode/LC0377_Combination_Sum_IV_Test.java
@@ -6,15 +6,10 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LC0377_Combination_Sum_IV_Test {
-    LC0377_Combination_Sum_IV test;
-
-    @BeforeEach
-    void setUp() {
-        test = new LC0377_Combination_Sum_IV();
-    }
 
     @Test
     void test() {
+        LC0377_Combination_Sum_IV test = new LC0377_Combination_Sum_IV();
         assertThat(test.combinationSum4(new int[] {1, 2, 3}, 4)).isEqualTo(7);
         assertThat(test.combinationSum4(new int[] {9}, 3)).isEqualTo(0);
     }
