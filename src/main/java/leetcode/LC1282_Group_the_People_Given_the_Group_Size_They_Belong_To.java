b53ba43b05ded903f1af307aea508349836ba09d
changsubkwak <paradise1732@gmail.com>
2023-09-12 01:17:03 +0900

    feat: solved - 1282. Group the People Given the Group Size They Belong To

--- src/main/java/leetcode/LC1282_Group_the_People_Given_the_Group_Size_They_Belong_To.java
+++ src/main/java/leetcode/LC1282_Group_the_People_Given_the_Group_Size_They_Belong_To.java
@@ -1,9 +1,40 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LC1282_Group_the_People_Given_the_Group_Size_They_Belong_To {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < groupSizes.length; i++) {
+            List<Integer> list = new ArrayList<>();
+            if (map.containsKey(groupSizes[i])) {
+                list = map.get(groupSizes[i]);
+            }
+            list.add(i);
+            map.put(groupSizes[i], list);
+        }
+
+        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+            int key   = entry.getKey();
+            List<Integer> value = entry.getValue();
+
+            int j = 1;
+            List<Integer> part = new ArrayList<>();
+            while (j <= value.size()) {
+                part.add(value.get(j - 1));
+                if (j % key == 0) {
+                    result.add(part);
+                    part = new ArrayList<>();
+                }
+                j++;
+            }
+        }
+
+        return result;
     }
 }
