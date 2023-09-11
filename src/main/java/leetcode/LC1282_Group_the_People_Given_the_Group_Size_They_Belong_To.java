25ed35aa29fc800e27fcdefc4197035a5beb0a2c
changsubkwak <paradise1732@gmail.com>
2023-09-11 23:49:21 +0900

    feat: (WIP) solving - 1282. Group the People Given the Group Size They Belong To

---
+++ src/main/java/leetcode/LC1282_Group_the_People_Given_the_Group_Size_They_Belong_To.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+import java.util.List;
+
+public class LC1282_Group_the_People_Given_the_Group_Size_They_Belong_To {
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
+        return null;
+    }
+}
