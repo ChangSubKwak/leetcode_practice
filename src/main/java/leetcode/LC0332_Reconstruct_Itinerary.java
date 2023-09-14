3e28d90667dd4cf5315916fc20617b9a95c94f17
cskwak <changsubkwak@nongshim.co.kr>
2023-09-14 21:06:05 +0900

    feat: (WIP) solving - 332. Reconstruct Itinerary

---
+++ src/main/java/leetcode/LC0332_Reconstruct_Itinerary.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+import java.util.List;
+
+public class LC0332_Reconstruct_Itinerary {
+    public List<String> findItinerary(List<List<String>> tickets) {
+        return null;
+    }
+}
