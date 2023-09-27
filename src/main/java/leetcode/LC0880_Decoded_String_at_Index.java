ab3678176d601e9270be0e69f362ef3181b6f9ed
cskwak <changsubkwak@nongshim.co.kr>
2023-09-27 23:41:53 +0900

    feat: solved - 880. Decoded String at Index

--- src/main/java/leetcode/LC0880_Decoded_String_at_Index.java
+++ src/main/java/leetcode/LC0880_Decoded_String_at_Index.java
@@ -2,6 +2,33 @@ package leetcode;
 
 public class LC0880_Decoded_String_at_Index {
     public String decodeAtIndex(String s, int k) {
-        return null;
+        int i;
+
+        long length = 0;
+        for (i = 0; length < k; i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                length *= c - '0';
+            } else {
+                length++;
+            }
+        }
+
+        for (i--; i > 0; i--) {
+            if (Character.isDigit(s.charAt(i))) {
+                int number = s.charAt(i) - '0';
+                length = length / number;
+                k %= length;
+                continue;
+            }
+
+            if (k % length == 0) {
+                break;
+            }
+
+            length--;
+        }
+
+        return s.charAt(i) + "";
     }
 }
