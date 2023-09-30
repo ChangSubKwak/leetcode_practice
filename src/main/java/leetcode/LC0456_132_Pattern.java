f28b73385dd8d5f6fd7aabeebb243d2bb245c9c9
paradise1732 <paradise1732@gmail.com>
2023-09-30 18:47:48 +0900

    feat: solved - 456. 132 Pattern

--- src/main/java/leetcode/LC0456_132_Pattern.java
+++ src/main/java/leetcode/LC0456_132_Pattern.java
@@ -1,7 +1,30 @@
 package leetcode;
 
+import java.util.Stack;
+
 public class LC0456_132_Pattern {
     public boolean find132pattern(int[] nums) {
+        int length = nums.length;
+        int[] min = new int[length];
+        
+        min[0] = nums[0];
+        for (int i = 1; i < length; i++) {
+            min[i] = Math.min(min[i - 1], nums[i]);
+        }
+        
+        Stack<Integer> rightCandidate = new Stack<>();
+        for (int i = length - 1; i >= 0; i--) {
+            while (!rightCandidate.empty() && min[i] >= rightCandidate.peek()) {
+                rightCandidate.pop();
+            }
+            
+            if (!rightCandidate.empty() && min[i] < nums[i] && nums[i] > rightCandidate.peek()) {
+                return true;
+            }
+            
+            rightCandidate.push(nums[i]);
+        }
+        
         return false;
     }
 }
