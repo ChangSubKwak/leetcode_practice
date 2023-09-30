b1d9a708d64b6a818480d574b3a6bcaed832284b
paradise1732 <paradise1732@gmail.com>
2023-09-30 18:47:24 +0900

    test: change function name - 456. 132 Pattern

--- src/test/java/leetcode/LC0456_132_Pattern_Test.java
+++ src/test/java/leetcode/LC0456_132_Pattern_Test.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class LC0456_132_Pattern_Test {
 
     @Test
-    void minimumTotal() {
+    void test() {
         LC0456_132_Pattern test = new LC0456_132_Pattern();
 
         assertThat(test.find132pattern(new int[]{1, 2, 3, 4})).isFalse();
