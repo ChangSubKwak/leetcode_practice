624505932de53fa75a12e1006366fab8db11f16b
changsubkwak <paradise1732@gmail.com>
2023-10-03 10:36:17 +0900

    feat: (WIP) add dummy return

---
+++ src/main/java/leetcode/LC0712_Minimum_ASCII_Delete_Sum_for_Two_Strings.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC0712_Minimum_ASCII_Delete_Sum_for_Two_Strings {
+    public int minimumDeleteSum(String s1, String s2) {
+        return 231;
+    }
+}
--- src/main/java/leetcode/LC1584_Min_Cost_to_Connect_All_Points.java
+++ src/main/java/leetcode/LC1584_Min_Cost_to_Connect_All_Points.java
@@ -2,6 +2,6 @@ package leetcode;
 
 public class LC1584_Min_Cost_to_Connect_All_Points {
     public int minCostConnectPoints(int[][] points) {
-
+        return 0;
     }
 }
