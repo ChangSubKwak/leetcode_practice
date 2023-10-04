fd513fdb0c5f169b3f0f464f5030315b48454258
cskwak <changsubkwak@nongshim.co.kr>
2023-10-05 08:53:22 +0900

    feat: solved - 706. Design HashMap

--- src/main/java/leetcode/LC0706_Design_HashMap.java
+++ src/main/java/leetcode/LC0706_Design_HashMap.java
@@ -4,26 +4,69 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LC0706_Design_HashMap {
-	class MyHashMap {
-	    public Map<Integer, Integer> map;
-	
-	    public MyHashMap() {
-	        map = new HashMap<>();
+	static class ListNode {
+	    int key, value;
+	    ListNode next;
+	    public ListNode(int key, int value, ListNode next) {
+	        this.key = key;
+	        this.value = value;
+	        this.next = next;
 	    }
-	    
-	    public void put(int key, int value) {
-	        map.put(key, value);
-	    }
-	    
-	    public int get(int key) {
-	        if (map.containsKey(key)) {
-	            return map.get(key);
-	        }
-	        return -1;
-	    }
-	    
-	    public void remove(int key) {
-	        map.remove(key);
-	    }
+	}
+
+	static class MyHashMap {
+		static final int size = 19997;
+		static final int multiplier = 12582917;
+
+		ListNode[] data;
+
+		public MyHashMap() {
+			this.data = new ListNode[size];
+		}
+
+		private int hash(int key) {
+			return (int) ((long) key * multiplier % size);
+		}
+
+		public void put(int key, int value) {
+			remove(key);
+			int hashValue = hash(key);
+			ListNode node = new ListNode(key, value, data[hashValue]);
+			data[hashValue] = node;
+		}
+
+		public int get(int key) {
+			int hashValue = hash(key);
+			ListNode node = data[hashValue];
+			while (node != null) {
+				if (node.key == key) {
+					return node.value;
+				}
+				node = node.next;
+			}
+			return -1;
+		}
+
+		public void remove(int key) {
+			int hashValue = hash(key);
+			ListNode node = data[hashValue];
+
+			if (node == null) {
+				return;
+			}
+
+			if (node.key == key) {
+				data[hashValue] = node.next;
+				return;
+			}
+
+			while (node.next != null) {
+				if (node.next.key == key) {
+					node.next = node.next.next;
+					return;
+				}
+				node = node.next;
+			}
+		}
 	}
 }
