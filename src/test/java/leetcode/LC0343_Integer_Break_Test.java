efe59802fc899855a0ad245383323ab70646ec0f
cskwak <changsubkwak@nongshim.co.kr>
2023-10-06 20:20:03 +0900

    test: add static import

--- src/test/java/leetcode/LC0343_Integer_Break_Test.java
+++ src/test/java/leetcode/LC0343_Integer_Break_Test.java
@@ -3,12 +3,14 @@ package leetcode;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class LC0343_Integer_Break_Test {
 
     @Test
     void minimumTotal() {
         LC0343_Integer_Break test = new LC0343_Integer_Break();
-        Assertions.assertThat(test.integerBreak(2)).isEqualTo(1);
-        Assertions.assertThat(test.integerBreak(10)).isEqualTo(36);
+        assertThat(test.integerBreak(2)).isEqualTo(1);
+        assertThat(test.integerBreak(10)).isEqualTo(36);
     }
 }
