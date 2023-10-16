f2854d04a8c55a679bee40d5ad7d620e77de412e
paradise1732 <paradise1732@gmail.com>
2023-10-17 01:19:21 +0900

    feat: solved - 119. Pascals's Triangle II

--- src/main/java/leetcode/LC0119_Pascals_Triangle_II.java
+++ src/main/java/leetcode/LC0119_Pascals_Triangle_II.java
@@ -1,9 +1,25 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class LC0119_Pascals_Triangle_II {
     public List<Integer> getRow(int rowIndex) {
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(Collections.singletonList(1));
+        result.add(Arrays.asList(1, 1));
+        
+        for (int i = 2; i <= rowIndex; i++) {
+            List<Integer> newRow = new ArrayList<>();
+            newRow.add(1);
+            for (int j = 1; j <= i - 1; j++) {
+                newRow.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+            }
+            newRow.add(1);
+            result.add(newRow);
+        }
+        return result.get(rowIndex);
     }
 }
