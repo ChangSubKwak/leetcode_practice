cf838bf0918baf83f255c655d752a41eb2d40337
changsubkwak <paradise1732@gmail.com>
2023-10-26 22:57:52 +0900

    test: modified - 823. Binary Trees With Factors

--- src/test/java/leetcode/LC0823_Binary_Trees_With_Factors_Test.java
+++ src/test/java/leetcode/LC0823_Binary_Trees_With_Factors_Test.java
@@ -7,15 +7,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LC0823_Binary_Trees_With_Factors_Test {
     
-    LC0823_Binary_Trees_With_Factors_X test;
-    
-    @BeforeEach
-    void setUp() {
-        test = new LC0823_Binary_Trees_With_Factors_X();
-    }
-    
     @Test
     void test() {
+        LC0823_Binary_Trees_With_Factors test = new LC0823_Binary_Trees_With_Factors();
         assertThat(test.numFactoredBinaryTrees(new int[]{2, 4})).isEqualTo(3);
         assertThat(test.numFactoredBinaryTrees(new int[]{2, 4, 5, 10})).isEqualTo(7);
     }
