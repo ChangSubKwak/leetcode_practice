6fcf7c26f25e224b8719bd73005d9dfec2a7ec06
cskwak <changsubkwak@nongshim.co.kr>
2023-10-27 23:05:34 +0900

    test: change simply - 5. Longest Palindromic Substring

--- src/test/java/leetcode/LC0005_Longest_Palindromic_Substring_Test.java
+++ src/test/java/leetcode/LC0005_Longest_Palindromic_Substring_Test.java
@@ -6,20 +6,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class LC0005_Longest_Palindromic_Substring_Test {
-	LC0005_Longest_Palindromic_Substring lc;
-	
-	@BeforeEach
-	void setUp() {
-		lc = new LC0005_Longest_Palindromic_Substring();
-	}
-	
+
 	@Test
 	void test() {
-		assertThat(lc.longestPalindrome("babad")).isEqualTo("bab");
-		assertThat(lc.longestPalindrome("cbbd")).isEqualTo("bb");
-		assertThat(lc.longestPalindrome("a")).isEqualTo("a");
-		assertThat(lc.longestPalindrome("ac")).isEqualTo("a");
-		assertThat(lc.longestPalindrome("bb")).isEqualTo("bb");
-		assertThat(lc.longestPalindrome("zudfweormatjycujjirzjpyrmaxurectxrtqedmmgergwdvjmjtstdhcihacqnothgttgqfywcpgnuvwglvfiuxteopoyizgehkwuvvkqxbnufkcbodlhdmbqyghkojrgokpwdhtdrwmvdegwycecrgjvuexlguayzcammupgeskrvpthrmwqaqsdcgycdupykppiyhwzwcplivjnnvwhqkkxildtyjltklcokcrgqnnwzzeuqioyahqpuskkpbxhvzvqyhlegmoviogzwuiqahiouhnecjwysmtarjjdjqdrkljawzasriouuiqkcwwqsxifbndjmyprdozhwaoibpqrthpcjphgsfbeqrqqoqiqqdicvybzxhklehzzapbvcyleljawowluqgxxwlrymzojshlwkmzwpixgfjljkmwdtjeabgyrpbqyyykmoaqdambpkyyvukalbrzoyoufjqeftniddsfqnilxlplselqatdgjziphvrbokofvuerpsvqmzakbyzxtxvyanvjpfyvyiivqusfrsufjanmfibgrkwtiuoykiavpbqeyfsuteuxxjiyxvlvgmehycdvxdorpepmsinvmyzeqeiikajopqedyopirmhymozernxzaueljjrhcsofwyddkpnvcvzixdjknikyhzmstvbducjcoyoeoaqruuewclzqqqxzpgykrkygxnmlsrjudoaejxkipkgmcoqtxhelvsizgdwdyjwuumazxfstoaxeqqxoqezakdqjwpkrbldpcbbxexquqrznavcrprnydufsidakvrpuzgfisdxreldbqfizngtrilnbqboxwmwienlkmmiuifrvytukcqcpeqdwwucymgvyrektsnfijdcdoawbcwkkjkqwzffnuqituihjaklvthulmcjrhqcyzvekzqlxgddjoir")).isEqualTo("gykrkyg");
+		LC0005_Longest_Palindromic_Substring test = new LC0005_Longest_Palindromic_Substring();
+		
+		assertThat(test.longestPalindrome("babad")).isEqualTo("bab");
+		assertThat(test.longestPalindrome("cbbd")).isEqualTo("bb");
+		assertThat(test.longestPalindrome("a")).isEqualTo("a");
+		assertThat(test.longestPalindrome("ac")).isEqualTo("a");
+		assertThat(test.longestPalindrome("bb")).isEqualTo("bb");
+		assertThat(test.longestPalindrome("zudfweormatjycujjirzjpyrmaxurectxrtqedmmgergwdvjmjtstdhcihacqnothgttgqfywcpgnuvwglvfiuxteopoyizgehkwuvvkqxbnufkcbodlhdmbqyghkojrgokpwdhtdrwmvdegwycecrgjvuexlguayzcammupgeskrvpthrmwqaqsdcgycdupykppiyhwzwcplivjnnvwhqkkxildtyjltklcokcrgqnnwzzeuqioyahqpuskkpbxhvzvqyhlegmoviogzwuiqahiouhnecjwysmtarjjdjqdrkljawzasriouuiqkcwwqsxifbndjmyprdozhwaoibpqrthpcjphgsfbeqrqqoqiqqdicvybzxhklehzzapbvcyleljawowluqgxxwlrymzojshlwkmzwpixgfjljkmwdtjeabgyrpbqyyykmoaqdambpkyyvukalbrzoyoufjqeftniddsfqnilxlplselqatdgjziphvrbokofvuerpsvqmzakbyzxtxvyanvjpfyvyiivqusfrsufjanmfibgrkwtiuoykiavpbqeyfsuteuxxjiyxvlvgmehycdvxdorpepmsinvmyzeqeiikajopqedyopirmhymozernxzaueljjrhcsofwyddkpnvcvzixdjknikyhzmstvbducjcoyoeoaqruuewclzqqqxzpgykrkygxnmlsrjudoaejxkipkgmcoqtxhelvsizgdwdyjwuumazxfstoaxeqqxoqezakdqjwpkrbldpcbbxexquqrznavcrprnydufsidakvrpuzgfisdxreldbqfizngtrilnbqboxwmwienlkmmiuifrvytukcqcpeqdwwucymgvyrektsnfijdcdoawbcwkkjkqwzffnuqituihjaklvthulmcjrhqcyzvekzqlxgddjoir")).isEqualTo("gykrkyg");
 	}
 }
