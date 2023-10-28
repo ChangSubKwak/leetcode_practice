8359d11b6ac0710b8c26df4195744954fa69e8d4
changsubkwak <paradise1732@gmail.com>
2023-10-28 19:07:58 +0900

    test: change simply - 1220. Count Vowels Permutation

--- src/test/java/leetcode/LC1220_Count_Vowels_Permutation_Test.java
+++ src/test/java/leetcode/LC1220_Count_Vowels_Permutation_Test.java
@@ -7,15 +7,9 @@ import org.junit.jupiter.api.*;
 
 class LC1220_Count_Vowels_Permutation_Test {
     
-    private LC1220_Count_Vowels_Permutation test;
-    
-    @BeforeEach
-    void setUp() {
-        test = new LC1220_Count_Vowels_Permutation();
-    }
-    
     @Test
     void test() {
+        LC1220_Count_Vowels_Permutation test = new LC1220_Count_Vowels_Permutation();
         assertThat(test.countVowelPermutation(1)).isEqualTo(5);
         assertThat(test.countVowelPermutation(2)).isEqualTo(10);
         assertThat(test.countVowelPermutation(5)).isEqualTo(68);
