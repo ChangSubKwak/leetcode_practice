c5b60a7e19066e7c4ea247c1f50e6da2c2a6d965
changsubkwak <paradise1732@gmail.com>
2023-11-03 22:39:31 +0900

    feat: (WIP) solving - 1441. Build an Array With Stack Operations

---
+++ src/main/java/leetcode/LC1441_Build_an_Array_With_Stack_Operations.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+import java.util.List;
+
+public class LC1441_Build_an_Array_With_Stack_Operations {
+
+    public List<String> buildArray(int[] target, int n) {
+        return null;
+    }
+}
