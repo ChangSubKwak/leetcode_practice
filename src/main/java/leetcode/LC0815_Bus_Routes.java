5926cf82bc2300e47df4b9b4397df9c2e1956303
changsubkwak <paradise1732@gmail.com>
2023-11-12 23:33:37 +0900

    feat: (WIP) solving - 815. Bus Routes

---
+++ src/main/java/leetcode/LC0815_Bus_Routes.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class LC0815_Bus_Routes {
+    public int numBusesToDestination(int[][] routes, int source, int target) {
+        return 2;
+    }
+}
