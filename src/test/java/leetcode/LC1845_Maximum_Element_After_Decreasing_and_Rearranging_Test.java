90bde0049a6adda3b527e5adb95c2f74418149e8
changsubkwak <paradise1732@gmail.com>
2023-11-15 23:52:26 +0900

    test: implement test case - 1845. Maximum Element After Decreasing and Rearranging

---
+++ src/test/java/leetcode/LC1845_Maximum_Element_After_Decreasing_and_Rearranging_Test.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LC1845_Maximum_Element_After_Decreasing_and_Rearranging_Test {
+
+    @Test
+    void test() {
+        LC1845_Maximum_Element_After_Decreasing_and_Rearranging test = new LC1845_Maximum_Element_After_Decreasing_and_Rearranging();
+
+        assertThat(test.maximumElementAfterDecrementingAndRearranging(new int[]{2, 2, 1, 2, 1})).isEqualTo(2);
+        assertThat(test.maximumElementAfterDecrementingAndRearranging(new int[]{100, 1, 1000})).isEqualTo(3);
+    }
+}
