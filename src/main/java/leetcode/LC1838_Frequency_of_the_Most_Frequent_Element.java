5000e15b83a135fe37f9b743fd66dd80cd8b78a5
paradise1732 <paradise1732@gmail.com>
2023-11-19 00:49:50 +0900

    feat: solved - 1838. Frequency of the Most Frequent Element

--- src/main/java/leetcode/LC1838_Frequency_of_the_Most_Frequent_Element.java
+++ src/main/java/leetcode/LC1838_Frequency_of_the_Most_Frequent_Element.java
@@ -1,7 +1,30 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class LC1838_Frequency_of_the_Most_Frequent_Element {
     public int maxFrequency(int[] nums, int k) {
-        return 0;
+        Arrays.sort(nums);
+        
+        int left = 0;
+        int rangeLength = 0;
+        long current = 0;
+        
+        for (int right = 0; right < nums.length; right++) {
+            int target = nums[right];
+            current += target;
+            
+            long sum = (right - left + 1) * target;
+            
+            while (sum - current > k) {
+                current -= nums[left];
+                left++;
+                sum = (right - left + 1) * target;
+            }
+            
+            rangeLength = Math.max(rangeLength, right - left + 1);
+        }
+        
+        return rangeLength;
     }
 }
