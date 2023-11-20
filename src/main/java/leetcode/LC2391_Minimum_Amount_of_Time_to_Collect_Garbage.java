82671dbbc50f1600c5dac48dedc22f7c64610a22
paradise1732 <paradise1732@gmail.com>
2023-11-20 22:06:11 +0900

    feat: (WIP) solving - 2391. Minimum Amount of Time to Collect Garbage

---
+++ src/main/java/leetcode/LC2391_Minimum_Amount_of_Time_to_Collect_Garbage.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC2391_Minimum_Amount_of_Time_to_Collect_Garbage {
+    public int garbageCollection(String[] garbage, int[] travel) {
+        return 21;
+    }
+}
