2e226ad0e89585b3562da54a2c0841e92f850111
changsubkwak <paradise1732@gmail.com>
2023-11-21 20:29:02 +0900

    chore: delete wrong number file

--- src/main/java/leetcode/LC1845_Maximum_Element_After_Decreasing_and_Rearranging.java
+++
@@ -1,7 +0,0 @@
-package leetcode;
-
-public class LC1845_Maximum_Element_After_Decreasing_and_Rearranging {
-    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
-        return 0;
-    }
-}
