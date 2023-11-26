e05298b2784a8aab75b3b5dec4babbdd245180d7
changsubkwak <paradise1732@gmail.com>
2023-11-26 21:58:52 +0900

    fix: fix code grammer

--- src/test/java/leetcode/LC1727_Largest_Submatrix_With_Rearrangements_Test.java
+++ src/test/java/leetcode/LC1727_Largest_Submatrix_With_Rearrangements_Test.java
@@ -10,11 +10,11 @@ class LC1727_Largest_Submatrix_With_Rearrangements_Test {
     void exampleTest() {
         LC1727_Largest_Submatrix_With_Rearrangements test = new LC1727_Largest_Submatrix_With_Rearrangements();
 
-        assertThat(test.largestSubmatrix(new int[][]{{
+        assertThat(test.largestSubmatrix(new int[][]{
             new int[]{0, 0, 1},
             new int[]{1, 1, 1},
             new int[]{1, 0, 1}
-        }})).isEqualTo(4);
+        })).isEqualTo(4);
 
     }
 }
