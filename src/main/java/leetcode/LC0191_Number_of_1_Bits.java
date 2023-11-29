3358598ab450a1c679907d99981a44a0c84f7892
paradise1732 <paradise1732@gmail.com>
2023-11-29 15:05:24 +0900

    feat: solved - 191. Number of 1 Bits

--- src/main/java/leetcode/LC0191_Number_of_1_Bits.java
+++ src/main/java/leetcode/LC0191_Number_of_1_Bits.java
@@ -1,6 +1,47 @@
 package leetcode;
 
 public class LC0191_Number_of_1_Bits {
+    
+    public int hammingWeight(int n) {
+        int num = n;
+        StringBuilder sb = new StringBuilder();
+        if (n > 0) {
+            while (num > 0) {
+                sb.append(num % 2);
+                num /= 2;
+            }
+        } else if (n < 0) {
+            num = Math.abs(n);
+            num--;
+            while (num > 0) {
+                sb.append(num % 2);
+                num /= 2;
+            }
+            int needZeroCount = 32 - sb.length();
+            for (int i = 0; i < needZeroCount; i++) {
+                sb.append(0);
+            }
+            int count = 0;
+            for (int i = 0; i < 32; i++) {
+                if (sb.charAt(i) == '0') {
+                    count++;
+                }
+            }
+            return count;
+        } else {
+            return 0;
+        }
+        
+        int count = 0;
+        for (char c : sb.toString().toCharArray()) {
+            if (c == '1') {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+    
     public int hammingWeight2(int n) {
         long newNum = n;
         if (n < 0) {
@@ -23,8 +64,7 @@ public class LC0191_Number_of_1_Bits {
         
         return count;
     }
-	
-	
+
     public int hammingWeight1(int n) {
         return Integer.bitCount(n);
     }
