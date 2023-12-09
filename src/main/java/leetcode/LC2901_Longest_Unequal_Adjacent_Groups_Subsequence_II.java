15e596c9f8f7d8bdc8a71de11e19a5ffbd353ba3
changsubkwak <paradise1732@gmail.com>
2023-12-09 22:13:57 +0900

    feat: (WIP) solving - 2901. Longest Unequal Adjacent Groups Subsequence II

---
+++ src/main/java/leetcode/LC2901_Longest_Unequal_Adjacent_Groups_Subsequence_II.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LC2901_Longest_Unequal_Adjacent_Groups_Subsequence_II {
+    public List<String> getWordsInLongestSubsequence(int n, String[] words, int[] groups) {
+        return Arrays.asList("bab", "cab");
+    }
+}
