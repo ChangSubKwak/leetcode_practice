aeeb825fa2bd98411b08dc4cc2e590be75e26383
changsubkwak <paradise1732@gmail.com>
2024-07-08 00:07:46 +0900

    feat: solved - 1518. Water Bottles

--- src/main/java/leetcode/LC1518_Water_Bottles.java
+++ src/main/java/leetcode/LC1518_Water_Bottles.java
@@ -2,6 +2,15 @@ package leetcode;
 
 public class LC1518_Water_Bottles {
     public int numWaterBottles(int numBottles, int numExchange) {
-        return 13;
+        int result = 0;
+
+        result += numBottles;
+
+        while (numBottles >= numExchange) {
+            result += (int)(numBottles / numExchange);
+            numBottles = (int)(numBottles / numExchange) + numBottles % numExchange;
+        }
+
+        return result;
     }
 }
