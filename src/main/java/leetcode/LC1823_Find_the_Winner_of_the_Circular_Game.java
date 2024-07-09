c66972aebc8c988ca5c5c8611492c4f940046ad0
changsubkwak <paradise1732@gmail.com>
2024-07-09 09:10:27 +0900

    feat: solved - 1823. Find the Winner of the Circular Game

--- src/main/java/leetcode/LC1823_Find_the_Winner_of_the_Circular_Game.java
+++ src/main/java/leetcode/LC1823_Find_the_Winner_of_the_Circular_Game.java
@@ -1,7 +1,23 @@
 package leetcode;
 
+import java.util.LinkedList;
+
 public class LC1823_Find_the_Winner_of_the_Circular_Game {
     public int findTheWinner(int n, int k) {
-        return 0;
+        LinkedList<Integer> friend = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            friend.add(i);
+        }
+
+        int i = 0;
+        while (friend.size() >= 2) {
+            i += (k - 1);
+            i %= friend.size();
+            friend.remove(i);
+        }
+
+        int result = friend.get(0);
+
+        return result;
     }
 }
