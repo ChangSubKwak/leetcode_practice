e30ac19e13f09f5bf4d0f2d84fc2cf5e5d0ffaa6
changsubkwak <paradise1732@gmail.com>
2024-07-19 21:01:11 +0900

    feat: (WIP) solving - 1380. Lucky Numbers in a Matrix

---
+++ src/main/java/leetcode/LC1380_Lucky_Numbers_in_a_Matrix.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LC1380_Lucky_Numbers_in_a_Matrix {
+    public List<Integer> luckyNumbers (int[][] matrix) {
+        return Arrays.asList(15);
+    }
+}
