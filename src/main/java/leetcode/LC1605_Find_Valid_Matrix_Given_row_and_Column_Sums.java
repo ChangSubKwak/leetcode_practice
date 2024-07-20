480f656ae9f6a9aa829cff1ea65a21a691a2d83d
changsubkwak <paradise1732@gmail.com>
2024-07-20 22:37:59 +0900

    feat: solved - 1605. Find Valid Matrix Given Row and Column Sums

--- src/main/java/leetcode/LC1605_Find_Valid_Matrix_Given_row_and_Column_Sums.java
+++ src/main/java/leetcode/LC1605_Find_Valid_Matrix_Given_row_and_Column_Sums.java
@@ -2,6 +2,22 @@ package leetcode;
 
 public class LC1605_Find_Valid_Matrix_Given_row_and_Column_Sums {
     public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
-        return null;
+        int row = rowSum.length;
+        int col = colSum.length;
+
+        int[] currentRowSum = new int[row];
+        int[] currentColSum = new int[col];
+
+        int[][] originalMatrix = new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                originalMatrix[i][j] = Math.min(rowSum[i] - currentRowSum[i], colSum[j] - currentColSum[j]);
+                currentRowSum[i] += originalMatrix[i][j];
+                currentColSum[j] += originalMatrix[i][j];
+            }
+        }
+
+        return originalMatrix;
     }
 }
