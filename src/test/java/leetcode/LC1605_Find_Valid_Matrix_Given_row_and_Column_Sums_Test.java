0a46f5ebce90f3162b879b4c2c94fcd828c3d9bf
changsubkwak <paradise1732@gmail.com>
2024-07-20 22:37:42 +0900

    test: modify test case(better) - 1605. Find Valid Matrix Given Row and Column Sums

--- src/test/java/leetcode/LC1605_Find_Valid_Matrix_Given_row_and_Column_Sums_Test.java
+++ src/test/java/leetcode/LC1605_Find_Valid_Matrix_Given_row_and_Column_Sums_Test.java
@@ -5,18 +5,57 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LC1605_Find_Valid_Matrix_Given_row_and_Column_Sums_Test {
-    @Test
-    void test() {
-        LC1605_Find_Valid_Matrix_Given_row_and_Column_Sums test = new LC1605_Find_Valid_Matrix_Given_row_and_Column_Sums();
-        assertThat(test.restoreMatrix(new int[]{3, 8}, new int[]{4, 7})).isEqualTo(new int[][]{
-            {3, 0},
-            {1, 7}
-        });
 
-        assertThat(test.restoreMatrix(new int[]{5, 7, 10}, new int[]{8, 6, 8})).isEqualTo(new int[][]{
-            {0, 5, 0},
-            {6, 1, 0},
-            {2, 0, 8}
-        });
+    @Test
+    void test1() {
+        LC1605_Find_Valid_Matrix_Given_row_and_Column_Sums test = new LC1605_Find_Valid_Matrix_Given_row_and_Column_Sums();
+
+        int[][] result = test.restoreMatrix(new int[]{3, 8}, new int[]{4, 7});
+        int[] rowSum = rowSum(result);
+        int[] columnSum = columnSum(result);
+
+        assertThat(rowSum).isEqualTo(new int[]{3, 8});
+        assertThat(columnSum).isEqualTo(new int[]{4, 7});
     }
+
+    @Test
+    void test2() {
+        LC1605_Find_Valid_Matrix_Given_row_and_Column_Sums test = new LC1605_Find_Valid_Matrix_Given_row_and_Column_Sums();
+
+        int[][] result = test.restoreMatrix(new int[]{5, 7, 10}, new int[]{8, 6, 8});
+        int[] rowSum = rowSum(result);
+        int[] columnSum = columnSum(result);
+
+        assertThat(rowSum).isEqualTo(new int[]{5, 7, 10});
+        assertThat(columnSum).isEqualTo(new int[]{8, 6, 8});
+    }
+
+    private int[] rowSum(int[][] matrix) {
+        int[] sum = new int[matrix.length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            int partSum = 0;
+            for (int j = 0; j < matrix[0].length; j++) {
+                partSum += matrix[i][j];
+            }
+            sum[i] = partSum;
+        }
+
+        return sum;
+    }
+
+    private int[] columnSum(int[][] matrix) {
+        int[] sum = new int[matrix[0].length];
+
+        for (int i = 0; i < matrix[0].length; i++) {
+            int partSum = 0;
+            for (int j = 0; j < matrix.length; j++) {
+                partSum += matrix[j][i];
+            }
+            sum[i] = partSum;
+        }
+
+        return sum;
+    }
+
 }
