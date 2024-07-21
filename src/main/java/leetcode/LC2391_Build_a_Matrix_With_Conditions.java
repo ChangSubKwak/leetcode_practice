512c6b2853b0908557cc5b0c084a3ec818edcd81
changsubkwak <paradise1732@gmail.com>
2024-07-21 23:57:05 +0900

    feat: (WIP) solving - 2392. Build a Matrix With Conditions

---
+++ src/main/java/leetcode/LC2391_Build_a_Matrix_With_Conditions.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC2391_Build_a_Matrix_With_Conditions {
+    public int[][] buildMatrix(int k, int[][] rowConditions, int[][] colConditions) {
+        return null;
+    }
+}
