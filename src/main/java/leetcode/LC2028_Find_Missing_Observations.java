7318a2d6fae72db446081e5f2c39b98a9252598e
changsubkwak <paradise1732@gmail.com>
2024-09-05 22:54:34 +0900

    feat: (WIP) solving - 2028. Find Missing Observations

---
+++ src/main/java/leetcode/LC2028_Find_Missing_Observations.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC2028_Find_Missing_Observations {
+        public int[] missingRolls(int[] rolls, int mean, int n) {
+            return new int[]{};
+        }
+}
