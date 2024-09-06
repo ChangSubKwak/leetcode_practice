b03fd9e2307c55039088c4d8886611ef4f43c29b
changsubkwak <paradise1732@gmail.com>
2024-09-06 22:34:26 +0900

    feat: solved - 2028. Find Missing Observations

--- src/main/java/leetcode/LC2028_Find_Missing_Observations.java
+++ src/main/java/leetcode/LC2028_Find_Missing_Observations.java
@@ -1,7 +1,39 @@
 package leetcode;
 
 public class LC2028_Find_Missing_Observations {
-        public int[] missingRolls(int[] rolls, int mean, int n) {
+    public int[] missingRolls(int[] rolls, int mean, int n) {
+        int m = rolls.length;
+        int length = m + n;
+        int rollsTotal = 0;
+        for (int num: rolls) {
+            rollsTotal += num;
+        }
+
+        int remainTotal = length * mean - rollsTotal;
+        int remainAverage = remainTotal / n;
+        int finalRemain = remainTotal - remainAverage * n;
+        int[] result = new int[n];
+        int resultIndex = 0;
+
+        if (remainTotal > 6 * n || remainAverage <= 0) {
             return new int[]{};
         }
+
+        while (resultIndex < n) {
+            result[resultIndex] = remainAverage;
+            resultIndex++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int difference = Math.min(finalRemain, (6 - result[i]));
+            result[i] = result[i] + difference;
+            finalRemain -= difference;
+
+            if (finalRemain <= 0) {
+                break;
+            }
+        }
+
+        return result;
+    }
 }
