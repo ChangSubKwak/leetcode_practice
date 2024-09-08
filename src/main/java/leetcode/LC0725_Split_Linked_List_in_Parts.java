b48d85c11d499f8d56cd76bbdfb84555b4933ffa
changsubkwak <paradise1732@gmail.com>
2024-09-08 23:54:52 +0900

    feat: solved - 725. Split Linked List in Parts

--- src/main/java/leetcode/LC0725_Split_Linked_List_in_Parts.java
+++ src/main/java/leetcode/LC0725_Split_Linked_List_in_Parts.java
@@ -2,6 +2,26 @@ package leetcode;
 
 public class LC0725_Split_Linked_List_in_Parts {
     public ListNode[] splitListToParts(ListNode head, int k) {
-        return null;
+        ListNode cur = head;
+        int N = 0;
+        while (cur != null) {
+            cur = cur.next;
+            N++;
+        }
+
+        int width = N / k, rem = N % k;
+
+        ListNode[] ans = new ListNode[k];
+        cur = head;
+        for (int i = 0; i < k; ++i) {
+            ListNode tempHead = new ListNode(0);
+            ListNode write = tempHead;
+            for (int j = 0; j < width + (i < rem ? 1 : 0); ++j) {
+                write = write.next = new ListNode(cur.val);
+                if (cur != null) cur = cur.next;
+            }
+            ans[i] = tempHead.next;
+        }
+        return ans;
     }
 }
