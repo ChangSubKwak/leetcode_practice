3ea2fa2e2773580be54beb7f83661d61e8d669b9
changsubkwak <paradise1732@gmail.com>
2024-09-09 14:56:44 +0900

    feat: (WIP) fix syntax error - 2326. Spiral Matrix IV

--- src/main/java/leetcode/LC2326_Spiral_Matrix_IV.java
+++ src/main/java/leetcode/LC2326_Spiral_Matrix_IV.java
@@ -2,6 +2,6 @@ package leetcode;
 
 public class LC2326_Spiral_Matrix_IV {
     public int[][] spiralMatrix(int m, int n, ListNode head) {
-        return new int[][];
+        return new int[][]{};
     }
 }
