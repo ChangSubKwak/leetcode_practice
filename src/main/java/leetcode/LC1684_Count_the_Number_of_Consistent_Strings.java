396fcec889a0d590b147655f288cb6eb03af978a
changsubkwak <paradise1732@gmail.com>
2024-09-12 10:51:37 +0900

    feat: (WIP) solving - 1684. Count the Number of Consistent Strings

---
+++ src/main/java/leetcode/LC1684_Count_the_Number_of_Consistent_Strings.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC1684_Count_the_Number_of_Consistent_Strings {
+    public int countConsistentStrings(String allowed, String[] words) {
+        return 2;
+    }
+}
