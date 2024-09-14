86661e4affbfed8989358f66522d7dc38bbb33ae
changsubkwak <paradise1732@gmail.com>
2024-09-14 23:59:47 +0900

    test: implement test case - 2419. Longest Subarray With Maximum Bitwise AND

---
+++ src/test/java/leetcode/LC2419_Longest_Subarray_With_Maximum_Bitwise_AND_Test.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LC2419_Longest_Subarray_With_Maximum_Bitwise_AND_Test {
+    @Test
+    void exampleTest() {
+        LC2419_Longest_Subarray_With_Maximum_Bitwise_AND test = new LC2419_Longest_Subarray_With_Maximum_Bitwise_AND();
+        assertThat(test.longestSubarray(new int[]{1, 2, 3, 3, 2, 2, })).isEqualTo(2);
+    }
+}
