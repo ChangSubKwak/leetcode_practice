c6767959c60914747c295a80fe6bb0c73058c6c9
changsubkwak <paradise1732@gmail.com>
2024-09-16 23:51:23 +0900

    feat: (WIP) solving - 1371. Find the Longest Substring Containing Vowels in Even Counts

---
+++ src/main/java/leetcode/LC1371_Find_the_Longest_Substring_Containing_Vowels_in_Even_Counts.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC1371_Find_the_Longest_Substring_Containing_Vowels_in_Even_Counts {
+    public int findTheLongestSubstring(String s) {
+        return 13;
+    }
+}
