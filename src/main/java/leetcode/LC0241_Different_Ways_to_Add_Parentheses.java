6f0dd59323fad2e125b13702e0f7ae639804b8bb
changsubkwak <paradise1732@gmail.com>
2024-09-19 18:46:25 +0900

    feat: (WIP) solving - 241. Different Ways to Add Parentheses

---
+++ src/main/java/leetcode/LC0241_Different_Ways_to_Add_Parentheses.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LC0241_Different_Ways_to_Add_Parentheses {
+    public List<Integer> diffWaysToCompute(String expression) {
+        return Arrays.asList(0, 2);
+    }
+}
