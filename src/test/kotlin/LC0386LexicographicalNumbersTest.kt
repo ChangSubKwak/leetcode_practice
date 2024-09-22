c4fad4d8042a4fb802f0ea5675adbbe77a0f25da
changsubkwak <paradise1732@gmail.com>
2024-09-22 22:25:13 +0900

    test: fix test case - 386. Lexicographical Numbers

--- src/test/kotlin/LC0386LexicographicalNumbersTest.kt
+++ src/test/kotlin/LC0386LexicographicalNumbersTest.kt
@@ -7,7 +7,7 @@ class LC0386LexicographicalNumbersTest {
     fun testLexicalOrder() {
         val test = LC0386LexicographicalNumbers()
 
-        assertEquals(test.lexicalOrder(13), listOf(1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 6, 7, 8, 9))
+        assertEquals(test.lexicalOrder(13), listOf(1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9))
         assertEquals(test.lexicalOrder(2), listOf(1, 2))
     }
 }
