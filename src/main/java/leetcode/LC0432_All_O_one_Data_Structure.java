cd1973ae0b41db8a2b4bcedc70811d208848ce96
changsubkwak <paradise1732@gmail.com>
2024-09-29 21:40:25 +0900

    feat: (WIP) solving - 432. All O'one Data Structure

---
+++ src/main/java/leetcode/LC0432_All_O_one_Data_Structure.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+public class LC0432_All_O_one_Data_Structure {
+    static class AllOne {
+
+        public AllOne() {
+
+        }
+
+        public void inc(String key) {
+
+        }
+
+        public void dec(String key) {
+
+        }
+
+        public String getMaxKey() {
+
+        }
+
+        public String getMinKey() {
+
+        }
+    }
+}
