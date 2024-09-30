d17b54d01ea7be7eadcc6243a7a97c81ebb71a45
changsubkwak <paradise1732@gmail.com>
2024-09-30 22:46:06 +0900

    feat: solved - 1381. Design a Stack With Increment Operation

--- src/main/java/leetcode/LC1381_Design_a_Stack_With_Increment_Operation.java
+++ src/main/java/leetcode/LC1381_Design_a_Stack_With_Increment_Operation.java
@@ -2,21 +2,36 @@ package leetcode;
 
 public class LC1381_Design_a_Stack_With_Increment_Operation {
     static class CustomStack {
+        private int[] stack;
+        private int currentIndex;
 
         public CustomStack(int maxSize) {
-
+            stack = new int[maxSize];
+            currentIndex = 0;
         }
 
         public void push(int x) {
-
+            if (currentIndex >= stack.length) {
+                return;
+            }
+            stack[currentIndex] = x;
+            currentIndex++;
         }
 
         public int pop() {
-            return 0;
+            if (currentIndex <= 0) {
+                return -1;
+            }
+
+            currentIndex--;
+            return stack[currentIndex];
         }
 
         public void increment(int k, int val) {
-
+            k = Math.min(k, stack.length);
+            for (int i = 0; i < k; i++) {
+                stack[i] += val;
+            }
         }
     }
 }
