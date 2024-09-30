0380660554df6af704d1aa16c09ed4e96c764822
changsubkwak <paradise1732@gmail.com>
2024-09-30 22:12:07 +0900

    feat: (WIP) solving - 1381. Design a Stack With Increment Operation

---
+++ src/main/java/leetcode/LC1381_Design_a_Stack_With_Increment_Operation.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class LC1381_Design_a_Stack_With_Increment_Operation {
+    static class CustomStack {
+
+        public CustomStack(int maxSize) {
+
+        }
+
+        public void push(int x) {
+
+        }
+
+        public int pop() {
+            return 0;
+        }
+
+        public void increment(int k, int val) {
+
+        }
+    }
+}
