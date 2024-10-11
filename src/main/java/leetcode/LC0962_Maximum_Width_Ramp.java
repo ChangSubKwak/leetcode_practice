15ca9d50cb28dfd3cce4d3654bfff058a6828066
changsubkwak <paradise1732@gmail.com>
2024-10-11 22:14:29 +0900

    feat: solved - 926. Maximum Width Ramp

--- src/main/java/leetcode/LC0962_Maximum_Width_Ramp.java
+++ src/main/java/leetcode/LC0962_Maximum_Width_Ramp.java
@@ -1,7 +1,29 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class LC0962_Maximum_Width_Ramp {
     public int maxWidthRamp(int[] nums) {
-        return 0;
+        int length = nums.length;
+        Integer[] indices = new Integer[length];
+
+        for (int i = 0; i < length; i++) {
+            indices[i] = i;
+        }
+
+        Arrays.sort(indices, (i, j) ->
+            nums[i] != nums[j] ? nums[i] - nums[j] : i - j
+        );
+        System.out.println(Arrays.toString(indices));
+
+        int minIndex = length;
+        int maxWidth = 0;
+
+        for (int i : indices) {
+            maxWidth = Math.max(maxWidth, i - minIndex);
+            minIndex = Math.min(minIndex, i);
+        }
+
+        return maxWidth;
     }
 }
