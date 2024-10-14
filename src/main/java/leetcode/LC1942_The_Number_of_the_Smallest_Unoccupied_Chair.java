4ab81e4586c3b85091a6c3423e3d776bfbf1ad5a
changsubkwak <paradise1732@gmail.com>
2024-10-14 23:54:17 +0900

    feat: solved - 1942. The Number of the Smallest Unoccupied Chair

--- src/main/java/leetcode/LC1942_The_Number_of_the_Smallest_Unoccupied_Chair.java
+++ src/main/java/leetcode/LC1942_The_Number_of_the_Smallest_Unoccupied_Chair.java
@@ -6,7 +6,8 @@ import java.util.PriorityQueue;
 
 public class LC1942_The_Number_of_the_Smallest_Unoccupied_Chair {
     public int smallestChair(int[][] times, int targetFriend) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> chairToPerson = new HashMap<>();
+        Map<Integer, Integer> personToChair = new HashMap<>();
         PriorityQueue<Integer> chairQueue = new PriorityQueue<>();
         PriorityQueue<int[]> arrivalQueueLowest = new PriorityQueue<>((x, y) -> x[0] - y[0]);
         PriorityQueue<int[]> leavingQueueLowest = new PriorityQueue<>((x, y) -> x[0] - y[0]);
@@ -14,22 +15,28 @@ public class LC1942_The_Number_of_the_Smallest_Unoccupied_Chair {
         for (int i = 0; i < times.length; i++) {
             arrivalQueueLowest.add(new int[]{times[i][0], i});
             leavingQueueLowest.add(new int[]{times[i][1], i});
+            chairQueue.add(i);
         }
 
-        while (true) {
-            if (arrivalQueueLowest.size() > 0 && leavingQueueLowest.size() > 0) {
-                int[] arrival = arrivalQueueLowest.peek();
-                int[] leaving = leavingQueueLowest.peek();
+        while (arrivalQueueLowest.size() > 0) {
+            int[] arrival = arrivalQueueLowest.peek();
+            int[] leaving = leavingQueueLowest.peek();
 
-                if (arrival[0] < leaving[0]) {
-                    if (chairQueue.size() == 0) {
-                        chairQueue.add(0);
-                    }
-                }
+            if (arrival[0] < leaving[0]) {
+                int chairNumber = chairQueue.poll();
+                chairToPerson.put(chairNumber, arrival[1]);
+                personToChair.put(arrival[1], chairNumber);
+                arrivalQueueLowest.poll();
+                continue;
             }
-            break;
+            else {
+                int chairNumber = personToChair.get(leaving[1]);
+                chairToPerson.remove(chairNumber);
+                chairQueue.add(chairNumber);
+                leavingQueueLowest.poll();
+            }
         }
-        
-        return 0;
+
+        return personToChair.get(targetFriend);
     }
 }
