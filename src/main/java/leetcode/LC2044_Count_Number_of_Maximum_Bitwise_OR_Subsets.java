24d66b8d1ddf28e73b2d58618bfc2d9298f3f7b7
changsubkwak <paradise1732@gmail.com>
2024-10-19 23:52:02 +0900

    feat: (WIP) solving - 2044. Count Number of Maximum Bitwise OR Subsets

--- src/main/java/leetcode/LC2044_Count_Number_of_Maximum_Bitwise_OR_Subsets.java
+++ src/main/java/leetcode/LC2044_Count_Number_of_Maximum_Bitwise_OR_Subsets.java
@@ -2,6 +2,29 @@ package leetcode;
 
 public class LC2044_Count_Number_of_Maximum_Bitwise_OR_Subsets {
     public int countMaxOrSubsets(int[] nums) {
-        return 0;
+        int maxValue = 0;
+        int length = nums.length;
+
+        for (int i = 0; i < length; i++) {
+            int cumulatedValue = nums[i];
+            for (int j = i; j < length; j++) {
+                cumulatedValue = cumulatedValue | nums[j];
+                maxValue = Math.max(maxValue, cumulatedValue);
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < length; i++) {
+            int cumulatedValue = nums[i];
+            for (int j = i; j < length; j++) {
+                cumulatedValue = cumulatedValue | nums[j];
+//                System.out.println("i = " + i + " j = " + j + " cumulatedValue = " + cumulatedValue);
+                if (maxValue == cumulatedValue) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
     }
 }
