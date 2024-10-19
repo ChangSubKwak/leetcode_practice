895142f425a99373a770b96f669351596c259f10
changsubkwak <paradise1732@gmail.com>
2024-10-20 00:13:24 +0900

    feat: solved - 2044. Count Number of Maximum Bitwise OR Subsets

--- src/main/java/leetcode/LC2044_Count_Number_of_Maximum_Bitwise_OR_Subsets.java
+++ src/main/java/leetcode/LC2044_Count_Number_of_Maximum_Bitwise_OR_Subsets.java
@@ -2,29 +2,26 @@ package leetcode;
 
 public class LC2044_Count_Number_of_Maximum_Bitwise_OR_Subsets {
     public int countMaxOrSubsets(int[] nums) {
-        int maxValue = 0;
-        int length = nums.length;
-
-        for (int i = 0; i < length; i++) {
-            int cumulatedValue = nums[i];
-            for (int j = i; j < length; j++) {
-                cumulatedValue = cumulatedValue | nums[j];
-                maxValue = Math.max(maxValue, cumulatedValue);
-            }
+        int maxOrValue = 0;
+        for (int num : nums) {
+            maxOrValue |= num;
         }
 
-        int count = 0;
-        for (int i = 0; i < length; i++) {
-            int cumulatedValue = nums[i];
-            for (int j = i; j < length; j++) {
-                cumulatedValue = cumulatedValue | nums[j];
-//                System.out.println("i = " + i + " j = " + j + " cumulatedValue = " + cumulatedValue);
-                if (maxValue == cumulatedValue) {
-                    count++;
-                }
-            }
+        return countSubsets(nums, 0, 0, maxOrValue);
+    }
+
+    private int countSubsets(int[] nums, int index, int currentOr, int targetOr) {
+        if (index == nums.length && currentOr == targetOr) {
+            return 1;
         }
 
-        return count;
+        if (index == nums.length) {
+            return 0;
+        }
+
+        int countIfExclude = countSubsets(nums, index + 1, currentOr, targetOr);
+        int countIfInclude = countSubsets(nums, index + 1, currentOr | nums[index], targetOr);
+
+        return countIfExclude + countIfInclude;
     }
 }
