0e77718bde451682ab3fa3fba9027c7f1bcd2e51
whj0531 <hyunju6442@naver.com>
2024-10-22 23:30:08 +0900

    feat: solved - 1593. Split a String Into the Max Number of Unique Strings

--- src/main/java/leetcode/LC1593_Split_a_String_Into_the_Max_Number_of_Unique_Strings.java
+++ src/main/java/leetcode/LC1593_Split_a_String_Into_the_Max_Number_of_Unique_Strings.java
@@ -1,7 +1,29 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LC1593_Split_a_String_Into_the_Max_Number_of_Unique_Strings {
     public int maxUniqueSplit(String s) {
-        return 0;
+        Set<String> seen = new HashSet<>();
+        return backtrack(s, 0, seen);
+    }
+
+    private int backtrack(String s, int start, Set<String> seen) {
+        if (start == s.length()) {
+            return 0;
+        }
+
+        int maxCount = 0;
+
+        for (int end = start + 1; end <= s.length(); ++end) {
+            String substring = s.substring(start, end);
+            if (!seen.contains(substring)) {
+                seen.add(substring);
+                maxCount = Math.max(maxCount, 1 + backtrack(s, end, seen));
+                seen.remove(substring);
+            }
+        }
+        return maxCount;
     }
 }
