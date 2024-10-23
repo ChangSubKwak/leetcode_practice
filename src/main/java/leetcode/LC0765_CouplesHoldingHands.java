e1110434e7713b2b1936c4eccf9ff10cf6950f51
changsubkwak <paradise1732@gmail.com>
2024-10-23 22:15:18 +0900

    chore: remove unknown character

--- src/main/java/leetcode/LC0765_CouplesHoldingHands.java
+++ src/main/java/leetcode/LC0765_CouplesHoldingHands.java
@@ -5,9 +5,8 @@ public class LC0765_CouplesHoldingHands {
     	int ans = 0;
     	for (int i = 0 ; i < row.length ; i += 2) {
     		int x = row[i];
-    		if (row[i+1] == (x^1)) continue;		// row[i] 와 row[i+1]은 커플
+    		if (row[i+1] == (x^1)) continue;
     		ans++;
-    		// 자리 찾기
     		for (int j = i+1; j < row.length; j++) {
     			if (row[j] == (x^1)) {
     				row[j] = row[i+1];
