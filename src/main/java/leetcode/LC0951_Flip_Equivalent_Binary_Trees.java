289c6603bc3f25eaf4632470bc930fa288d35e62
changsubkwak <paradise1732@gmail.com>
2024-10-24 23:13:29 +0900

    feat: (WIP) - solving - 951. Flip Equivalent Binary Trees

---
+++ src/main/java/leetcode/LC0951_Flip_Equivalent_Binary_Trees.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class LC0951_Flip_Equivalent_Binary_Trees {
+    private boolean recur(TreeNode root1, TreeNode root2) {
+        return false;
+    }
+
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        return recur(root1, root2);
+    }
+}
