d7caa1b397992edecc72bcd52a3c33794100a0cd
changsubkwak <paradise1732@gmail.com>
2024-10-25 23:27:04 +0900

    feat: solved - 951. Flip Equivalent Binary Trees

--- src/main/java/leetcode/LC0951_Flip_Equivalent_Binary_Trees.java
+++ src/main/java/leetcode/LC0951_Flip_Equivalent_Binary_Trees.java
@@ -2,6 +2,68 @@ package leetcode;
 
 public class LC0951_Flip_Equivalent_Binary_Trees {
     private boolean recur(TreeNode root1, TreeNode root2) {
+        if (root1 != null && root2 == null) {
+            return false;
+        }
+
+        if (root1 == null && root2 != null) {
+            return false;
+        }
+
+        if (root1 == null) {
+            return true;
+        }
+
+        if (root1.val != root2.val) {
+            return false;
+        }
+
+        if (root1.left == null && root1.right == null) {
+            return root2.left == null && root2.right == null;
+        }
+
+        if (root2.left == null && root2.right == null) {
+            return false;
+        }
+
+        if (root1.left == null) {
+            if (root2.left == null && root1.right.val == root2.right.val) {
+                return recur(root1.right, root2.right);
+            }
+
+            if (root2.right == null && root1.right.val == root2.left.val) {
+                return recur(root1.right, root2.left);
+            }
+            return false;
+        }
+
+        if (root1.right == null) {
+            if (root2.left == null && root1.left.val == root2.right.val) {
+                return recur(root1.left, root2.right) && recur(root1.left, root2.right);
+            }
+
+            if (root2.right == null && root1.left.val == root2.left.val) {
+                return recur(root1.left, root2.left) && recur(root1.right, root2.right);
+            }
+            return false;
+        }
+
+        if (root2.left == null) {
+            return false;
+        }
+
+        if (root2.right == null) {
+            return false;
+        }
+
+        if (root1.left.val == root2.left.val && root1.right.val == root2.right.val) {
+            return recur(root1.left, root2.left) && recur(root1.right, root2.right);
+        }
+
+        if (root1.left.val == root2.right.val && root1.right.val == root2.left.val) {
+            return recur(root1.left, root2.right) && recur(root1.right, root2.left);
+        }
+
         return false;
     }
 
