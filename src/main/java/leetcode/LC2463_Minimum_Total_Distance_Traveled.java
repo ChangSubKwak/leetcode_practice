6e0bb0a8af951bb4c94869e8ad001252c3a88bcb
changsubkwak <paradise1732@gmail.com>
2024-10-31 21:54:06 +0900

    feat: (WIP) solving - 2463. Minimum Total Distance Traveled

---
+++ src/main/java/leetcode/LC2463_Minimum_Total_Distance_Traveled.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+import java.util.List;
+
+public class LC2463_Minimum_Total_Distance_Traveled {
+    public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
+        return 4;
+    }
+}
