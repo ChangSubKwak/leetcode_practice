53b94c1de1a53fe2e512a962c67499b076cc7a9a
changsubkwak <paradise1732@gmail.com>
2024-11-13 03:42:47 +0900

    test: implement test case - 2070. Most Beautiful Item for Each Query

---
+++ src/test/java/leetcode/LC2070_Most_Beautiful_Item_for_Each_Query_Test.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LC2070_Most_Beautiful_Item_for_Each_Query_Test {
+    LC2070_Most_Beautiful_Item_for_Each_Query test = new LC2070_Most_Beautiful_Item_for_Each_Query();
+
+    @Test
+    void exampleTest() {
+        assertThat(test.maximumBeauty(
+            new int[][]{{1, 2}, {3, 2}, {2, 4}, {5, 6}, {3, 5}},
+            new int[]{1, 2, 3, 4, 5, 6})
+        ).isEqualTo(new int[]{2, 4, 5, 5, 6, 6});
+
+        assertThat(test.maximumBeauty(
+            new int[][]{{1, 2}, {1, 2}, {1, 3}, {1, 4}},
+            new int[]{1})
+        ).isEqualTo(new int[]{4});
+    }
+}
