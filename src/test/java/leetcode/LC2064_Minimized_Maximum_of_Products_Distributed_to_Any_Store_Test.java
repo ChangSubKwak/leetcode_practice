1b1e8757369101548e25d7bf19d36819d16d4ff0
changsubkwak <paradise1732@gmail.com>
2024-11-14 23:02:04 +0900

    test: implement test case - 2064. Minimized Maximum of Products Distributed to Any Store

---
+++ src/test/java/leetcode/LC2064_Minimized_Maximum_of_Products_Distributed_to_Any_Store_Test.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LC2064_Minimized_Maximum_of_Products_Distributed_to_Any_Store_Test {
+    @Test
+    void exampleTest() {
+        LC2064_Minimized_Maximum_of_Products_Distributed_to_Any_Store test = new LC2064_Minimized_Maximum_of_Products_Distributed_to_Any_Store();
+
+        assertThat(test.minimizedMaximum(6, new int[]{11, 6})).isEqualTo(3);
+        assertThat(test.minimizedMaximum(6, new int[]{11, 6})).isEqualTo(3);
+        assertThat(test.minimizedMaximum(6, new int[]{11, 6})).isEqualTo(3);
+
+    }
+}
