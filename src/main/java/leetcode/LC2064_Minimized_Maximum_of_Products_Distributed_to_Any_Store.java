7be7859499d6b42b7fb7fe0a7381cd24818249ae
changsubkwak <paradise1732@gmail.com>
2024-11-14 23:02:20 +0900

    feat: (WIP) solving - 2064. Minimized Maximum of Products Distributed to Any Store

---
+++ src/main/java/leetcode/LC2064_Minimized_Maximum_of_Products_Distributed_to_Any_Store.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC2064_Minimized_Maximum_of_Products_Distributed_to_Any_Store {
+    public int minimizedMaximum(int n, int[] quantities) {
+        return 0;
+    }
+}
