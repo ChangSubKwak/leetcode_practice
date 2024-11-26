21f848e5a148ea2e2c68f32da5c76071577bcdbb
changsubkwak <paradise1732@gmail.com>
2024-11-26 22:47:45 +0900

    feat: (WIP) solving - 2924. Find Champion II

---
+++ src/main/java/leetcode/LC2924_Find_Champion_II.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class LC2924_Find_Champion_II {
+    public int findChampion(int n, int[][] edges) {
+        boolean[] isWins = new boolean[n];
+        Arrays.fill(isWins, true);
+
+        for (int[] edge : edges) {
+            int loser = edge[1];
+            isWins[loser] = false;
+        }
+
+        int champion = -1;
+        int championcount = 0;
+
+        for (int i = 0; i < n; i++) {
+            boolean isWin = isWins[i];
+            if (isWin) {
+                champion = i;
+                championcount++;
+            }
+        }
+
+        return championcount == 1 ? champion : -1;
+    }
+}
