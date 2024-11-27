8bc0b4fbf934e70e6944380cbd9b6d9fa818a877
changsubkwak <paradise1732@gmail.com>
2024-11-27 20:37:02 +0900

    feat: (WIP) solving - 3243. Shortest Distance After Road Addition Queries I

---
+++ src/main/java/leetcode/LC3243_Shortest_Distance_After_Road_Addition_Queries_I.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC3243_Shortest_Distance_After_Road_Addition_Queries_I {
+    public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
+        return new int[]{};
+    }
+}
