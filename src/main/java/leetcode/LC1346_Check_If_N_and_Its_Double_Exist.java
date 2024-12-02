adc53b362bf3aeee0f37b56523d7e0f14c985cd6
changsubkwak <paradise1732@gmail.com>
2024-12-02 23:44:36 +0900

    feat: solved - 1346. Check If N and Its Double Exist

--- src/main/java/leetcode/LC1346_Check_If_N_and_Its_Double_Exist.java
+++ src/main/java/leetcode/LC1346_Check_If_N_and_Its_Double_Exist.java
@@ -2,6 +2,16 @@ package leetcode;
 
 public class LC1346_Check_If_N_and_Its_Double_Exist {
     public boolean checkIfExist(int[] arr) {
-        return true;
+        int length = arr.length;
+
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (arr[i] == arr[j] * 2 || arr[i] * 2 == arr[j]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
