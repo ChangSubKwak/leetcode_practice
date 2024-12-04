7e53c973be1af49d6a1f9f72220f4a64ad25e7a0
changsubkwak <paradise1732@gmail.com>
2024-12-04 23:29:47 +0900

    feat: (WIP) solving - 2825. Make String a Subsequence Using Cyclic Increments

---
+++ src/main/java/leetcode/LC2825_Make_String_a_Subsequence_Using_Cyclic_Increments.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC2825_Make_String_a_Subsequence_Using_Cyclic_Increments {
+    public boolean canMakeSubsequence(String str1, String str2) {
+        return true;
+    }
+}
