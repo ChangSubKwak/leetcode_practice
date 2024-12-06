3192fdb9d1f72367c98287ce1afffcb0d86da16a
changsubkwak <paradise1732@gmail.com>
2024-12-06 22:46:01 +0900

    feat: (WIP) solving - 2554. Maximum Number of Integers to Choose From a Range I

---
+++ src/main/java/leetcode/LC2554_Maximum_Number_of_Integers_to_Choose_From_a_Range_I.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class LC2554_Maximum_Number_of_Integers_to_Choose_From_a_Range_I {
+    public int maxCount(int[] banned, int n, int maxSum) {
+        Arrays.sort(banned);
+        return 0;
+    }
+}
