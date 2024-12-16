b9302a6cb23aecf2d00d6502ca600dab0043a375
changsubkwak <paradise1732@gmail.com>
2024-12-16 22:28:28 +0900

    feat: (WIP) solving - 3264. Final Array State After K Multiplication Operations I

---
+++ src/main/java/leetcode/LC3264_Final_Array_State_After_K_Multiplication_Operations_I.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC3264_Final_Array_State_After_K_Multiplication_Operations_I {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        return null;
+    }
+}
