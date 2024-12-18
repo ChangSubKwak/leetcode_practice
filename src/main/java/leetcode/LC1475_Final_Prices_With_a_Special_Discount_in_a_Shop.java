8290ddd40aeb9f03f24bfb7f1e566b9528740e79
changsubkwak <paradise1732@gmail.com>
2024-12-18 22:16:50 +0900

    feat: solved - 1475. Final Prices With a Special Discount in a Shop

---
+++ src/main/java/leetcode/LC1475_Final_Prices_With_a_Special_Discount_in_a_Shop.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class LC1475_Final_Prices_With_a_Special_Discount_in_a_Shop {
+    public int[] finalPrices(int[] prices) {
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[i] >= prices[j]) {
+                    prices[i] -= prices[j];
+                    break;
+                }
+            }
+        }
+        return prices;
+    }
+}
