87aee507c15ea6e7988ec8ce191909a657cb5526
changsubkwak <paradise1732@gmail.com>
2024-12-21 15:40:47 +0900

    feat: (WIP) solving - 2872. Maximum Number of K-Divisible Components

---
+++ src/main/java/leetcode/LC2872_Maximum_Number_of_K_Divisible_Components.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC2872_Maximum_Number_of_K_Divisible_Components {
+    public int maxKDivisibleComponents(int n, int[][] edges, int[] values, int k) {
+        return 2;
+    }
+}
