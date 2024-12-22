00366ca372d46fa94c6d9fcc5f7c1ddf9cb30789
changsubkwak <paradise1732@gmail.com>
2024-12-22 23:19:01 +0900

    feat: (WIP) solving - 2940. Find Building Where Alice and Bob Can Meet

---
+++ src/main/java/leetcode/LC2940_Find_Building_Where_Alice_and_Bob_Can_Meet.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC2940_Find_Building_Where_Alice_and_Bob_Can_Meet {
+    public int[] leftmostBuildingQueries(int[] heights, int[][] queries) {
+        return null;
+    }
+}
