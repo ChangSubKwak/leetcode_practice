145ab844856373e8c234cbb1327ed688dceb9101
changsubkwak <paradise1732@gmail.com>
2024-12-23 21:12:46 +0900

    feat: (WIP) solving - 2471. Minimum Number of Operations to Sort a Binary Tree by Level

---
+++ src/main/java/leetcode/LC2471_Minimum_Number_of_Operations_to_Sort_a_Binary_Tree_by_Level.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC2471_Minimum_Number_of_Operations_to_Sort_a_Binary_Tree_by_Level {
+    public int minimumOperations(TreeNode root) {
+        return 0;
+    }
+}
