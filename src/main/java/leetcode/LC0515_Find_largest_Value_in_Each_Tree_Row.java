6a08babe9ce7ac5d41ece5bd09a24f43a410cb1d
changsubkwak <paradise1732@gmail.com>
2024-12-25 14:09:59 +0900

    feat: (WIP) solving - 515. Find Largest Value in Each Tree Row

---
+++ src/main/java/leetcode/LC0515_Find_largest_Value_in_Each_Tree_Row.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+import java.util.List;
+
+public class LC0515_Find_largest_Value_in_Each_Tree_Row {
+    public List<Integer> largestValues(TreeNode root) {
+        return null;
+    }
+}
