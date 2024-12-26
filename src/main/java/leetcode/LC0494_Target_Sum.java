79d181e7af2b27ef2b510d2fb4b1b02cfdf7344f
changsubkwak <paradise1732@gmail.com>
2024-12-27 00:00:45 +0900

    feat: solved - 494. Target Sum

--- src/main/java/leetcode/LC0494_Target_Sum.java
+++ src/main/java/leetcode/LC0494_Target_Sum.java
@@ -1,7 +1,18 @@
 package leetcode;
 
 public class LC0494_Target_Sum {
+    private int recur(int[] nums, int target, int sum, int depth) {
+        if (depth == nums.length) {
+            return sum == target ? 1 : 0;
+        }
+
+        int plus  = recur(nums, target, sum + nums[depth], depth + 1);
+        int minus = recur(nums, target, sum - nums[depth], depth + 1);
+
+        return plus + minus;
+    }
+
     public int findTargetSumWays(int[] nums, int target) {
-        return 1;
+        return recur(nums, target, 0, 0);
     }
 }
