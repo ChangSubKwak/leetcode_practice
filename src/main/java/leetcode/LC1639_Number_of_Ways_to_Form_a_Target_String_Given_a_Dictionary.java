9c611c5864945e376b30d93fd744da3e7782772d
changsubkwak <paradise1732@gmail.com>
2024-12-29 20:44:24 +0900

    feat: (WIP) solving - 1639. Number of Ways to Form a Target String Given a Dictionary

---
+++ src/main/java/leetcode/LC1639_Number_of_Ways_to_Form_a_Target_String_Given_a_Dictionary.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC1639_Number_of_Ways_to_Form_a_Target_String_Given_a_Dictionary {
+    public int numWays(String[] words, String target) {
+        return 0;
+    }
+}
