afd64a560a59285ca8a2fb54c43dfdd7f46e0b56
changsubkwak <paradise1732@gmail.com>
2024-12-30 23:27:58 +0900

    test: fix error - 1639. Number of Ways to Form a Target String Given a Dictionary

--- src/test/java/leetcode/LC1639_Number_of_Ways_to_Form_a_Target_String_Given_a_Dictionary_Test.java
+++ src/test/java/leetcode/LC1639_Number_of_Ways_to_Form_a_Target_String_Given_a_Dictionary_Test.java
@@ -2,8 +2,6 @@ package leetcode;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LC1639_Number_of_Ways_to_Form_a_Target_String_Given_a_Dictionary_Test {
@@ -11,7 +9,7 @@ class LC1639_Number_of_Ways_to_Form_a_Target_String_Given_a_Dictionary_Test {
     void test() {
         LC1639_Number_of_Ways_to_Form_a_Target_String_Given_a_Dictionary test = new LC1639_Number_of_Ways_to_Form_a_Target_String_Given_a_Dictionary();
 
-        assertThat(test.numWays(Arrays.asList("acca", "bbbb", "caca"), "aba")).isEqualTo(6);
-        assertThat(test.numWays(Arrays.asList("abba", "baab"), "bab")).isEqualTo(4);
+        assertThat(test.numWays(new String[]{"acca", "bbbb", "caca"}, "aba")).isEqualTo(6);
+        assertThat(test.numWays(new String[]{"abba", "baab"}, "bab")).isEqualTo(4);
     }
 }
