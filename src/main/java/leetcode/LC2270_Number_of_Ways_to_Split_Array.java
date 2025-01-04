798be8005ba973a25702ce706727535c818a33dd
changsubkwak <paradise1732@gmail.com>
2025-01-04 23:52:59 +0900

    feat: (WIP) solving - 2270. Number of Ways to Split Array

--- src/main/java/leetcode/LC2270_Number_of_Ways_to_Split_Array.java
+++ src/main/java/leetcode/LC2270_Number_of_Ways_to_Split_Array.java
@@ -1,20 +1,7 @@
 package leetcode;
 
-import static org.assertj.core.api.Assertions.*;
-
-import org.junit.jupiter.api.*;
-
-public class LC2270_Number_of_Ways_To_Split_Array_Test {
-	LC2270_Number_of_Ways_To_Split_Array test;
-	
-	@BeforeEach
-	void setUp() {
-		test = new LC2270_Number_of_Ways_To_Split_Array();
-	}
-	
-	@Test
-	void test() {
-		assertThat(test.waysToSplitArray(new int[]{10, 4, -8, 7})).isEqualTo(2);
-		assertThat(test.waysToSplitArray(new int[]{2, 3, 1, 0})).isEqualTo(2);
-	}
+public class LC2270_Number_of_Ways_to_Split_Array {
+    public int waysToSplitArray(int[] nums) {
+        return 0;
+    }
 }
