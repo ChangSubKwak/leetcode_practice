732b9b386d783d40fb9c39dcada3dd2002b4dd72
changsubkwak <paradise1732@gmail.com>
2025-01-07 01:03:26 +0900

    feat: (WIP) solving - 1769. Minimum Number of Operations to Move All Balls to Each Box

---
+++ src/main/java/leetcode/LC1769_Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC1769_Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box {
+    public int[] minOperations(String boxes) {
+        return new int[]{1, 2, 3};
+    }
+}
