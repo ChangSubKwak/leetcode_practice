fcda26a0a0e699a96cf96d38e765ee1bc166f455
changsubkwak <paradise1732@gmail.com>
2025-01-09 22:11:31 +0900

    feat: (WIP) solving - 2185. Counting Words With a Given Prefix

---
+++ src/main/java/leetcode/LC2185_Counting_Words_With_a_Given_Prefix.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class LC2185_Counting_Words_With_a_Given_Prefix {
+    public int prefixCount(String[] words, String pref) {
+        int count = 0;
+        for (String word : words) {
+            if (word.startsWith(pref)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
