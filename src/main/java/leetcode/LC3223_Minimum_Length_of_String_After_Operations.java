a04df84f9f878f05a8c3ea5ea9dd1039a537cff1
changsubkwak <paradise1732@gmail.com>
2025-01-14 01:28:55 +0900

    feat: solved - 3223. Minimum Length of String After Operations

--- src/main/java/leetcode/LC3223_Minimum_Length_of_String_After_Operations.java
+++ src/main/java/leetcode/LC3223_Minimum_Length_of_String_After_Operations.java
@@ -1,7 +1,25 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LC3223_Minimum_Length_of_String_After_Operations {
     public int minimumLength(String s) {
-        return 0;
+        Map<Character, Integer> charFrequencyMap = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            charFrequencyMap.put(c, charFrequencyMap.getOrDefault(c, 0) + 1);
+        }
+
+        int deleteCount = 0;
+        for (int frequency : charFrequencyMap.values()) {
+            if (frequency % 2 == 1) {
+                deleteCount += frequency - 1;
+                continue;
+            }
+
+            deleteCount += frequency - 2;
+        }
+
+        return s.length() - deleteCount;
     }
 }
