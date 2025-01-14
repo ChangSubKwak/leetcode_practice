d2068fd050d3d397b2251e1e6662848caa3138a2
changsubkwak <paradise1732@gmail.com>
2025-01-14 21:00:10 +0900

    solved - 2116. Check If a Parentheses String Can Be Valid

--- src/main/java/leetcode/LC2116_Check_If_a_Parentheses_String_Can_Be_Valid.java
+++ src/main/java/leetcode/LC2116_Check_If_a_Parentheses_String_Can_Be_Valid.java
@@ -1,7 +1,48 @@
 package leetcode;
 
 public class LC2116_Check_If_a_Parentheses_String_Can_Be_Valid {
-    public boolean canBeValiid(String s, String locked) {
-        return false;
+    public boolean canBeValid(String s, String locked) {
+        int length = s.length();
+
+        // If length of string is odd, return false.
+        if (length % 2 == 1) {
+            return false;
+        }
+
+        Stack<Integer> openBrackets = new Stack<>();
+        Stack<Integer> unlocked = new Stack<>();
+
+        // Iterate through the string to handle '(' and ')'
+        for (int i = 0; i < length; i++) {
+            if (locked.charAt(i) == '0') {
+                unlocked.push(i);
+            } else if (s.charAt(i) == '(') {
+                openBrackets.push(i);
+            } else if (s.charAt(i) == ')') {
+                if (!openBrackets.empty()) {
+                    openBrackets.pop();
+                } else if (!unlocked.empty()) {
+                    unlocked.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        // Match remaining open brackets with unlocked characters
+        while (
+            !openBrackets.empty() &&
+            !unlocked.empty() &&
+            openBrackets.peek() < unlocked.peek()
+        ) {
+            openBrackets.pop();
+            unlocked.pop();
+        }
+
+        if (!openBrackets.empty()) {
+            return false;
+        }
+
+        return true;
     }
 }
