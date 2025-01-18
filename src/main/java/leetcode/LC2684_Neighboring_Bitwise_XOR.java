3900bb9dfc311908db0e62972d6f58e910dacd24
changsubkwak <paradise1732@gmail.com>
2025-01-18 23:19:36 +0900

    feat: transcribed - 2684. Neighboring Bitwise XOR

--- src/main/java/leetcode/LC2684_Neighboring_Bitwise_XOR.java
+++ src/main/java/leetcode/LC2684_Neighboring_Bitwise_XOR.java
@@ -2,6 +2,20 @@ package leetcode;
 
 public class LC2684_Neighboring_Bitwise_XOR {
     public boolean doesValidArrayExist(int[] derived) {
-        return true;
+        int[] original = new int[derived.length + 1];
+        original[0] = 0;
+        for (int i = 0; i < derived.length; i++) {
+            original[i + 1] = derived[i] ^ original[i];
+        }
+
+        boolean checkForZero = (original[0] == original[original.length - 1]);
+
+        original[0] = 1;
+        for (int i = 0; i < derived.length; i++) {
+            original[i + 1] = derived[i] ^ original[i];
+        }
+        boolean checkForOne = (original[0] == original[original.length - 1]);
+
+        return checkForZero || checkForOne;
     }
 }
