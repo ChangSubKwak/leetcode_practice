5da76ebcf9c4302ad84ece5a6314198b0fb3052f
changsubkwak <paradise1732@gmail.com>
2025-01-27 22:32:32 +0900

    feat: (WIP) solving - 1462. Course Schedule IV

---
+++ src/main/java/leetcode/LC1462_Course_Schedule_IV.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+import java.util.List;
+
+public class LC1462_Course_Schedule_IV {
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        return null;
+    }
+}
