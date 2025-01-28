e757068890ea301d1a4fab6db3161ebb25a8eb2f
changsubkwak <paradise1732@gmail.com>
2025-01-28 23:29:59 +0900

    feat: (WIP) add variable - 1462. Course Schedule IV

--- src/main/java/leetcode/LC1462_Course_Schedule_IV.java
+++ src/main/java/leetcode/LC1462_Course_Schedule_IV.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class LC1462_Course_Schedule_IV {
     public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
         Map<Integer, List<Integer>> adjList = new HashMap<>();
+        
         return null;
     }
 }
