39ba747bccd383df0a1f86afe593a799237f2cdb
changsubkwak <paradise1732@gmail.com>
2025-01-29 22:41:41 +0900

    feat: (WIP) implement constructing map code - 1462. Course Schedule IV

--- src/main/java/leetcode/LC1462_Course_Schedule_IV.java
+++ src/main/java/leetcode/LC1462_Course_Schedule_IV.java
@@ -6,6 +6,11 @@ public class LC1462_Course_Schedule_IV {
     public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
         Map<Integer, List<Integer>> adjList = new HashMap<>();
         
+        for (int[] edge : prerequisites) {
+            adjList.computeIfAbsent(edge[0], k -> new ArrayList<>())
+                .add(edge[1]);
+        }
+
         return null;
     }
 }
