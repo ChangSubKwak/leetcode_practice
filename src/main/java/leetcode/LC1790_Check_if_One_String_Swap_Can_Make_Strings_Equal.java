3d9e0f8a2563661f21c705805cb84dcaf3730944
changsubkwak <paradise1732@gmail.com>
2025-02-06 23:03:28 +0900

    feat: solved - 1790. Check if One String Swap Can Make Strings Equal

--- src/main/java/leetcode/LC1790_Check_if_One_String_Swap_Can_Make_Strings_Equal.java
+++ src/main/java/leetcode/LC1790_Check_if_One_String_Swap_Can_Make_Strings_Equal.java
@@ -1,7 +1,36 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LC1790_Check_if_One_String_Swap_Can_Make_Strings_Equal {
     public boolean areAlmostEqual(String s1, String s2) {
+        int length = s1.length();
+        int countIfDifference = 0;
+
+        Map<Character, Integer> count1 = new HashMap<>();
+        Map<Character, Integer> count2 = new HashMap<>();
+
+        for (int i = 0; i < length; i++) {
+            count1.put(s1.charAt(i), count1.getOrDefault(s1.charAt(i), 0) + 1);
+            count2.put(s2.charAt(i), count2.getOrDefault(s2.charAt(i), 0) + 1);
+
+            if (s1.charAt(i) != s2.charAt(i)) {
+                countIfDifference++;
+            }
+
+            if (countIfDifference > 2) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            char c = (char)('a' + i);
+            if (count1.get(c) != count2.get(c)) {
+                return false;
+            }
+        }
+
         return true;
     }
 }
