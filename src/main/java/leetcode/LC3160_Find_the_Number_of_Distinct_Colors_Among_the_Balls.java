3c3d8a3e691b2d144e4d0666c37d7de1bb2730d6
changsubkwak <paradise1732@gmail.com>
2025-02-07 23:37:17 +0900

    feat: (WIP) solving - 3160. Find the Number of Distinct Colors Among the Balls

---
+++ src/main/java/leetcode/LC3160_Find_the_Number_of_Distinct_Colors_Among_the_Balls.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC3160_Find_the_Number_of_Distinct_Colors_Among_the_Balls {
+    public int[] queryResults(int limit, int[][] queries) {
+        return null;
+    }
+}
