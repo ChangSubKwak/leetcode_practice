12b1e3b1b0e74fd8254c7d5318186e4c00d169ae
changsubkwak <paradise1732@gmail.com>
2025-02-08 23:34:27 +0900

    feat: (WIP) solving - 2349. Design. a Number Container System

---
+++ src/main/java/leetcode/LC2349_Design_a_Number_Container_System.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class LC2349_Design_a_Number_Container_System {
+    static class NumberContainers {
+
+        public NumberContainers() {
+
+        }
+
+        public void change(int index, int number) {
+
+        }
+
+        public int find(int number) {
+            return 0;
+        }
+    }
+}
