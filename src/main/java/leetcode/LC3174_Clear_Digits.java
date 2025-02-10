78c0b9b556324ca75b05f8d6a4366036e64dc862
changsubkwak <paradise1732@gmail.com>
2025-02-11 07:31:39 +0900

    feat: solved - 3174. Clear Digits

--- src/main/java/leetcode/LC3174_Clear_Digits.java
+++ src/main/java/leetcode/LC3174_Clear_Digits.java
@@ -2,6 +2,17 @@ package leetcode;
 
 public class LC3174_Clear_Digits {
     public String clearDigits(String s) {
-        return "";
+        int i = 0;
+        StringBuilder sb = new StringBuilder(s);
+        while (i < sb.length()) {
+            if (i >= 1 && Character.isDigit(sb.charAt(i))) {
+                sb = new StringBuilder(sb.substring(0, i - 1) + sb.substring(i + 1, sb.length()));
+                i--;
+                continue;
+            }
+            i++;
+        }
+
+        return sb.toString();
     }
 }
