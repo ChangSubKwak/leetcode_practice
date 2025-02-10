882207e17376948a444f0c704686ea524671f93e
changsubkwak <paradise1732@gmail.com>
2025-02-11 07:33:26 +0900

    test: implement test case - 2116. Check If a Parentheses String Can Be Valid

--- src/test/java/leetcode/LC2116_Check_If_a_Parentheses_String_Can_Be_Valid_Test.java
+++ src/test/java/leetcode/LC2116_Check_If_a_Parentheses_String_Can_Be_Valid_Test.java
@@ -9,8 +9,8 @@ public class LC2116_Check_If_a_Parentheses_String_Can_Be_Valid_Test {
     void exampleTest() {
         LC2116_Check_If_a_Parentheses_String_Can_Be_Valid test = new LC2116_Check_If_a_Parentheses_String_Can_Be_Valid();
         
-        assertThat(test.canBeValiid("))()))", "010100")).isEqualTo(true);
-        assertThat(test.canBeValiid("()()", "0000")).isEqualTo(true);
-        assertThat(test.canBeValiid(")", "0")).isEqualTo(false);
+        assertThat(test.canBeValid("))()))", "010100")).isEqualTo(true);
+        assertThat(test.canBeValid("()()", "0000")).isEqualTo(true);
+        assertThat(test.canBeValid(")", "0")).isEqualTo(false);
     }
 }
