86d4b32231e9d9eb7ffd85552b72cb1a171b0aaf
changsubkwak <paradise1732@gmail.com>
2025-02-15 01:46:44 +0900

    feat: solved - 3066. Minimum Operations to Exceed Threshold Value II

--- src/main/java/leetcode/LC3066_Minimum_Operations_to_Exceed_Threshold_Value_II.java
+++ src/main/java/leetcode/LC3066_Minimum_Operations_to_Exceed_Threshold_Value_II.java
@@ -1,7 +1,26 @@
 package leetcode;
 
+import java.util.PriorityQueue;
+
 public class LC3066_Minimum_Operations_to_Exceed_Threshold_Value_II {
     public int minOperations(int[] nums, int k) {
-        return 0;
+        PriorityQueue<Long> minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            minHeap.add((long)num);
+        }
+
+        int count = 0;
+        while (k > minHeap.peek()) {
+            long x = minHeap.poll();
+            long y = minHeap.poll();
+            long newValue = Math.min(x, y) * 2 + Math.max(x, y);
+
+            minHeap.add(newValue);
+
+            count++;
+        }
+
+        return count;
     }
 }
