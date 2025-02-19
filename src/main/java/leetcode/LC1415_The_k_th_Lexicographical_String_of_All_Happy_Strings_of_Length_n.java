5c5d3db9bd159336e57681e0fe26836bb506aa06
changsubkwak <paradise1732@gmail.com>
2025-02-20 00:06:18 +0900

    feat: sovled - 1415. The k-th Lexicographical String of All Happy Strings of Length n

--- src/main/java/leetcode/LC1415_The_k_th_Lexicographical_String_of_All_Happy_Strings_of_Length_n.java
+++ src/main/java/leetcode/LC1415_The_k_th_Lexicographical_String_of_All_Happy_Strings_of_Length_n.java
@@ -1,7 +1,39 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class LC1415_The_k_th_Lexicographical_String_of_All_Happy_Strings_of_Length_n {
+    List<String> happyStrings;
+
     public String getHappyString(int n, int k) {
-        return "";
+        String currentString = "";
+        happyStrings = new ArrayList<>();
+        generateHappyStrings(n, currentString);
+
+        if (happyStrings.size() < k) {
+            return "";
+        }
+
+        Collections.sort(happyStrings);
+
+        return happyStrings.get(k - 1);
+    }
+
+    protected void generateHappyStrings(int n, String currentString) {
+        if (currentString.length() == n) {
+            happyStrings.add(currentString);
+            return;
+        }
+
+        for (char currentChar = 'a'; currentChar <= 'c'; currentChar++) {
+            if (
+                currentString.length() > 0 &&
+                currentString.charAt(currentString.length() - 1) == currentChar
+            ) continue;
+
+            generateHappyStrings(n, currentString + currentChar);
+        }
     }
 }
