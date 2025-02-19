2f7dd6872b1fde48bbd88c89fb8c9b8f82afc200
changsubkwak <paradise1732@gmail.com>
2025-02-19 23:45:17 +0900

    feat: (WIP) sovling - 1415. The k-th Lexicographical String of All Happy Strings of Length n

---
+++ src/main/java/leetcode/LC1415_The_k_th_Lexicographical_String_of_All_Happy_Strings_of_Length_n.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC1415_The_k_th_Lexicographical_String_of_All_Happy_Strings_of_Length_n {
+    public String getHappyString(int n, int k) {
+        return "";
+    }
+}
