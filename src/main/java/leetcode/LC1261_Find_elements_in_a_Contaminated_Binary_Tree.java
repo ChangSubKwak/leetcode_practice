e278913ab98036ff43c59f01eb4d362f4e1b6a0d
changsubkwak <paradise1732@gmail.com>
2025-02-21 12:37:04 +0900

    feat: (WIP) solving - 1261. Find Elements in a Contaminated Binary Tree

---
+++ src/main/java/leetcode/LC1261_Find_elements_in_a_Contaminated_Binary_Tree.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.util.Set;
+
+public class LC1261_Find_elements_in_a_Contaminated_Binary_Tree {
+    public static class FindElements {
+        Set<Integer> set;
+
+        public FindElements(TreeNode root) {
+        }
+
+        public boolean find(int target) {
+            return true;
+        }
+    }
+}
