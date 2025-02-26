73b850bdc3ae973753d4113fb30a4b646c3957dd
changsubkwak <paradise1732@gmail.com>
2025-02-27 02:00:47 +0900

    feat: solved - 889. Construct Binary Tree from Preorder and Postorder Traversal

--- src/main/java/leetcode/LC1749_Maximum_Absolute_Sum_of_Any_Subarray.java
+++ src/main/java/leetcode/LC1749_Maximum_Absolute_Sum_of_Any_Subarray.java
@@ -2,6 +2,29 @@ package leetcode;
 
 public class LC1749_Maximum_Absolute_Sum_of_Any_Subarray {
     public int maxAbsoluteSum(int[] nums) {
-        return 0;
+        int minPrefixSum = Integer.MAX_VALUE;
+        int maxPrefixSum = Integer.MIN_VALUE;
+        int prefixSum = 0;
+        int maxAbsSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            prefixSum += nums[i];
+            minPrefixSum = Math.min(minPrefixSum, prefixSum);
+            maxPrefixSum = Math.max(maxPrefixSum, prefixSum);
+
+            if (prefixSum >= 0) {
+                maxAbsSum = Math.max(maxAbsSum, prefixSum);
+                maxAbsSum = Math.max(maxAbsSum, prefixSum - minPrefixSum);
+                continue;
+            }
+
+            if (prefixSum < 0) {
+                maxAbsSum = Math.max(maxAbsSum, Math.abs(prefixSum));
+                maxAbsSum = Math.max(maxAbsSum, Math.abs(prefixSum - maxPrefixSum));
+                continue;
+            }
+        }
+
+        return maxAbsSum;
     }
 }
