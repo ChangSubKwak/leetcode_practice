ff5cbda0523cb736326b3058e6a38e44196b2552
changsubkwak <paradise1732@gmail.com>
2025-03-02 00:41:28 +0900

    fix: change test case value - 2460. Apply Operations to an Array

--- src/test/java/leetcode/LC2460_Apply_Operations_to_an_Array_Test.java
+++ src/test/java/leetcode/LC2460_Apply_Operations_to_an_Array_Test.java
@@ -11,6 +11,6 @@ public class LC2460_Apply_Operations_to_an_Array_Test {
         LC2460_Apply_Operations_to_an_Array test = new LC2460_Apply_Operations_to_an_Array();
 
         assertThat(test.applyOperations(new int[]{1, 2, 2, 1, 1, 0})).isEqualTo(new int[]{1, 4, 2, 0, 0, 0});
-        assertThat(test.applyOperations(new int[]{1, 0})).isEqualTo(new int[]{0, 1});
+        assertThat(test.applyOperations(new int[]{0, 1})).isEqualTo(new int[]{1, 0});
     }
 }
