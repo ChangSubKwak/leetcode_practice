36d34f639a23cbcbf5832bc90d3c614988be7819
changsubkwak <paradise1732@gmail.com>
2025-03-10 01:17:07 +0900

    feat: sovled - 3208. Alternating Groups II

--- src/main/java/leetcode/LC3208_Alternating_Groups_II.java
+++ src/main/java/leetcode/LC3208_Alternating_Groups_II.java
@@ -2,6 +2,36 @@ package leetcode;
 
 public class LC3208_Alternating_Groups_II {
     public int numberOfAlternatingGroups(int[] colors, int k) {
-        return 0;
+        int[] extendedColors = new int[colors.length + k - 1];
+        for (int i = 0; i < colors.length; i++) {
+            extendedColors[i] = colors[i];
+        }
+
+        for (int i = 0; i < k - 1; i++) {
+            extendedColors[colors.length + i] = colors[i];
+        }
+
+        int length = extendedColors.length;
+        int result = 0;
+        int left = 0;
+        int right = 1;
+
+        while (right < length) {
+            if (extendedColors[right] == extendedColors[right - 1]) {
+                left = right;
+                right++;
+                continue;
+            }
+            right++;
+
+            if (right - left < k) {
+                continue;
+            }
+
+            result++;
+            left++;
+        }
+
+        return result;
     }
 }
