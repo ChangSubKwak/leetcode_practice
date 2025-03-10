37ff54a96cb92a4dd3ca4a1a67e14774c052bf4f
changsubkwak <paradise1732@gmail.com>
2025-03-11 02:41:05 +0900

    feat: (WIP) solving - 3306. Count of Substrings Containing Every Vowel and K Consonants II

---
+++ src/main/java/leetcode/LC3306_Count_of_Substrings_Containing_Every_Vowel_and_K_Consonants_II.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC3306_Count_of_Substrings_Containing_Every_Vowel_and_K_Consonants_II {
+    public long countOfSubstrings(String word, int k) {
+        return 0L;
+    }
+}
