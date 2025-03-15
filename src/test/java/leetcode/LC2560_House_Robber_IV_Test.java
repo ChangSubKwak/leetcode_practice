0c0293f58930b61606fd59a479860d8937b04518
changsubkwak <paradise1732@gmail.com>
2025-03-15 23:32:03 +0900

    test: implement test case - 2560. House Robber IV

---
+++ src/test/java/leetcode/LC2560_House_Robber_IV_Test.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LC2560_House_Robber_IV_Test {
+    @Test
+    void test() {
+        LC2560_House_Robber_IV test = new LC2560_House_Robber_IV();
+
+        assertThat(test.minCapability(new String[]{2, 3, 5, 9}, 2)).isEqualTo(5);
+        assertThat(test.minCapability(new String[]{2, 7, 9, 3, 1}, 2)).isEqualTo(2);
+
+    }
+}
