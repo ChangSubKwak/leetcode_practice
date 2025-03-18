ab722d37f27c0121b817cca57c74d43ca3840268
changsubkwak <paradise1732@gmail.com>
2025-03-18 21:21:46 +0900

    feat: solved - 2206. Divide Array Into Equal Pairs

--- src/main/java/leetcode/LC2206_Divide_Array_Into_Equal_Pairs.java
+++ src/main/java/leetcode/LC2206_Divide_Array_Into_Equal_Pairs.java
@@ -1,7 +1,23 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LC2206_Divide_Array_Into_Equal_Pairs {
     public boolean divideArray(int[] nums) {
-        return false;
+        Map<Integer, Integer> count = new HashMap<>();
+
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            // System.out.println(entry);
+            if (entry.getValue() % 2 != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
