2115ed541ba438950593c0d571fd02caeccc8e33
changsubkwak <paradise1732@gmail.com>
2025-03-18 21:22:54 +0900

    fix: change type String 2 int - 2560. House Robber IV

--- src/test/java/leetcode/LC2560_House_Robber_IV_Test.java
+++ src/test/java/leetcode/LC2560_House_Robber_IV_Test.java
@@ -9,8 +9,8 @@ public class LC2560_House_Robber_IV_Test {
     void test() {
         LC2560_House_Robber_IV test = new LC2560_House_Robber_IV();
 
-        assertThat(test.minCapability(new String[]{2, 3, 5, 9}, 2)).isEqualTo(5);
-        assertThat(test.minCapability(new String[]{2, 7, 9, 3, 1}, 2)).isEqualTo(2);
+        assertThat(test.minCapability(new int[]{2, 3, 5, 9}, 2)).isEqualTo(5);
+        assertThat(test.minCapability(new int[]{2, 7, 9, 3, 1}, 2)).isEqualTo(2);
 
     }
 }
