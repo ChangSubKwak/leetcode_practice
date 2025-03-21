aee49e5dd7585f0730d4d83a45741ac9f7ead565
changsubkwak <paradise1732@gmail.com>
2025-03-21 23:46:22 +0900

    feat: (WIP) solving - 2115. Find All Possible Recipes from Given Supplies

---
+++ src/main/java/leetcode/LC2115_Find_All_Possible_Recipes_from_Given_Supplies.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+import java.util.List;
+
+public class LC2115_Find_All_Possible_Recipes_from_Given_Supplies {
+    public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+        return null;
+    }
+}
