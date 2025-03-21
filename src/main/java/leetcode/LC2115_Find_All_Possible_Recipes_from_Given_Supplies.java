c0d8daeebc9da2ec4f28a9e62a796f2c274ea06e
changsubkwak <paradise1732@gmail.com>
2025-03-22 00:13:27 +0900

    feat: solved - 2115. Find All Possible Recipes from Given Supplies

--- src/main/java/leetcode/LC2115_Find_All_Possible_Recipes_from_Given_Supplies.java
+++ src/main/java/leetcode/LC2115_Find_All_Possible_Recipes_from_Given_Supplies.java
@@ -1,9 +1,42 @@
 package leetcode;
 
-import java.util.List;
+import java.util.*;
 
 public class LC2115_Find_All_Possible_Recipes_from_Given_Supplies {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        return null;
+        LinkedList<String> recipesList = new LinkedList<>(Arrays.asList(recipes));
+
+        Map<String, Set<String>> ingredientsMap = new HashMap<>();
+        for (int i = 0; i < ingredients.size(); i++) {
+            ingredientsMap.put(recipes[i], new HashSet<>(ingredients.get(i)));
+        }
+
+        Set<String> suppliesSet = new HashSet<>();
+        for (int i = 0; i < supplies.length; i++) {
+            suppliesSet.add(supplies[i]);
+        }
+
+        List<String> result = new ArrayList<>();
+        while (true) {
+            boolean isNecessaryBreak = true;
+            for (int i = 0; i < recipesList.size(); i++) {
+                if (suppliesSet.containsAll(ingredients.get(i))) {
+                    suppliesSet.add(recipesList.get(i));
+                    result.add(recipesList.get(i));
+
+                    recipesList.remove(i);
+                    ingredients.remove(i);
+
+                    isNecessaryBreak = false;
+                    break;
+                }
+            }
+
+            if (isNecessaryBreak) {
+                break;
+            }
+        }
+
+        return result;
     }
 }
