be0839f0368f5efa83003cdd59f1b5abb766929f
changsubkwak <paradise1732@gmail.com>
2025-03-28 22:27:39 +0900

    test: implement test case - 2503. Maximum Number of Points From Grid Queries

---
+++ src/test/java/leetcode/LC2503_Maximum_Number_of_Points_From_Grid_Queries_Test.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LC2503_Maximum_Number_of_Points_From_Grid_Queries_Test {
+    @Test
+    void test() {
+        LC2503_Maximum_Number_of_Points_From_Grid_Queries test = new LC2503_Maximum_Number_of_Points_From_Grid_Queries();
+
+        assertThat(test.maxPoints(new int[][]{{1, 2, 3}, {2, 5, 7}, {3, 5, 1}})).isEqualTo(new int[]{5, 6, 2});
+        assertThat(test.maxPoints(new int[][]{{5, 2, 1}, {1, 1, 2}})).isEqualTo(new int[]{3});
+    }
+}
