8e901e4856d1cb0b0bab0c25e92755777c2ca16f
changsubkwak <paradise1732@gmail.com>
2025-03-29 23:56:30 +0900

    feat: (WIP) sovling #2 - 2503. Maximum Number of Points From Grid Queries

--- src/main/java/leetcode/LC2503_Maximum_Number_of_Points_From_Grid_Queries.java
+++ src/main/java/leetcode/LC2503_Maximum_Number_of_Points_From_Grid_Queries.java
@@ -2,6 +2,10 @@ package leetcode;
 
 public class LC2503_Maximum_Number_of_Points_From_Grid_Queries {
     public int[] maxPoints(int[][] grid, int[] queries) {
+        int rowCount = grid.length, colCount = grid[0].length;
+        int[] result = new int[queries.length];
+        int[][] directions = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+
         return null;
     }
 }
