eae75a7a1b74d9c3047d9da62aaba09b4cc981ad
changsubkwak <paradise1732@gmail.com>
2025-03-30 23:50:23 +0900

    test: implement test case - 763. Partition Labels

---
+++ src/test/java/leetcode/LC0763_Partition_Labels_Test.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LC0763_Partition_Labels_Test {
+	@Test
+	void test() {
+		LC0763_Partition_Labels test = new LC0763_Partition_Labels();
+
+		assertThat(test.isToeplitzMatrix("ababcbacadefegdehijhklij")).isEqualTo(Arrays.asList(9, 7, 8));
+		assertThat(test.isToeplitzMatrix("eccbbbbdec")).isEqualTo(Arrays.asList(10));
+	}
+}
