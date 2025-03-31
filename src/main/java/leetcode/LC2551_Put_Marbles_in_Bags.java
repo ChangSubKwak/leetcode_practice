ee0ee9a23b226e8cd40d07aacc0362b261fb203d
changsubkwak <paradise1732@gmail.com>
2025-03-31 23:55:34 +0900

    feat: (WIP) sovling - 2551. Put Marbles in Bags

--- src/main/java/leetcode/LC2551_Put_Marbles_in_Bags.java
+++ src/main/java/leetcode/LC2551_Put_Marbles_in_Bags.java
@@ -2,6 +2,10 @@ package leetcode;
 
 public class LC2551_Put_Marbles_in_Bags {
     public long putMarbles(int[] weights, int k) {
+        int n = weights.length;
+        int[] pairWeights = new int[n - 1];
+
+
         return 0;
     }
 }
