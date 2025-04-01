a92942d966872fe0323e3581087afbf4d145139a
changsubkwak <paradise1732@gmail.com>
2025-04-02 03:36:38 +0900

    feat: sovled - 2140. Solving Questions With Brainpower

--- src/main/java/leetcode/LC2140_Solving_Questions_With_Brainpower.java
+++ src/main/java/leetcode/LC2140_Solving_Questions_With_Brainpower.java
@@ -1,17 +1,23 @@
 package leetcode;
 
 public class LC2140_Solving_Questions_With_Brainpower {
-    private long recur(long[] memo, int[][] questions, int start) {
-        int length = questions.length;
-        for (int i = start; i < length; i++) {
-            //memo[i] = Math.max(memo[i], questions[i][0]
-        }
-        return 0;
-    }
-
     public long mostPoints(int[][] questions) {
-        long[] memo = new long[questions.length];
+        int length = questions.length;
+        long[] memo = new long[length];
+        memo[length - 1] = questions[length - 1][0];
 
-        return recur(memo, questions, 0);
+        for (int i = length - 2; i >= 0; i--) {
+            memo[i] = questions[i][0];
+            int nextIndex = i + questions[i][1] + 1;
+
+            if (nextIndex < length) {
+                memo[i] += memo[nextIndex];
+            }
+
+            memo[i] = Math.max(memo[i], memo[i + 1]);
+            // System.out.println(i + " : " + memo[i] + " : " + memo[i + 1] + " : " + Arrays.toString(memo));
+        }
+
+        return memo[0];
     }
 }
