11feb9aaf245d8caa914c792481dfae4b5da948f
changsubkwak <paradise1732@gmail.com>
2025-04-02 03:37:28 +0900

    fix: modified wrong code

--- src/test/java/leetcode/LC0763_Partition_Labels_Test.java
+++ src/test/java/leetcode/LC0763_Partition_Labels_Test.java
@@ -11,7 +11,7 @@ public class LC0763_Partition_Labels_Test {
 	void test() {
 		LC0763_Partition_Labels test = new LC0763_Partition_Labels();
 
-		assertThat(test.isToeplitzMatrix("ababcbacadefegdehijhklij")).isEqualTo(Arrays.asList(9, 7, 8));
-		assertThat(test.isToeplitzMatrix("eccbbbbdec")).isEqualTo(Arrays.asList(10));
+		assertThat(test.partitionLabels("ababcbacadefegdehijhklij")).isEqualTo(Arrays.asList(9, 7, 8));
+		assertThat(test.partitionLabels("eccbbbbdec")).isEqualTo(Arrays.asList(10));
 	}
 }
--- src/test/java/leetcode/LC2503_Maximum_Number_of_Points_From_Grid_Queries_Test.java
+++ src/test/java/leetcode/LC2503_Maximum_Number_of_Points_From_Grid_Queries_Test.java
@@ -9,7 +9,7 @@ class LC2503_Maximum_Number_of_Points_From_Grid_Queries_Test {
     void test() {
         LC2503_Maximum_Number_of_Points_From_Grid_Queries test = new LC2503_Maximum_Number_of_Points_From_Grid_Queries();
 
-        assertThat(test.maxPoints(new int[][]{{1, 2, 3}, {2, 5, 7}, {3, 5, 1}})).isEqualTo(new int[]{5, 6, 2});
-        assertThat(test.maxPoints(new int[][]{{5, 2, 1}, {1, 1, 2}})).isEqualTo(new int[]{3});
+        assertThat(test.maxPoints(new int[][]{{1, 2, 3}, {2, 5, 7}, {3, 5, 1}}, new int[]{5, 6, 2})).isEqualTo(new int[]{5, 8, 1});
+        assertThat(test.maxPoints(new int[][]{{5, 2, 1}, {1, 1, 2}}, new int[]{3})).isEqualTo(new int[]{0});
     }
 }
