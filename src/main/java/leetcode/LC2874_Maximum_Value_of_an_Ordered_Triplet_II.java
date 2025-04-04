e16a37fcaa3b9d651095ca78e86c39fe744425cb
changsubkwak <paradise1732@gmail.com>
2025-04-04 23:06:57 +0900

    refactor: change class name - 2874. Maximum Value of an Ordered Triplet II

--- src/main/java/leetcode/LC2874_Maximum_Value_of_an_Ordered_Triplet_II.java
+++ src/main/java/leetcode/LC2874_Maximum_Value_of_an_Ordered_Triplet_II.java
@@ -1,6 +1,6 @@
 package leetcode;
 
-public class LC2873_Maximum_Value_of_an_Ordered_Triplet_II {
+public class LC2874_Maximum_Value_of_an_Ordered_Triplet_II {
     public long maximumTripletValue(int[] nums) {
         return 0L;
     }
--- src/test/java/leetcode/LC2874_Maximum_Value_of_an_Ordered_Triplet_II_Test.java
+++ src/test/java/leetcode/LC2874_Maximum_Value_of_an_Ordered_Triplet_II_Test.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class LC2873_Maximum_Value_of_an_Ordered_Triplet_II_Test {
+class LC2874_Maximum_Value_of_an_Ordered_Triplet_II_Test {
     @Test
     void test() {
-        LC2873_Maximum_Value_of_an_Ordered_Triplet_II test = new LC2873_Maximum_Value_of_an_Ordered_Triplet_II();
+        LC2874_Maximum_Value_of_an_Ordered_Triplet_II test = new LC2874_Maximum_Value_of_an_Ordered_Triplet_II();
 
         assertThat(test.maximumTripletValue(new int[]{12, 6, 1, 2, 7})).isEqualTo(77);
         assertThat(test.maximumTripletValue(new int[]{1, 10, 3, 4, 19})).isEqualTo(133);
