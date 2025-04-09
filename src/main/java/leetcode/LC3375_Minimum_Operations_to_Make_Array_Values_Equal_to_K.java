80eb712ba002ff40ed9975e1bf505c017b56eab6
changsubkwak <paradise1732@gmail.com>
2025-04-10 00:03:32 +0900

    feat: solved - 3375. Minimum Operations to Make Array Values Equal to K

--- src/main/java/leetcode/LC3375_Minimum_Operations_to_Make_Array_Values_Equal_to_K.java
+++ src/main/java/leetcode/LC3375_Minimum_Operations_to_Make_Array_Values_Equal_to_K.java
@@ -1,7 +1,25 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LC3375_Minimum_Operations_to_Make_Array_Values_Equal_to_K {
     public int minOperations(int[] nums, int k) {
-        return 0;
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (k > nums[i]) {
+                return -1;
+            }
+            set.add(nums[i]);
+        }
+
+        int count = 0;
+        for (Integer item : set) {
+            if (k < item) {
+                count++;
+            }
+        }
+        return count;
     }
 }
