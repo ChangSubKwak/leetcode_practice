9e6f79e6aa6a6f3998f2e5037b248a10dde4170e
changsubkwak <paradise1732@gmail.com>
2025-04-10 23:29:06 +0900

    feat: (WIP) sovling - feat: (WIP) sovling - 368. Largest Divisible Subset

---
+++ src/main/java/leetcode/LC2999_Count_the_Number_of_Powerful_Integers.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC2999_Count_the_Number_of_Powerful_Integers {
+    public long numberOfPowerfulInt(long start, long finish, int limit, String s) {
+        return 0L;
+    }
+}
