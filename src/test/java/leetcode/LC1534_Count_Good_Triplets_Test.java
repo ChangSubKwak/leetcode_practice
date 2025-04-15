aa16a3708cc787df39cb0ad62bd35da8755a8f97
changsubkwak <paradise1732@gmail.com>
2025-04-15 22:38:38 +0900

    fix: correct function name - 3375. Minimum Operations to Make Array Values Equal to K

--- src/test/java/leetcode/LC1534_Count_Good_Triplets_Test.java
+++ src/test/java/leetcode/LC1534_Count_Good_Triplets_Test.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class LC1534_Count_Good_Triplets_Test {
+class LC1534_Count_Good_Triplets_Test {
 
 	@Test
-	void test1() {
+	void exampleTest() {
 		LC1534_Count_Good_Triplets test = new LC1534_Count_Good_Triplets();
 
 		assertThat(test.countGoodTriplets(new int[]{3, 0, 1, 1, 9, 7}, 7, 2, 3)).isEqualTo(4);
