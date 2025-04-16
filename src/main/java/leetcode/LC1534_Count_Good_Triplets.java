6071da5a1d0d5ff1ca6f85a320e0685edee5314d
changsubkwak <paradise1732@gmail.com>
2025-04-16 23:13:36 +0900

    feat: solved - 1534. Count Good Triplets

--- src/main/java/leetcode/LC1534_Count_Good_Triplets.java
+++ src/main/java/leetcode/LC1534_Count_Good_Triplets.java
@@ -2,6 +2,23 @@ package leetcode;
 
 public class LC1534_Count_Good_Triplets {
     public int countGoodTriplets(int[] arr, int a, int b, int c) {
-        return 0;
+        int length = arr.length;
+        int count = 0;
+
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                for (int k = j + 1; k < length; k++) {
+                    if (
+                            Math.abs(arr[i] - arr[j]) <= a &&
+                                    Math.abs(arr[j] - arr[k]) <= b &&
+                                    Math.abs(arr[i] - arr[k]) <= c
+                    ) {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
     }
 }
