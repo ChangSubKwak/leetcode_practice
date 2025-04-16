81259c05d0c9cbcc16816efbe259a3b1252f8f2c
changsubkwak <paradise1732@gmail.com>
2025-04-16 23:14:15 +0900

    fix: change function name - 1922. Count Good Numbers

--- src/test/java/LC1922_Count_Good_Numbers_Test.java
+++ src/test/java/LC1922_Count_Good_Numbers_Test.java
@@ -7,8 +7,8 @@ class LC1922_Count_Good_Numbers_Test {
     void examlpeTest() {
         LC1922_Count_Good_Numbers test = new LC1922_Count_Good_Numbers();
 
-        assertThat(test.countgoodNumbers(1)).isEqualTo(5);
-        assertThat(test.countgoodNumbers(4)).isEqualTo(400);
-        assertThat(test.countgoodNumbers(50)).isEqualTo(564908303);
+        assertThat(test.countGoodNumbers(1)).isEqualTo(5);
+        assertThat(test.countGoodNumbers(4)).isEqualTo(400);
+        assertThat(test.countGoodNumbers(50)).isEqualTo(564908303);
     }
 }
