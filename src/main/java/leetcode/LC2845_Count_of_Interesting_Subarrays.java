cf760f433e4847a0b72b996cfef8fb00fcab6ea6
changsubkwak <paradise1732@gmail.com>
2025-04-25 22:54:16 +0900

    feat: (WIP) solving - 2845. Count of Interesting Subarrays

---
+++ src/main/java/leetcode/LC2845_Count_of_Interesting_Subarrays.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+import java.util.List;
+
+public class LC2845_Count_of_Interesting_Subarrays {
+    public long countInterestingSubarrays(List<Integer> nums, int modulo, int k) {
+        return 0;
+    }
+}
