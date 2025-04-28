5782407f7437c3d9fcffa5deb60177c2763663ab
changsubkwak <paradise1732@gmail.com>
2025-04-28 22:22:00 +0900

    feat: solved - 3392. Count Subarrays of Length Three With a Condition

--- src/main/java/leetcode/LC3392_Count_Subarrays_of_Length_Three_With_a_Condition.java
+++ src/main/java/leetcode/LC3392_Count_Subarrays_of_Length_Three_With_a_Condition.java
@@ -2,6 +2,13 @@ package leetcode;
 
 public class LC3392_Count_Subarrays_of_Length_Three_With_a_Condition {
     public int countSubarrays(int[] nums) {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i + 1] == (nums[i] + nums[i + 2]) * 2) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
