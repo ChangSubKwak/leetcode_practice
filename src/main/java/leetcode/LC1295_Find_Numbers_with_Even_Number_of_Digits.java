8a5232095100edd08cc4eeaeb080508f264e61d4
changsubkwak <paradise1732@gmail.com>
2025-05-01 00:15:01 +0900

    feat: - solved - 1295. Find Numbers with Even Number of Digits - add production code - 2962. Count Subarrays Where Max Element Appears at Least K Times

--- src/main/java/leetcode/LC1295_Find_Numbers_with_Even_Number_of_Digits.java
+++ src/main/java/leetcode/LC1295_Find_Numbers_with_Even_Number_of_Digits.java
@@ -2,6 +2,15 @@ package leetcode;
 
 public class LC1295_Find_Numbers_with_Even_Number_of_Digits {
     public int findNumbers(int[] nums) {
-        return 0;
+        int count = 0;
+        for (int num : nums) {
+            String numString = String.valueOf(num);
+
+            if (numString.length() % 2 == 0) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
