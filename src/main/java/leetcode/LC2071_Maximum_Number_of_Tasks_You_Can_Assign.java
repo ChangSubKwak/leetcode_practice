5f4f1f966f4084668e814e05cffdf3be18ff89bd
changsubkwak <paradise1732@gmail.com>
2025-05-02 00:07:58 +0900

    feat: (WIP) sovling - 2071. Maximum Number of Tasks You Can Assign

---
+++ src/main/java/leetcode/LC2071_Maximum_Number_of_Tasks_You_Can_Assign.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC2071_Maximum_Number_of_Tasks_You_Can_Assign {
+    public int maxTaskAssign(int[] tasks, int[] workers, int pills, int strength) {
+        return 0;
+    }
+}
