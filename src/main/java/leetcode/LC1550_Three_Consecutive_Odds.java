1df92a476b494ef6e9335e0bcd31852d94832f01
changsubkwak <paradise1732@gmail.com>
2025-05-12 23:49:30 +0900

    feat: sovled - 1550. Three Consecutive Odds

--- src/main/java/leetcode/LC1550_Three_Consecutive_Odds.java
+++ src/main/java/leetcode/LC1550_Three_Consecutive_Odds.java
@@ -2,6 +2,18 @@ package leetcode;
 
 public class LC1550_Three_Consecutive_Odds {
     public boolean threeConsecutiveOdds(int[] arr) {
+        int length = arr.length;
+
+        for (int i = 0; i < length - 2; i++) {
+            int first  = arr[i];
+            int second = arr[i + 1];
+            int third  = arr[i + 2];
+
+            if (first % 2 == 1 && second % 2 == 1 && third % 2 == 1) {
+                return true;
+            }
+        }
+
         return false;
     }
 }
