a20edeeaf2f7127b766de518b194c06ca66c058f
changsubkwak <paradise1732@gmail.com>
2025-05-13 23:31:33 +0900

    feat: (WIP) solving - 3335. Total Characters in String After Transformations I

---
+++ src/main/java/leetcode/LC3335_Total_Characters_in_String_After_Transformations_I.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC3335_Total_Characters_in_String_After_Transformations_I {
+    public int lengthAfterTransformations(String s, int t) {
+        return 0;
+    }
+}
