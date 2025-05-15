a16e0264c698752965328948abefd5aa3670e1ef
changsubkwak <paradise1732@gmail.com>
2025-05-15 23:44:16 +0900

    test: fix, add test case - 3337. Total Characters in String After Transformations II

--- src/test/java/leetcode/LC3337_Total_Characters_in_String_After_Transformations_II_Test.java
+++ src/test/java/leetcode/LC3337_Total_Characters_in_String_After_Transformations_II_Test.java
@@ -1,7 +1,7 @@
 package leetcode;
 
 import org.junit.jupiter.api.Test;
-
+import java.util.Arrays;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LC3337_Total_Characters_in_String_After_Transformations_II_Test {
@@ -9,6 +9,16 @@ public class LC3337_Total_Characters_in_String_After_Transformations_II_Test {
     void test() {
         LC3335_Total_Characters_in_String_After_Transformations_II test = new LC3335_Total_Characters_in_String_After_Transformations_II();
 
-        assertThat(test.lengthAfterTransformations("abcyy", 2)).isEqualTo(7);
+        assertThat(test.lengthAfterTransformations(
+            "abcyy",
+            2,
+            Arrays.asList(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2)
+        )).isEqualTo(7);
+
+        assertThat(test.lengthAfterTransformations(
+            "azbk",
+            1,
+            Arrays.asList(2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2)
+        )).isEqualTo(8);
     }
 }
