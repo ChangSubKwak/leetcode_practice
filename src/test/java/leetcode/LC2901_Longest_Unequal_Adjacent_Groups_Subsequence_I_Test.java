acc03a08df1e093b27708566b43502a8dde50b52
changsubkwak <paradise1732@gmail.com>
2025-05-19 00:33:36 +0900

    test: implement test case - 2901. Longest Unequal Adjacent Groups Subsequence I

---
+++ src/test/java/leetcode/LC2901_Longest_Unequal_Adjacent_Groups_Subsequence_I_Test.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LC2901_Longest_Unequal_Adjacent_Groups_Subsequence_I_Test {
+    @Test
+    void test() {
+        LC2901_Longest_Unequal_Adjacent_Groups_Subsequence_I test = new LC2901_Longest_Unequal_Adjacent_Groups_Subsequence_I();
+
+        assertThat(test.getLongestSubsequence(new String[]{"e", "a", "b"}, new int[]{0, 0, 1})).isEqualTo(Arrays.asList("e", "b"));
+        assertThat(test.getLongestSubsequence(new String[]{"a", "b", "c", "d"}, new int[]{1, 0, 1, 1})).isEqualTo(Arrays.asList("a", "b", "c"));
+    }
+}
