02d80a3b6196615d45f802d4df6b36b1c46bfca8
changsubkwak <paradise1732@gmail.com>
2025-05-20 23:42:30 +0900

    feat: solved - 3024. Type of Triangle

--- src/main/java/leetcode/LC3024_Type_of_Triangle.java
+++ src/main/java/leetcode/LC3024_Type_of_Triangle.java
@@ -1,7 +1,28 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LC3024_Type_of_Triangle {
     public String triangleType(int[] nums) {
-        return "";
+        Arrays.sort(nums);
+        if (nums[0] + nums[1] <= nums[2]) {
+            return "none";
+        }
+
+        if (nums[0] == nums[1] && nums[1] == nums[2]) {
+            return "equilateral";
+        }
+
+        List<Integer> numsList = Arrays.stream(nums)
+                .boxed()
+                .collect(Collectors.toList());
+
+        if (numsList.contains(nums[0] ^ nums[1] ^ nums[2])) {
+            return "isosceles";
+        }
+
+        return "scalene";
     }
 }
