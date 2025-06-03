05b4e23515467a60488b6cb38a023c064c836651
changsubkwak <paradise1732@gmail.com>
2025-06-03 23:52:56 +0900

    feat: (WIP) sovling - 1298. Maximum Candies You Can Get from Boxes

---
+++ src/main/java/leetcode/LC1298_Maximum_Candies_You_Can_Get_from_Boxes.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC1298_Maximum_Candies_You_Can_Get_from_Boxes {
+    public int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
+        return 0;
+    }
+}
