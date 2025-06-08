e9a42f4ffb131682c20bc63e9d6e1205c73fe23a
changsubkwak <paradise1732@gmail.com>
2025-06-09 03:15:02 +0900

    feat: solved - 386. Lexicographical Numbers

--- src/main/java/leetcode/LC0386_Lexicographical_Numbers.java
+++ src/main/java/leetcode/LC0386_Lexicographical_Numbers.java
@@ -1,9 +1,33 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LC0386_Lexicographical_Numbers {
     public List<Integer> lexicalOrder(int n) {
-        return List.of();
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            generate(i, n, numbers);
+        }
+        return numbers;
+    }
+
+    private void generate(int current, int limit, List<Integer> result) {
+        if (current > limit) {
+            return;
+        }
+
+        result.add(current);
+
+        for (int i = 0; i <= 9; i++) {
+            int next = current * 10 + i;
+
+            if (next <= limit) {
+                generate(next, limit, result);
+                continue;
+            }
+
+            break;
+        }
     }
 }
