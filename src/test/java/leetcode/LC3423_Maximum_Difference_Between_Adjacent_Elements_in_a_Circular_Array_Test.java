fdd844fd467fe39f6037c86338645f12fb27275a
changsubkwak <paradise1732@gmail.com>
2025-06-12 21:50:25 +0900

    test: implement test case - 3423. Maximum Difference Between Adjacent Elements in a Circular Array

---
+++ src/test/java/leetcode/LC3423_Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array_Test.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LC3423_Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array_Test {
+    @Test
+    void test() {
+        LC3423_Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array test = new LC3423_Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array();
+
+        assertThat(test.answerString(new int[]{1, 2, 4})).isEqualTo(3);
+        assertThat(test.answerString(new int[]{-5, 10, -5})).isEqualTo(5);
+
+    }
+}
