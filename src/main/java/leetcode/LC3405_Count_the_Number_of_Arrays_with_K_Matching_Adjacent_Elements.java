7af79fe10c400f3455e86353f7e2cc2f027d6ac8
changsubkwak <paradise1732@gmail.com>
2025-06-17 23:42:32 +0900

    feat: (WIP) sovling - 3405. Count the Number of Arrays with K Matching Adjacent Elements

---
+++ src/main/java/leetcode/LC3405_Count_the_Number_of_Arrays_with_K_Matching_Adjacent_Elements.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC3405_Count_the_Number_of_Arrays_with_K_Matching_Adjacent_Elements {
+    public int countGoodArrays(int n, int m, int k) {
+        return 0;
+    }
+}
