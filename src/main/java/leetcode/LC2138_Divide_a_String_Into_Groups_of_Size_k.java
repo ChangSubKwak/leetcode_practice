324b55b2bb300556c4c7fe3ed7e1d6b3e8dfc418
changsubkwak <paradise1732@gmail.com>
2025-06-23 22:14:03 +0900

    feat: sovled - 2138. Divide a String Into Groups of Size k

--- src/main/java/leetcode/LC2138_Divide_a_String_Into_Groups_of_Size_k.java
+++ src/main/java/leetcode/LC2138_Divide_a_String_Into_Groups_of_Size_k.java
@@ -2,6 +2,20 @@ package leetcode;
 
 public class LC2138_Divide_a_String_Into_Groups_of_Size_k {
     public String[] divideString(String s, int k, char fill) {
-        return null;
+        int indexSize = (int)Math.ceil(s.length() * 1.0 / k);
+        String[] result = new String[indexSize];
+
+        for (int i = 0; i < indexSize; i++) {
+            result[i] = s.substring(i * k, Math.min((i + 1) * k, s.length()));
+        }
+
+        String last = result[indexSize - 1];
+        for (int i = last.length(); i < k; i++) {
+            last += fill;
+        }
+
+        result[indexSize - 1] = last;
+
+        return result;
     }
 }
