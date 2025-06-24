8c307c466e192569694a4f403ac2197f7ecc556f
changsubkwak <paradise1732@gmail.com>
2025-06-24 22:07:35 +0900

    feat: (WIP) solving - 2200. Find All K-Distant Indices in an Array

---
+++ src/main/java/leetcode/LC2200_Find_All_K_Distant_Indices_in_an_Array.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+import java.util.List;
+
+public class LC2200_Find_All_K_Distant_Indices_in_an_Array {
+    public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+        return List.of();
+    }
+}
