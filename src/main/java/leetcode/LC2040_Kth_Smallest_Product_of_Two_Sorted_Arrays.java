c9509c42c01c23ee1a8b42414d815b2027cbae03
changsubkwak <paradise1732@gmail.com>
2025-06-25 20:10:05 +0900

    feat: (WIP) solving - 2040. Kth Smallest Product of Two Sorted Arrays

---
+++ src/main/java/leetcode/LC2040_Kth_Smallest_Product_of_Two_Sorted_Arrays.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC2040_Kth_Smallest_Product_of_Two_Sorted_Arrays {
+    public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
+        return 0L;
+    }
+}
