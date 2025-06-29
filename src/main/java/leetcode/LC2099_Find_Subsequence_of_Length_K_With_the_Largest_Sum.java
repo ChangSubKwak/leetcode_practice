1620a6d854ea6869a4ef151b2c6824743de01254
changsubkwak <paradise1732@gmail.com>
2025-06-29 23:58:38 +0900

    feat: (WIP) sovling - 2099. Find Subsequence of Length K With the Largest Sum #2

--- src/main/java/leetcode/LC2099_Find_Subsequence_of_Length_K_With_the_Largest_Sum.java
+++ src/main/java/leetcode/LC2099_Find_Subsequence_of_Length_K_With_the_Largest_Sum.java
@@ -2,6 +2,6 @@ package leetcode;
 
 public class LC2099_Find_Subsequence_of_Length_K_With_the_Largest_Sum {
     public int[] maxSubsequence(int[] nums, int k) {
-        return null;
+        return new int[]{};
     }
 }
