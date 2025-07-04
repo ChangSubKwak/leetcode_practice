d77f4024d461572f93ecd01cbc68739c206d9dc7
changsubkwak <paradise1732@gmail.com>
2025-07-04 22:16:13 +0900

    feat: solved - 3304. Find the K-th Character in String Game I

--- src/main/java/leetcode/LC3304_Find_the_K_th_Character_in_String_Game_I.java
+++ src/main/java/leetcode/LC3304_Find_the_K_th_Character_in_String_Game_I.java
@@ -2,6 +2,22 @@ package leetcode;
 
 public class LC3304_Find_the_K_th_Character_in_String_Game_I {
     public char kthCharacter(int k) {
-        return 'a';
+        StringBuilder sb = new StringBuilder();
+
+        while (k > sb.length()) {
+
+            if (sb.length() == 0) {
+                sb.append("a");
+                continue;
+            }
+
+            int length = sb.length();
+            for (int i = 0; i < length; i++) {
+                int next = (sb.charAt(i) - 'a' + 1) % 26;
+                sb.append((char)('a' + next));
+            }
+        }
+
+        return sb.charAt(k - 1);
     }
 }
