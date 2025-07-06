9041b8b9559297d311f67ef8209ce1b4ca7fefc3
changsubkwak <paradise1732@gmail.com>
2025-07-06 23:48:50 +0900

    feat: (WIP) sovling - 1865. Finding Pairs With a Certain Sum

---
+++ src/main/java/leetcode/LC1865_Finding_Pairs_With_a_Certain_Sum.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class LC1865_Finding_Pairs_With_a_Certain_Sum {
+    static class FindSumPairs {
+
+        public FindSumPairs(int[] nums1, int[] nums2) {
+
+        }
+
+        public void add(int index, int val) {
+
+        }
+
+        public int count(int tot) {
+            return 0;
+        }
+    }
+}
