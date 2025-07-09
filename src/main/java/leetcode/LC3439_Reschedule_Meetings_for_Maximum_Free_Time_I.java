f05dab172bfc55f1479b0c8fedc24433c34e4176
changsubkwak <paradise1732@gmail.com>
2025-07-09 23:02:13 +0900

    feat: (WIP) solving - 3439. Reschedule Meetings for Maximum Free Time I

---
+++ src/main/java/leetcode/LC3439_Reschedule_Meetings_for_Maximum_Free_Time_I.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC3439_Reschedule_Meetings_for_Maximum_Free_Time_I {
+    public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
+        return 0;
+    }
+}
