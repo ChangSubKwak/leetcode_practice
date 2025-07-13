e745c77962c955acd311b061709bee35e316ee83
changsubkwak <paradise1732@gmail.com>
2025-07-13 23:48:20 +0900

    feat: (WIP) sovling - 2410. Maximum Matching of Players With Trainers

---
+++ src/main/java/leetcode/LC2410_Maximum_Matching_of_Players_With_Trainers.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC2410_Maximum_Matching_of_Players_With_Trainers {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        return 0;
+    }
+}
