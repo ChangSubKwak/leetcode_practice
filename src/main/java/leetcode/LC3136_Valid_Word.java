c66afbeeb44418e39b40be5a0e88b4b22185fc57
changsubkwak <paradise1732@gmail.com>
2025-07-16 22:31:32 +0900

    feat: solved - 3136. Valid Word

--- src/main/java/leetcode/LC3136_Valid_Word.java
+++ src/main/java/leetcode/LC3136_Valid_Word.java
@@ -2,6 +2,36 @@ package leetcode;
 
 public class LC3136_Valid_Word {
     public boolean isValid(String word) {
+        if (word.length() < 3) {
+            return false;
+        }
+
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+        boolean hasDigit = false;
+        for (char c : word.toLowerCase().toCharArray()) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                hasVowel = true;
+                continue;
+            }
+
+            if ('0' <= c && c <= '9') {
+                hasDigit = true;
+                continue;
+            }
+
+            if ('a' <= c && c <= 'z') {
+                hasConsonant = true;
+                continue;
+            }
+
+            return false;
+        }
+
+        if (!hasVowel || !hasConsonant) {
+            return false;
+        }
+
         return true;
     }
 }
