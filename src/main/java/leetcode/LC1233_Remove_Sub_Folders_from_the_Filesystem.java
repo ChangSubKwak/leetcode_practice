3665ba39adfc54bff51ad1704d29fb087c6749a5
changsubkwak <paradise1732@gmail.com>
2025-07-19 23:04:49 +0900

    feat: (WIP) sovling - 1233. Remove Sub-Folders from the Filesystem

---
+++ src/main/java/leetcode/LC1233_Remove_Sub_Folders_from_the_Filesystem.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+import java.util.List;
+
+public class LC1233_Remove_Sub_Folders_from_the_Filesystem {
+    public List<String> removeSubfolders(String[] folder) {
+        return null;
+    }
+}
