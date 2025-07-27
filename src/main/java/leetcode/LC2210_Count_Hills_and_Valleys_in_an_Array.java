5168962184533ed0c1a0b0979be2c12b747d4d6a
changsubkwak <paradise1732@gmail.com>
2025-07-28 03:04:46 +0900

    feat: solved - 2210. Count Hills and Valleys in an Array

--- src/main/java/leetcode/LC2210_Count_Hills_and_Valleys_in_an_Array.java
+++ src/main/java/leetcode/LC2210_Count_Hills_and_Valleys_in_an_Array.java
@@ -2,6 +2,24 @@ package leetcode;
 
 public class LC2210_Count_Hills_and_Valleys_in_an_Array {
     public int countHillValley(int[] nums) {
-        return 0;
+        int count = 0;
+
+        for (int i = 1; i < nums.length - 1; i++) {
+            int next = 1;
+            while(i + next < nums.length - 1 && nums[i] == nums[i + next]) {
+                next++;
+            }
+
+            if (nums[i - 1] < nums[i] && nums[i] > nums[i + next]) {
+                count++;
+                continue;
+            }
+
+            if (nums[i - 1] > nums[i] && nums[i] < nums[i + next]) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
