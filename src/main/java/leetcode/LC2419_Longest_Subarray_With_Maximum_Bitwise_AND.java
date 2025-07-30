3cc57f88d933eff69b299cabe6561cd6d9977bde
changsubkwak <paradise1732@gmail.com>
2025-07-30 23:56:32 +0900

    feat: (WIP) sovling - 2419. Longest Subarray With Maximum Bitwise AND

--- src/main/java/leetcode/LC2419_Longest_Subarray_With_Maximum_Bitwise_AND.java
+++ src/main/java/leetcode/LC2419_Longest_Subarray_With_Maximum_Bitwise_AND.java
@@ -2,6 +2,18 @@ package leetcode;
 
 public class LC2419_Longest_Subarray_With_Maximum_Bitwise_AND {
     public int longestSubarray(int[] nums) {
+        int maxValue = 0;
+        int answer = 0;
+        int currentStreak = 0;
+
+        for (int num : nums) {
+            if (maxValue < num) {
+                maxValue = num;
+                answer = 0;
+                currentStreak = 0;
+            }
+        }
+
         return 2;
     }
 }
