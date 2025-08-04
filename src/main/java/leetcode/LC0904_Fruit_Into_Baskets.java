ec87bac897a0a9b95811e2a39db4cf4b944db8ff
changsubkwak <paradise1732@gmail.com>
2025-08-05 00:53:15 +0900

    feat: solved - 904. Fruit Into Baskets

--- src/main/java/leetcode/LC0904_Fruit_Into_Baskets.java
+++ src/main/java/leetcode/LC0904_Fruit_Into_Baskets.java
@@ -1,7 +1,52 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LC0904_Fruit_Into_Baskets {
     public int totalFruit(int[] fruits) {
-        return 0;
+        int length = fruits.length;
+
+        Map<Integer, Integer> count = new HashMap<>();
+        int left = 0;
+        int previousKey = fruits[0];
+        int max = -1;
+
+        for (int i = 0; i < length; i++) {
+            count.put(fruits[i], count.getOrDefault(fruits[i], 0) + 1);
+
+            if (count.size() == 1) {
+                max = Math.max(max, count.get(previousKey));
+                continue;
+            }
+
+            if (count.size() == 2) {
+                int sum = 0;
+                for(int key : count.keySet()) {
+                    sum += count.get(key);
+                }
+                max = Math.max(max, sum);
+                continue;
+            }
+
+            if (count.size() > 2) {
+                max = Math.max(max, i - left);
+                int j;
+                for (j = left; j < i; j++) {
+                    count.put(fruits[j], count.get(fruits[j]) - 1);
+                    if (count.get(fruits[j]) == 0) {
+                        count.remove(fruits[j]);
+                    }
+
+                    if (count.size() <= 2) {
+                        break;
+                    }
+                }
+                left = j + 1;
+                previousKey = fruits[i - 1];
+            }
+        }
+
+        return max;
     }
 }
