f5f7f42bb9d2553cfe00fd2aff7c919395c82264
changsubkwak <paradise1732@gmail.com>
2025-08-06 23:36:21 +0900

    feat: solved - 904. Fruit Into Baskets II

--- src/main/java/leetcode/LC3477_Fruits_Into_Baskets_II.java
+++ src/main/java/leetcode/LC3477_Fruits_Into_Baskets_II.java
@@ -2,6 +2,22 @@ package leetcode;
 
 public class LC3477_Fruits_Into_Baskets_II {
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
-        return -1;
+        for (int i = 0; i < fruits.length; i++) {
+            for (int j = 0; j < baskets.length; j++) {
+                if (fruits[i] <= baskets[j]) {
+                    baskets[j] = 0;
+                    break;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < baskets.length; i++) {
+            if (baskets[i] != 0) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
