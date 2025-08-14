1af2a5cbec1d05a0f564dee44144791cf4bf23b9
changsubkwak <paradise1732@gmail.com>
2025-08-14 23:24:24 +0900

    feat: solved - 326. Power of Three

--- src/main/java/leetcode/LC0326_Power_of_Three.java
+++ src/main/java/leetcode/LC0326_Power_of_Three.java
@@ -2,6 +2,18 @@ package leetcode;
 
 public class LC0326_Power_of_Three {
     public boolean isPowerOfThree(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        while (n > 1) {
+            if (n % 3 != 0) {
+                return false;
+            }
+
+            n /= 3;
+        }
+
         return true;
     }
 }
