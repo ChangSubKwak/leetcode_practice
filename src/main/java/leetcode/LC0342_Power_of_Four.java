c338e21b3d25fe99c05567fe7e756ddc21672ecf
changsubkwak <paradise1732@gmail.com>
2025-08-15 15:47:58 +0900

    feat: (WIP) solving - 342. Power of Four

--- src/main/java/leetcode/LC0342_Power_of_Four.java
+++ src/main/java/leetcode/LC0342_Power_of_Four.java
@@ -2,12 +2,6 @@ package leetcode;
 
 public class LC0342_Power_of_Four {
     public boolean isPowerOfFour(int n) {
-        for (int i = 0; i <= 15; i++) {
-            int powNum = (int)(Math.pow(4, i));
-            if (n == powNum) {
-                return true;
-            }
-        }
         return false;
     }
 }
