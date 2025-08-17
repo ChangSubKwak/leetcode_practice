d3a89171d074e9255c5d22fdead678cc52020953
changsubkwak <paradise1732@gmail.com>
2025-08-18 00:00:37 +0900

    feat: (WIP) solving - 837. New 21 Game

--- src/main/java/leetcode/LC0837_New_21_Game.java
+++ src/main/java/leetcode/LC0837_New_21_Game.java
@@ -2,6 +2,9 @@ package leetcode;
 
 public class LC0837_New_21_Game {
     public double new21Game(int n, int k, int maxPts) {
+        double[] dp = new double[n + 1];
+        dp[0] = 1;
+
         return 0;
     }
 }
