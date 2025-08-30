567faf1b85d14284a7269ac44ef6cf5c3713f5c7
changsubkwak <paradise1732@gmail.com>
2025-08-30 23:02:44 +0900

    feat: (WIP) solving - 36. Valid Sudoku

---
+++ src/main/java/leetcode/LC0036_Valid_Sudoku.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC0036_Valid_Sudoku {
+    public boolean isValidSudoku(char[][] board) {
+        return false;
+	}
+}
