8b1c0898579fd0069986f0196e313a8495ad7895
changsubkwak <paradise1732@gmail.com>
2025-09-08 23:12:24 +0900

    feat: solved - 1304. Find N Unique Integers Sum up to Zero

--- src/main/java/leetcode/LC1304_Find_N_Unique_Integers_Sum_up_to_Zero.java
+++ src/main/java/leetcode/LC1304_Find_N_Unique_Integers_Sum_up_to_Zero.java
@@ -2,6 +2,21 @@ package leetcode;
 
 public class LC1304_Find_N_Unique_Integers_Sum_up_to_Zero {
     public int[] sumZero(int n) {
-        return null;
+        int[] result = new int[n];
+
+        if (n == 1) {
+            return new int[]{0};
+        }
+
+        for (int i = 1; i <= (int)(n / 2); i++) {
+            result[2 * (i - 1)] = i;
+            result[2 * (i - 1) + 1] = -i;
+        }
+
+        if (n % 2 == 1) {
+            result[n - 1] = 0;
+        }
+
+        return result;
     }
 }
