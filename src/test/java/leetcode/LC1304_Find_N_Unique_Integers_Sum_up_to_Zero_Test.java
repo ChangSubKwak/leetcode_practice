7e40e67a67db095110381b3a9079c8ff31594708
changsubkwak <paradise1732@gmail.com>
2025-09-08 23:12:08 +0900

    test: fix test case - 1304. Find N Unique Integers Sum up to Zero

--- src/test/java/leetcode/LC1304_Find_N_Unique_Integers_Sum_up_to_Zero_Test.java
+++ src/test/java/leetcode/LC1304_Find_N_Unique_Integers_Sum_up_to_Zero_Test.java
@@ -3,6 +3,8 @@ package leetcode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LC1304_Find_N_Unique_Integers_Sum_up_to_Zero_Test {
@@ -15,8 +17,8 @@ public class LC1304_Find_N_Unique_Integers_Sum_up_to_Zero_Test {
 	
 	@Test
 	void test() {
-		assertThat(test.sumZero(5)).isEqualTo(new int[]{-7, -1, 1, 3, 4});
-		assertThat(test.sumZero(3)).isEqualTo(new int[]{-1, 0, 1});
-		assertThat(test.sumZero(1)).isEqualTo(new int[]{0});
+		assertThat(Arrays.stream(test.sumZero(5)).sum()).isEqualTo(0);
+		assertThat(Arrays.stream(test.sumZero(3)).sum()).isEqualTo(0);
+		assertThat(Arrays.stream(test.sumZero(1)).sum()).isEqualTo(0);
 	}
 }
