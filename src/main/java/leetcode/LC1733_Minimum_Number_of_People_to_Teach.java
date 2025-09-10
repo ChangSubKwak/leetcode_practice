2343afeb9dcc42b51500f03ebef03464fa5b1443
changsubkwak <paradise1732@gmail.com>
2025-09-10 23:49:26 +0900

    feat: (WIP) solving - 1733. Minimum Number of People to Teach

---
+++ src/main/java/leetcode/LC1733_Minimum_Number_of_People_to_Teach.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC1733_Minimum_Number_of_People_to_Teach {
+    public int minimumTeachings(int n, int[][] languages, int[][] friendships) {
+        return 0;
+    }
+}
