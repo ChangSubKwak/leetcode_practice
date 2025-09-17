ba4e78c8a7af235aaa3e92ed8b9071ad57be8cc8
changsubkwak <paradise1732@gmail.com>
2025-09-17 23:56:10 +0900

    feat: (WIP) solving - 2353. Design a Food Rating System

---
+++ src/main/java/leetcode/LC2353_Design_a_Food_Rating_System.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class LC2353_Design_a_Food_Rating_System {
+    static class FoodRatings {
+
+        public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+
+        }
+
+        public void changeRating(String food, int newRating) {
+
+        }
+
+        public String highestRated(String cuisine) {
+            return null;
+        }
+    }
+}
