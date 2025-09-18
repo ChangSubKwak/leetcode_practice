1e278da68f6b65f5737506c5769ebba1eea8115a
changsubkwak <paradise1732@gmail.com>
2025-09-19 00:01:26 +0900

    feat: (WIP) solving - 3408. Design Task Manager

---
+++ src/main/java/leetcode/LC3408_Design_Task_Manager.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.List;
+
+public class LC3408_Design_Task_Manager {
+    static class TaskManager {
+
+        public TaskManager(List<List<Integer>> tasks) {
+
+        }
+
+        public void add(int userId, int taskId, int priority) {
+
+        }
+
+        public void edit(int taskId, int newPriority) {
+
+        }
+
+        public void rmv(int taskId) {
+
+        }
+
+        public int execTop() {
+            return 0;
+        }
+    }
+}
