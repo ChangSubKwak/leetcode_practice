fd04f538cfbd5d5ba66464d901c6947327ac30ff
changsubkwak <paradise1732@gmail.com>
2025-09-20 19:43:17 +0900

    feat: (WIP) solving - 3508. Implement Router

---
+++ src/main/java/leetcode/LC3508_Implement_Router.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.TreeSet;
+
+public class LC3508_Implement_Router {
+    static class Router {
+        public Router(int memoryLimit) {
+
+        }
+
+        public boolean addPacket(int source, int destination, int timestamp) {
+            return true;
+        }
+
+        public int[] forwardPacket() {
+            return null;
+        }
+
+        public int getCount(int destination, int startTime, int endTime) {
+            return 0;
+        }
+    }
+}
