d3d3ace97cc2b6228f90816b508ec5ce0eeb5c87
changsubkwak <paradise1732@gmail.com>
2025-09-23 21:48:28 +0900

    feat: solved - 3005. Count Elements With Maximum Frequency

--- src/main/java/leetcode/LC3005_Count_Elements_With_Maximum_Frequency.java
+++ src/main/java/leetcode/LC3005_Count_Elements_With_Maximum_Frequency.java
@@ -2,6 +2,21 @@ package leetcode;
 
 public class LC3005_Count_Elements_With_Maximum_Frequency {
     public int maxFrequencyElements(int[] nums) {
-        return 0;
+        int[] count = new int[101];
+
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count[nums[i]]++;
+            max = Math.max(max, count[nums[i]]);
+        }
+
+        int result = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (count[i] == max) {
+                result += max;
+            }
+        }
+
+        return result;
     }
 }
