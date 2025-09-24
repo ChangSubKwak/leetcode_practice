c1b6c4eef334b94e6691064c01f3f6986853435f
changsubkwak <paradise1732@gmail.com>
2025-09-24 23:51:20 +0900

    test: fix test case - 166. Fraction to Recurring Decimal

--- src/test/java/leetcode/LC0166_Fraction_to_Recurring_Decimal.java
+++ src/test/java/leetcode/LC0166_Fraction_to_Recurring_Decimal.java
@@ -4,12 +4,13 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class LC0168_Excel_Sheet_Column_Title_Test {
+class LC0166_Fraction_to_Recurring_Decimal_Test {
     @Test
     void test() {
-        LC0168_Excel_Sheet_Column_Title test = new LC0168_Excel_Sheet_Column_Title();
-        assertThat(test.convertToTitle(1)).isEqualTo("A");
-        assertThat(test.convertToTitle(28)).isEqualTo("AB");
-        assertThat(test.convertToTitle(701)).isEqualTo("ZY");
+        LC0166_Fraction_to_Recurring_Decimal test = new LC0166_Fraction_to_Recurring_Decimal();
+        
+        assertThat(test.fractionToDecimal(1, 2)).isEqualTo("0.5");
+        assertThat(test.fractionToDecimal(2, 1)).isEqualTo("2");
+        assertThat(test.fractionToDecimal(4, 333)).isEqualTo("0.(012)");
     }
 }
