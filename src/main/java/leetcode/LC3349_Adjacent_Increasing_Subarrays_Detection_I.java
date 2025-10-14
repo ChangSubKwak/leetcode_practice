ed8034dd94536dfc485e6b68d9d198cfcd48d667
changsubkwak <paradise1732@gmail.com>
2025-10-15 00:59:50 +0900

    feat: solved - 3349. Adjacent Increasing Subarrays Detection I

--- src/main/java/leetcode/LC3349_Adjacent_Increasing_Subarrays_Detection_I.java
+++ src/main/java/leetcode/LC3349_Adjacent_Increasing_Subarrays_Detection_I.java
@@ -4,6 +4,41 @@ import java.util.List;
 
 public class LC3349_Adjacent_Increasing_Subarrays_Detection_I {
     public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
-        return true;
+        if (k == 1) {
+            return true;
+        }
+
+        boolean isContinue = false;
+        for (int i = 0; i < nums.size() - 2 * k + 1; i++) {
+            isContinue = true;
+
+            for (int j = i; j < i + k - 1; j++) {
+                if (nums.get(j) >= nums.get(j + 1)) {
+                    isContinue = false;
+                    break;
+                }
+            }
+
+            if (!isContinue) {
+                continue;
+            }
+
+            isContinue = true;
+
+            for (int j = i + k; j < i + 2 * k - 1; j++) {
+                if (nums.get(j) >= nums.get(j + 1)) {
+                    isContinue = false;
+                    break;
+                }
+            }
+
+            if (!isContinue) {
+                continue;
+            }
+
+            break;
+        }
+
+        return isContinue;
     }
 }
