6eb38c7678d5894665f3c35dd91b664696077318
changsubkwak <paradise1732@gmail.com>
2025-10-14 23:48:49 +0900

    feat: (WIP) solving - 3349. Adjacent Increasing Subarrays Detection I

---
+++ src/main/java/leetcode/LC3349_Adjacent_Increasing_Subarrays_Detection_I.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+import java.util.List;
+
+public class LC3349_Adjacent_Increasing_Subarrays_Detection_I {
+    public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
+        return true;
+    }
+}
