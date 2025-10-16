b42ca33397c35d28360033f05862c27ab200ccc9
changsubkwak <paradise1732@gmail.com>
2025-10-16 23:40:44 +0900

    feat: (WIP) solving - 2598. Smallest Missing Non-negative Integer After Operations

---
+++ src/main/java/leetcode/LC2598_Smallest_Missing_Non_negative_Integer_After_Operations.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC2598_Smallest_Missing_Non_negative_Integer_After_Operations {
+    public int findSmallestInteger(int[] nums, int value) {
+        return 0;
+    }
+}
