5432a6d40a1eb632e49259d3d9393095a10df696
changsubkwak <paradise1732@gmail.com>
2025-10-17 23:35:26 +0900

    feat: (WIP) solving - 3003. Maximize the Number of Partitions After Operations

---
+++ src/main/java/leetcode/LC3003_Maximize_the_Number_of_Partitions_After_Operations.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class LC3003_Maximize_the_Number_of_Partitions_After_Operations {
+    public int maxPartitionsAfterOperations(String s, int k) {
+        return 0;
+    }
+}
